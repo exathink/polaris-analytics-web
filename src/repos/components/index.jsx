b5d6b6b2aa9946147da43d50624951c658ad6b6a
Krishna Kumar <kkumar@exathink.com>
2018-02-28 09:21:49 -0600

    removed app folders to replace with isomorphic boilerplate

--- src/repos/components/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <div>
-    <h1>This is where repos should go!</h1>
-    <h2>(but it's a top-secret hidden page)</h2>
-  </div>
-);
--- src/utils.js
+++
@@ -1,4 +0,0 @@
-export const getCookie = (n) => {
-  const c = `; ${document.cookie}`.match(`;\\s*${n}=([^;]+)`);
-  return c ? c[1] : null;
-};
