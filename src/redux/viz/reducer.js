a7fa6d2c4502e31f902e39cb7ce89089f0545a41
Maxi Schell <maximiliano.schell@whiteprompt.com>
2018-03-02 10:45:50 -0300

    Improve readability and consistency for viz code

--- src/redux/viz/reducer.js
+++ src/redux/viz/reducer.js
@@ -1,12 +1,11 @@
 import { Map } from 'immutable';
-
 import actions from './actions';
 
 const initState = new Map({viz_data: null});
-export default function vizReducer(state = initState, action) {
+
+export default (state=initState, action) => {
   switch (action.type) {
     case actions.FETCH_DATA_SUCCESS:
-      console.log('Fetch data success..')
       return state.set('viz_data', action.payload);
     default:
       return state;
--- src/redux/viz/saga.js
+++ src/redux/viz/saga.js
@@ -1,25 +1,25 @@
 import {all, call, fork, put, takeEvery} from "redux-saga/effects";
 import actions from "./actions";
-import {getSessionKey} from "../../utils";
+import { getSessionKey } from "../../utils";
 
 const GET_CHART = "http://polaris-services.exathink.localdev:8200/charts";
 
-export const fetchUserData = async (key) => {
-    console.log("Fetching data");
-    const response = await fetch(`${GET_CHART}/project-summary/Exathink/`, {
-        headers: { 'X-XSRF-TOKEN': key }
-    });
-    const data = await response.json();
-    console.log('Fetched: ', data);
-    // Call me redundant-man
-    return data;
+export const fetchVizData = async (key) => {
+  const response = await fetch(`${GET_CHART}/project-summary/Exathink/`, {
+      headers: { 'X-XSRF-TOKEN': key }
+  });
+  const data = await response.json();
+
+  return data;
 };
 
 export function* fetchData() {
   yield takeEvery(actions.FETCH_DATA, function*() {
     const sessionKey = getSessionKey();
+
     if (sessionKey) {
-      const data = yield call(fetchUserData, sessionKey)
+      const data = yield call(fetchVizData, sessionKey)
+
       yield put({
         type: actions.FETCH_DATA_SUCCESS,
         payload: data,
@@ -27,6 +27,7 @@ export function* fetchData() {
     }
   });
 }
+
 export default function* visSagas() {
   yield all([
     fork(fetchData),
