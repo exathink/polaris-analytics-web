52e994394e1416c31b3064ddeb5200d3c8f9ef85
Krishna Kumar <kkumar@exathink.com>
2018-03-03 13:49:31 -0600

    make landscape chart responsive and get all chart attributes from props

--- src/viz/projectLandscape/projectLandscape.js
+++ src/viz/projectLandscape/projectLandscape.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {ScatterChart, ResponsiveContainer, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, Legend, LabelList} from 'recharts';
 
 export default class ProjectLandscape extends React.Component {
 
@@ -10,12 +10,13 @@ export default class ProjectLandscape extends React.Component {
     ];
   }
 
-  static mapData(viz_data) {
+  mapData(viz_data) {
     if (viz_data) {
       return viz_data.map((data_point) => ({
         x: data_point.commit_count,
         y: data_point.contributor_count,
-        z: 30
+        z: data_point.contributor_count,
+        label: data_point.project,
       }))
     }
   }
@@ -24,26 +25,29 @@ export default class ProjectLandscape extends React.Component {
 
     let viz_domain = ProjectLandscape.viz_domains(this.props)[0];
     let viz_data = this.props.viz_data.get(viz_domain);
-
+    let chart_data = this.mapData(viz_data);
     return (
-        <ScatterChart
-          width={730}
-          height={250}
-          margin={{
-            top: 20, right: 20, bottom: 10, left: 10
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="x" name="Commits" unit="" />
-          <YAxis dataKey="y" name="Contributors" unit="" />
-          <Tooltip cursor={{strokeDasharray: '3 3'}} />
-          <Legend />
-          <Scatter
-            name="Exathink"
-            data={ProjectLandscape.mapData(viz_data)}
-            fill="#8884d8"
-          />
-        </ScatterChart>
+        <ResponsiveContainer width="100%" height="100%" >
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 10, left: 10
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="x" name="Commits" unit="" />
+            <YAxis dataKey="y" name="Contributors" unit="" />
+            <ZAxis dataKey="z" range={[64, 144]} name="Contributors" unit="km" />
+            <Tooltip cursor={{strokeDasharray: '3 3'}} />
+            <Legend />
+            <Scatter
+              name={`${this.props.account.company} Project Landscape`}
+              data={chart_data}
+              fill="#8884d8"
+            >
+              <LabelList dataKey="label" position="top" />
+            </Scatter>
+          </ScatterChart>
+        </ResponsiveContainer>
     )
   }
 }
