9f05c26774bfc6ed22847e23543fd13d20f63413
Maxi Schell <maximiliano.schell@whiteprompt.com>
2018-03-12 13:16:47 -0300

    [#155875769] Add basic dashboard style

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Flex, Box } from 'reflexbox';
 import LayoutWrapper from '../../components/utility/layoutWrapper';
 
-const wrapperStyle =  {width: '100%', height: '100%' };
+const wrapperStyle =  { width: '100%', height: '100%', padding: 0 };
+
 const rowStyle = { width: '100%' };
-const itemStyle = { height: '100%' };
+
+const itemStyle = {
+  height: '100%',
+  padding: '10%',
+  border: '2px solid slategrey',
+  background: 'ghostwhite'
+};
 
 export const DashboardItem = ({ w=1, children }) => (
   <Box w={w} style={itemStyle}>
