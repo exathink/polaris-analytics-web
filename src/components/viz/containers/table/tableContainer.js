a30779a8d965e0771c14d592a1099b97d2a5ae25
Krishna Kumar <kkumar@exathink.com>
2018-03-24 16:31:15 -0500

    Simplified style overrides for ReactTable container -- no need for react-dimensions wrapper [ #156232179 (https://www.pivotaltracker.com/story/show/156232179)]

--- src/components/viz/containers/table/tableContainer.js
+++ src/components/viz/containers/table/tableContainer.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 
-import {cloneChildrenWithProps} from "../../../../helpers/reactHelpers";
-import Dimensions from 'react-dimensions';
 
-class TableContainer extends React.Component {
-  render() {
-    const {containerHeight, containerWidth, children, ...rest} = this.props;
-    return (
-      <ReactTable style={{height: containerHeight, width: containerWidth}} {...rest}>
-        {cloneChildrenWithProps(children, {containerWidth: containerWidth, containerHeight: containerHeight, ...rest})}
+export default (props) => (
+      <ReactTable {...props} style={{height: "100%", width: "100%"}}>
       </ReactTable>
-    );
-  }
-}
-export default Dimensions({elementResize: true})(TableContainer);
+ );
+
+
