18ded47b814389712ba2d54a08af3692031030cf
Krishna Kumar <kkumar@exathink.com>
2018-04-10 08:40:20 -0500

    Added bundle analyzer to build [ #156627664 (https://www.pivotaltracker.com/story/show/156627664)]

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,5 @@
+const rewireBundleAnalyzer = require('react-app-rewire-bundle-analyzer');
+
 module.exports = function override(config, env) {
   if (env === 'production') {
     config.devtool = false;
