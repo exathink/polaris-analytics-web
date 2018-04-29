b29ebefb9413e5913d00c9f8d3177b04db8358d1
Krishna Kumar <kkumar@exathink.com>
2018-04-29 15:23:49 -0500

    Change buildRouteTree to return a full React component rather a functional component - prep for adding lifecycle behavior [ #157047854 (https://www.pivotaltracker.com/story/show/157047854)]

--- src/app/dashboards/routes.js
+++ src/app/dashboards/routes.js
@@ -2,66 +2,70 @@ import React from 'react';
 import {Redirect, Route, Switch} from 'react-router-dom';
 
 
-export const buildRoutes = (routeTree, path = '') => (
-  props => {
-    const {match} = props;
+export const buildRoutes = (routeTree, path = '') => {
 
-    if (routeTree.routes != null) {
-      return React.createElement(
-        Switch,
-        {},
-        ...routeTree.routes.map(
-          route => {
-            if (route.match === null) {
-              throw new Error(`Route did not specify a match property`)
-            }
-            const target =
-              route.component ? {component: route.component} :
-                route.render ? {render: route.render} :
-                  route.redirect ? {render: () => <Redirect to={`${match.url}/${route.redirect}`}/>} :
-                    null;
+  return class RouterNode extends React.Component {
+    render() {
+      const {match} = this.props;
 
-            // This is the child component displayed at this path. May be null.
-            const targetRoute =
-              target != null ?
-                route.match.length > 1 ?
-                  <Route
-                    key={`${route.match}.target`}
-                    path={`${match.path}/${route.match}`}
-                    {...target}
-                  /> :
-                  // Empty string match pattern: use exact match to base match path
-                  <Route
-                    key={`${route.match}.target`}
-                    exact path={`${match.path}`}
-                    {...target}
-                  /> :
-                null;
-
-            // This recursively builds the route for the routes that are rooted at this path. May be null
-            const childRouter =
-              route.routes ?
-                <Route
-                  key={`${route.match}.childRouter`}
-                  path={`${match.path}/${route.match}`}
-                  render={buildRoutes(route.routes, `${path}/${route.match}`)}
-                /> :
-                null;
-
-            if (targetRoute || childRouter) {
-              if (targetRoute && childRouter) {
-                //throw new Error(`Route must specify exactly one of target component or child routes`);
+      if (routeTree.routes != null) {
+        return React.createElement(
+          Switch,
+          {},
+          ...routeTree.routes.map(
+            route => {
+              if (route.match === null) {
+                throw new Error(`Route did not specify a match property`)
               }
-              return targetRoute || childRouter
-            } else {
-              throw new Error(`Route must specify either a target or child routes`);
+              const target =
+                route.component ? {component: route.component} :
+                  route.render ? {render: route.render} :
+                    route.redirect ? {render: () => <Redirect to={`${match.url}/${route.redirect}`}/>} :
+                      null;
+
+              // This is the child component displayed at this path. May be null.
+              const targetRoute =
+                target != null ?
+                  route.match.length > 1 ?
+                    <Route
+                      key={`${route.match}.target`}
+                      path={`${match.path}/${route.match}`}
+                      {...target}
+                    /> :
+                    // Empty string match pattern: use exact match to base match path
+                    <Route
+                      key={`${route.match}.target`}
+                      exact path={`${match.path}`}
+                      {...target}
+                    /> :
+                  null;
+
+              // This recursively builds the route for the routes that are rooted at this path. May be null
+              const childRouter =
+                route.routes ?
+                  <Route
+                    key={`${route.match}.childRouter`}
+                    path={`${match.path}/${route.match}`}
+                    component={buildRoutes(route.routes, `${path}/${route.match}`)}
+                  /> :
+                  null;
+
+              if (targetRoute || childRouter) {
+                if (targetRoute && childRouter) {
+                  //throw new Error(`Route must specify exactly one of target component or child routes`);
+                }
+                return targetRoute || childRouter
+              } else {
+                throw new Error(`Route must specify either a target or child routes`);
+              }
             }
-          }
+          )
         )
-      )
+      }
     }
   }
-);
+};
+
 
 
 
