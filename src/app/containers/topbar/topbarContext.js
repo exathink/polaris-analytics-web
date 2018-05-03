2d0a7dde2b5727e8289f3957e5d771b7a801cef8
Krishna Kumar <kkumar@exathink.com>
2018-05-02 19:30:03 -0500

    Basic wiring up of topBar to the context stack [ #157047854 (https://www.pivotaltracker.com/story/show/157047854)]

--- src/app/containers/topbar/topbarContext.js
+++ src/app/containers/topbar/topbarContext.js
@@ -1,32 +1,29 @@
 import React from 'react';
+import IntlMessages from '../../../components/utility/intlMessages';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-import {Route, Switch, withRouter} from 'react-router-dom';
+const topBar = (props) => {
+  const {navigation} = props;
+  if(navigation.length > 0) {
+      const currentContext = navigation[0];
+      return (
+        <h3>
+          <span className="nav-text">
+            <IntlMessages id={`context.${currentContext.name()}`}/>
+          </span>
+        </h3>
+      );
+  } else {
+      return null;
+  }
+};
 
-
-export default withRouter(() => (
-  <Switch>
-    <Route
-      path={'/app/dashboard/:aspect/organizations/:organization'}
-      render={(props) => <h3 className={"isoLeft"}>Organization: {`${props.match.params.organization}`}</h3>}
-    />
-    <Route
-      path={'/app/dashboard/:aspect/projects/:organization/:project'}
-      render={(props) => <h3 className={"isoLeft"}>Project: {`${props.match.params.project}`}</h3>}
-    />
-    <Route
-      path={'/app/dashboard/:aspect/repositories/:organization/:project/:repository'}
-      render={(props) => <h3 className={"isoLeft"}>Repository: {`${props.match.params.repository}`}</h3>}
-    />
-
-    <Route
-      render={(props) => {
-        console.log(props);
-        return null;
-      }}
-    />
-
-  </Switch>
-));
+export default withRouter(connect(
+  state => ({
+      navigation: state.navigation.toJS()
+  })
+)(topBar));
 
 
 
