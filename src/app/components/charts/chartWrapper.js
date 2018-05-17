735a1efe3b32440ebd45158b947f45dd03b8cbb6
Krishna Kumar <kkumar@exathink.com>
2018-05-16 21:10:44 -0500

    code reformat [ #157631932 (https://www.pivotaltracker.com/story/show/157631932)]

--- src/app/components/charts/chartWrapper.js
+++ src/app/components/charts/chartWrapper.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactHighcharts from 'react-highcharts';
-import Dimensions from  'react-dimensions';
+import Dimensions from 'react-dimensions';
 
 
 class ChartWrapper extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = ChartWrapper.setDefaults(props.config, props);
   }
@@ -23,11 +23,11 @@ class ChartWrapper extends React.Component {
 
 
   componentDidUpdate() {
-      const chart = this.getChart();
-      if(this.props.containerHeight !== chart.chartHeight || this.props.containerWidth !== chart.chartWidth) {
-        chart.setSize(this.props.containerWidth, this.props.containerHeight, false)
-      }
+    const chart = this.getChart();
+    if (this.props.containerHeight !== chart.chartHeight || this.props.containerWidth !== chart.chartWidth) {
+      chart.setSize(this.props.containerWidth, this.props.containerHeight, false)
     }
+  }
 
   render() {
     return (<ReactHighcharts config={this.state} isPureConfig={true} callback={this.props.afterRender} ref="chart"/>);
