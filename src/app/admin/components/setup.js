e5671692beb7835ff487de14688aaf5acd1e2e07
Krishna Kumar <kkumar@exathink.com>
2018-07-02 15:46:55 -0500

    Basic relay plumbing to execute graphql queries against server [ #158715222 (https://www.pivotaltracker.com/story/show/158715222)]

--- src/app/admin/components/setup.js
+++ src/app/admin/components/setup.js
@@ -1,4 +1,36 @@
+import React from 'react';
+import {graphql, QueryRenderer} from 'react-relay';
+import Relay from '../../relay/environment';
 
-export const Setup = () => {
-  return "Setup too";
-};
+
+export class Setup extends React.Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={Relay}
+        query={graphql`
+          query setupOrganizationQuery{
+            organizations {
+              edges {
+                node {
+                    name
+                    repoCount
+                }
+              }
+            }
+          }
+        `}
+        variables={{}}
+        render={({error, props}) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (!props) {
+            return <div>Loading...</div>;
+          }
+          return <div></div>;
+        }}
+      />
+    );
+  }
+}
