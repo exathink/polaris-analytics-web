ccaccb1c60c0d8eb57883a0f696c0621db198159
Krishna Kumar <kkumar@exathink.com>
2018-07-19 11:55:51 -0500

    refactored ActivitySummaryView to separate out the model attributes from presentation view [ #159140292 (https://www.pivotaltracker.com/story/show/159140292)]

--- src/app/dashboards/widgets/activity/ActivitySummary/view.js
+++ src/app/dashboards/widgets/activity/ActivitySummary/view.js
@@ -14,55 +14,63 @@ const human_span = (moment_a, moment_b) => {
   return years+months > 0 ? `${d_years}${(years > 0 ? ', ' : '')}${d_months}` : '0 Years';
 };
 
-export const ActivitySummaryView = (props: {model: ActivitySummaryModel}) => {
-
-  const {model}  = props;
-  const data = model.data;
-  const commits = data.commits? data.commits.toLocaleString() : '0';
-  const contributors = (data.contributors? data.contributors.toLocaleString() : '0');
-  const span = (data.latest_commit && data.earliest_commit ? human_span(data.latest_commit, data.earliest_commit) : 'N/A');
-  const bgColor = model.context.color();
-  const fontColor = "#ffffff";
+export const ActivitySummary = ({commitCount, span, contributorCount, fontColor, bgColor}) => {
   return (
     <React.Fragment>
       <VizRow h={"100%"}>
-        <VizItem w={1/3}>
+        <VizItem w={1 / 3}>
 
-            {/* Sticker Widget */}
-            <StickerWidget
-              number={commits}
-              text={"Commits"}
-              icon="ion-code"
-              fontColor={fontColor}
-              bgColor={bgColor}
-            />
-
-        </VizItem>
-        <VizItem w={1/3}>
-
-            {/* Sticker Widget */}
-            <StickerWidget
-              number={span}
-              text={'History'}
-              icon="ion-clock"
-              fontColor={fontColor}
-              bgColor={bgColor}
-            />
+          {/* Sticker Widget */}
+          <StickerWidget
+            number={commitCount}
+            text={"Commits"}
+            icon="ion-code"
+            fontColor={fontColor}
+            bgColor={bgColor}
+          />
 
         </VizItem>
-        <VizItem w={1/3}>
+        <VizItem w={1 / 3}>
 
-            {/* Sticker Widget */}
-            <StickerWidget
-              number={contributors}
-              text={`Contributor${data.contributors === 1 ? '' : 's'}`}
-              icon="ion-ios-people"
-              fontColor={fontColor}
-              bgColor={bgColor}
-            />
+          {/* Sticker Widget */}
+          <StickerWidget
+            number={span}
+            text={'History'}
+            icon="ion-clock"
+            fontColor={fontColor}
+            bgColor={bgColor}
+          />
+
+        </VizItem>
+        <VizItem w={1 / 3}>
+
+          {/* Sticker Widget */}
+          <StickerWidget
+            number={contributorCount}
+            text={`Contributor${contributorCount === 1 ? '' : 's'}`}
+            icon="ion-ios-people"
+            fontColor={fontColor}
+            bgColor={bgColor}
+          />
 
         </VizItem>
       </VizRow>
     </React.Fragment>
   )
 };
+
+export const ActivitySummaryView = (props: {model: ActivitySummaryModel}) => {
+  const data = props.model.data;
+
+  return (
+    <ActivitySummary
+        commitCount = {data.commits? data.commits.toLocaleString() : '0'}
+        contributorCount = {data.contributors? data.contributors.toLocaleString() : '0'}
+        span = {data.latest_commit && data.earliest_commit ? human_span(data.latest_commit, data.earliest_commit) : 'N/A'}
+        bgColor = {props.model.context.color()}
+        fontColor = "#ffffff"
+    />
+  )
+};
+
+
