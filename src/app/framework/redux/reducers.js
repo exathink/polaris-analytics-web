72dade46c652c5328fc2f11845a38a7c1aa83f33
Krishna Kumar <kkumar@exathink.com>
2018-07-18 20:24:18 -0500

    removed framework/navigation/redux directory [ #159140292 (https://www.pivotaltracker.com/story/show/159140292)]

--- src/app/framework/redux/reducers.js
+++ src/app/framework/redux/reducers.js
@@ -1,8 +1,6 @@
 import vizData from './vizData/reducer';
-import navigation from './navigation/reducer';
 import containerReducers from '../../containers/redux/reducers';
 export default {
   vizData,
-  navigation,
   ...containerReducers
 }
