d357e6b40478b99eb4c1dd6fc14561d1cbde51e2
Krishna Kumar <kkumar@exathink.com>
2018-07-24 15:02:46 -0500

    Inject intl into all charts. Use declared prop names for activityProfileBarChart in the signature rather than an opaque props [ #159282419 (https://www.pivotaltracker.com/story/show/159282419)]

--- src/app/dashboards/widgets/activity/ActivityLevel/components/activityProfileBarChart.js
+++ src/app/dashboards/widgets/activity/ActivityLevel/components/activityProfileBarChart.js
@@ -1,40 +1,24 @@
 import {tooltipHtml} from "../../../../../framework/viz/charts/index";
 import {ACTIVITY_LEVELS_REVERSED} from "../activityLevel";
-
 import {Chart} from "../../../../../framework/viz/charts/index";
-
 import {i18n} from "../../../../../i18n/index";
 
-Math.easeOutBounce = function (pos) {
-  if ((pos) < (1 / 2.75)) {
-    return (7.5625 * pos * pos);
-  }
-  if (pos < (2 / 2.75)) {
-    return (7.5625 * (pos -= (1.5 / 2.75)) * pos + 0.75);
-  }
-  if (pos < (2.5 / 2.75)) {
-    return (7.5625 * (pos -= (2.25 / 2.75)) * pos + 0.9375);
-  }
-  return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
-};
+
 
 export const ActivityProfileBarChart = Chart(
   {
-    chartUpdateProps: (props) => ({
-      model: props.model
+    chartUpdateProps: ({model}) => ({
+      model
     }),
     getConfig:
-      (props) => {
-        const totalsByActivityLevel = props.model.data.reduce(
+      ({model,  orientation, minimized, chartBackgroundColor,  intl}) => {
+        const totalsByActivityLevel = model.data.reduce(
           (totals, activitySummary) => {
             let level = activitySummary.activity_level.display_name;
             totals[level] = (totals[level] || 0) + 1;
             return totals
           },
           {});
-
-        const intl = props.intl;
-
 
         const series = ACTIVITY_LEVELS_REVERSED.map(activityLevel => ({
           name: i18n(intl, activityLevel.display_name),
@@ -46,13 +30,13 @@ export const ActivityProfileBarChart = Chart(
         }));
 
 
-        const title = `${props.orientation === 'vertical' ? i18n(intl, 'Profile') : i18n(intl, 'Activity Profile')} `;
+        const title = `${orientation === 'vertical' ? i18n(intl, 'Profile') : i18n(intl, 'Activity Profile')} `;
 
 
         return {
           chart: {
-            type: props.orientation === 'vertical' ? 'column' : 'bar',
-            backgroundColor: props.chartBackgroundColor,
+            type: orientation === 'vertical' ? 'column' : 'bar',
+            backgroundColor: chartBackgroundColor,
             spacing: [5,5,5,5]
           },
           plotOptions: {
@@ -65,7 +49,7 @@ export const ActivityProfileBarChart = Chart(
                 enabled: true,
                 align: 'center',
                 format: `<b>{series.name}</b><br>{percentage:.1f} %`,
-                rotation: props.orientation === 'vertical' ? 270 : 0,
+                rotation: orientation === 'vertical' ? 270 : 0,
                 filter: {
                   property: 'percentage',
                   operator: '>',
@@ -76,7 +60,7 @@ export const ActivityProfileBarChart = Chart(
           },
           title: {
             text: title,
-            align: props.orientation === 'vertical' ? 'center' : 'left'
+            align: orientation === 'vertical' ? 'center' : 'left'
           },
           tooltip: {
             useHTML: true,
@@ -85,7 +69,7 @@ export const ActivityProfileBarChart = Chart(
                 header: `${this.series.name}`,
                 body: [
                   [`${this.percentage.toFixed(0)}%`],
-                  (props.minimized ? [`${this.y}`] : [``])
+                  (minimized ? [`${this.y}`] : [``])
                 ]
               });
             },
@@ -104,16 +88,16 @@ export const ActivityProfileBarChart = Chart(
             title: {
               text: null
             },
-            max: props.model.data.length,
+            max: model.data.length,
             allowDecimals: false,
             gridLineWidth: 0,
-            plotLines: props.orientation === 'vertical' ? [{
-              value: props.model.data.length,
+            plotLines: orientation === 'vertical' ? [{
+              value: model.data.length,
               width: 2,
               color: 'grey',
               dashStyle: 'ShortDot',
               label: {
-                text: `${props.model.data.length}`,
+                text: `${model.data.length}`,
                 align: 'center',
                 textAlign: 'right',
               }
--- src/app/framework/viz/charts/chart.js
+++ src/app/framework/viz/charts/chart.js
@@ -1,75 +1,76 @@
 import React from "react";
 import {ChartWrapper} from "./index";
 import type {ChartConfigProvider} from "./chartConfigProvider";
+import {injectIntl} from "react-intl";
 
 export const Chart = (configProvider: ChartConfigProvider) => {
-  return class _ extends React.Component {
+  return injectIntl(class _ extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {};
-      this.chart = null;
-      this.eventHandler = null;
-    }
+      constructor(props) {
+        super(props);
+        this.state = {};
+        this.chart = null;
+        this.eventHandler = null;
+      }
 
-    setChart(chart) {
-      this.chart = chart;
-    }
+      setChart(chart) {
+        this.chart = chart;
+      }
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-      const chartUpdateProps = configProvider.chartUpdateProps(nextProps);
-      const propChange = Object.keys(chartUpdateProps).find(prop => chartUpdateProps[prop] !== prevState[prop]);
-      if (propChange) {
-        return {
-          ...chartUpdateProps,
-          config: prevState.config,
-          providerPropsUpdated: true
+      static getDerivedStateFromProps(nextProps, prevState) {
+        const chartUpdateProps = configProvider.chartUpdateProps(nextProps);
+        const propChange = Object.keys(chartUpdateProps).find(prop => chartUpdateProps[prop] !== prevState[prop]);
+        if (propChange) {
+          return {
+            ...chartUpdateProps,
+            config: prevState.config,
+            providerPropsUpdated: true
+          }
+        }
+        return null;
+      }
+
+      attachEvents(config) {
+        if (configProvider.eventHandler) {
+          this.eventHandler = new configProvider.eventHandler(config, this);
+        }
+        return config
+      }
+
+      updateConfig() {
+        if (this.state.providerPropsUpdated) {
+          const config = this.attachEvents(configProvider.getConfig(this.props));
+          this.setState(prevState => {
+            return {
+              ...{prevState},
+              config: config,
+              providerPropsUpdated: false
+            }
+          })
         }
       }
-      return null;
-    }
 
-    attachEvents(config){
-      if(configProvider.eventHandler) {
-        this.eventHandler = new configProvider.eventHandler(config, this);
+
+      componentDidMount() {
+        this.updateConfig();
       }
-      return config
-    }
 
-    updateConfig() {
-      if(this.state.providerPropsUpdated) {
-        const config = this.attachEvents(configProvider.getConfig(this.props));
-        this.setState( prevState => {
-          return {
-            ...{prevState},
-            config: config,
-            providerPropsUpdated: false
-          }
-        })
+      componentDidUpdate() {
+        this.updateConfig();
       }
-    }
 
 
+      onSelectionChange(selected) {
+        if (this.props.onSelectionChange && configProvider.mapPoints) {
+          this.props.onSelectionChange(configProvider.mapPoints(selected, this.props));
+        }
+      }
 
-    componentDidMount(){
-      this.updateConfig();
-    }
-
-    componentDidUpdate() {
-      this.updateConfig();
-    }
-
-
-
-    onSelectionChange(selected){
-      if(this.props.onSelectionChange && configProvider.mapPoints) {
-        this.props.onSelectionChange(configProvider.mapPoints(selected, this.props));
+      render() {
+        return (this.state.config ?
+          <ChartWrapper config={this.state.config} afterRender={this.setChart.bind(this)}/> : null);
       }
     }
-
-    render() {
-      return (this.state.config ? <ChartWrapper config={this.state.config} afterRender={this.setChart.bind(this)}/> : null);
-    }
-  }
+  )
 };
 
