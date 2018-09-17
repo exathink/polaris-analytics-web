9613d17fd8308187678661730652dd30d39fdfee
krishnaku <kkumar@exathink.com>
2018-09-17 10:41:51 -0500

    fixed issue with week 53 errors in cumulative commit graph [ #160422073 (https://www.pivotaltracker.com/story/show/160422073)]

--- src/app/dashboards/shared/views/cumulativeCommitCount/index.js
+++ src/app/dashboards/shared/views/cumulativeCommitCount/index.js
@@ -2,18 +2,21 @@ import {Chart, tooltipHtml} from "../../../../framework/viz/charts";
 import moment from 'moment';
 import {Colors} from "../../config";
 import {formatTerm} from "../../../../i18n";
-
+import {replicate} from "../../../../helpers/utility";
 
 function initSeries(cumulativeCommitCounts) {
   return cumulativeCommitCounts.map(cumulativeCommitCount => {
-    const week = `${cumulativeCommitCount.week}`.padStart(2, '0');
+    // week 53 (leap week) is not handled properly by moment. Coerce it back to week 52.
+    // this calc is not accurate strictly speaking, but it is good enough for
+    // most cases and wont matter much in the overall display of the chart.
+    // Can revisit if we need to.
+    const week = `${Math.min(cumulativeCommitCount.week, 52)}`.padStart(2, '0');
     const date  = moment(`${cumulativeCommitCount.year}W${week}`);
     return (
-      {
-        moment: date,
-        x: date.toDate(),
-        y: cumulativeCommitCount.cumulativeCommitCount
-      }
+      [
+        date.valueOf(),
+        cumulativeCommitCount.cumulativeCommitCount
+      ]
     )
   });
 }
@@ -52,7 +55,7 @@ export const CumulativeCommitCountChart = Chart({
           hideDelay: 50,
           formatter: function () {
             return tooltipHtml({
-              header: `Week of: ${this.point.moment.format("MMM Do, YYYY")}`,
+              header: `Week of: ${moment(this.x).format("MMM Do, YYYY")}`,
               body: [
                 [`${formatTerm(intl, 'Total Commits')}:`, `${intl.formatNumber(this.y)}`]
               ]
@@ -63,17 +66,13 @@ export const CumulativeCommitCountChart = Chart({
           key: 'cumulative commits',
           id: 'cumulative commits',
           name: 'Cumulative Commits',
-          color: context.color(),
-          data: series_data
+          data: series_data,
+          color: context.color()
         }],
-        plotOptions: {
-          series: {
-            animation: view !== 'detail'
-          }
-        },
         legend: {
           enabled: false
         },
+
       }
     }
 });
