b6030c699d8cde44718a74ce1010943cc4ca50c8
krishnaku <kkumar@exathink.com>
2018-10-21 12:25:15 -0500

    remove child element caching in DashboardLifecycleManager to allow organization udates to propagate through to children [ #161319922 (https://www.pivotaltracker.com/story/show/161319922)]

--- src/app/framework/viz/dashboard/dashboardLifecycleManager.js
+++ src/app/framework/viz/dashboard/dashboardLifecycleManager.js
@@ -1,20 +1,6 @@
 import React from "react";
 
 export class DashboardLifecycleManager extends React.Component {
-  constructor(props) {
-    super(props);
-    const {
-      render,
-      ...rest
-    } = props;
-
-    this.state={
-      // We memoize the render prop here (this is the child dashboard that is being shown)
-      // since we dont need to recreate this every time the render prop updates.
-      child: React.createElement(render, {...rest})
-    }
-  }
-
   componentDidMount() {
     const {
       onMount
@@ -26,6 +12,10 @@ export class DashboardLifecycleManager extends React.Component {
   }
 
   render () {
-    return this.state.child
+    const {
+      render,
+      ...rest
+    } = this.props;
+    return React.createElement(render, {...rest})
   }
 }
