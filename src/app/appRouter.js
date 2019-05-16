17306a3380a081dbdb313678d41e2ed8b2efe6ea
Krishna Kumar <kkumar@exathink.com>
2019-05-16 17:31:24 -0500

    Implement named export of AppRouter Component.
    [story=#166033219 subject=admin-user-create-account story_url=https://www.pivotaltracker.com/story/show/166033219
    ]

--- src/app/appRouter.js
+++ src/app/appRouter.js
@@ -1,6 +1,5 @@
+import React from 'react';
 import AppContext from './context';
-import {contextRouterFor} from "./framework/navigation/context/contextRouter";
+import {getContextRouterFor} from "./framework/navigation/context/contextRouter";
 
-
-const appRouter =  contextRouterFor(AppContext);
-export default appRouter;
+export const AppRouter = getContextRouterFor(AppContext)
