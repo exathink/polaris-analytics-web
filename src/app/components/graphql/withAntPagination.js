71e36b3160d028c070baacc405cf6aa8ede626cc
Krishna Kumar <kkumar@exathink.com>
2019-05-20 10:11:29 -0500

    Wire up withAntPagination to handle reload props properly. reload should be effective on initial load but not on paging.
    [story=#166033219 subject=admin-user-create-account story_url=https://www.pivotaltracker.com/story/show/166033219
    ]

--- src/app/components/graphql/withAntPagination.js
+++ src/app/components/graphql/withAntPagination.js
@@ -3,7 +3,8 @@ import React from "react";
 export const withAntPagination = (QueryWrappedPaginatedTable, pageSize = 5) => {
   return class PaginatedQuery extends React.Component {
     state = {
-      endCursor: null
+      endCursor: null,
+      reload: this.props.reload
     }
     onNewPage = (pagination) => {
       if (pagination.current > 1) {
@@ -13,14 +14,16 @@ export const withAntPagination = (QueryWrappedPaginatedTable, pageSize = 5) => {
         });
       } else {
         this.setState({
-          endCursor: null
+          endCursor: null,
+          reload: false
         })
       }
     };
 
     render() {
-      return <QueryWrappedPaginatedTable pageSize={pageSize} currentCursor={this.state.endCursor}
-                                         onNewPage={this.onNewPage}/>
+      const {reload, ...rest} = this.props;
+      return <QueryWrappedPaginatedTable paging={true} pageSize={pageSize} currentCursor={this.state.endCursor}
+                                         onNewPage={this.onNewPage} reload={this.state.reload} {...rest}/>
     }
 
   };
