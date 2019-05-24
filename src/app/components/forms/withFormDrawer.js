b52b1108d5975858c088787a38c96304c1112c2c
krishnaku <kkumar@exathink.com>
2019-05-24 09:10:24 -0500

    Change HOC to named function. Update tabIndex on cancel button so we can submit on Enter. [ story=#166170075 subject=create-account-with-owner story_url=https://www.pivotaltracker.com/story/show/166170075 ]

--- src/app/components/forms/withFormDrawer.js
+++ src/app/components/forms/withFormDrawer.js
@@ -5,52 +5,54 @@ import {withSubmissionHandler} from "./withSubmissionHandler";
 
 const {Option} = Select;
 
-export const withFormDrawer = (title, Form) => (
-  props => {
-    const {
-      submissionHandler: {
-        visible,
-        show,
-        hide,
-        onSubmit
-      }
-    } = props;
+export function withFormDrawer(title, Form) {
+  return (
+    props => {
+      const {
+        submissionHandler: {
+          visible,
+          show,
+          hide,
+          onSubmit
+        }
+      } = props;
 
-    return (
-      <React.Fragment>
-        <Button type="primary" onClick={show}>
-          <Icon type="plus"/> New account
-        </Button>
-        <Drawer
-          title={title}
-          width={720}
-          onClose={hide}
-          visible={visible}
-        >
-          <Form {...props} />
-          <div
-            style={{
-              position: 'absolute',
-              left: 0,
-              bottom: 0,
-              width: '100%',
-              borderTop: '1px solid #e9e9e9',
-              padding: '10px 16px',
-              background: '#fff',
-              textAlign: 'right',
-            }}
+      return (
+        <React.Fragment>
+          <Button type="primary" onClick={show}>
+            <Icon type="plus"/> New account
+          </Button>
+          <Drawer
+            title={title}
+            width={720}
+            onClose={hide}
+            visible={visible}
           >
-            <Button onClick={hide} style={{marginRight: 8}}>
-              Cancel
-            </Button>
-            <Button onClick={onSubmit} type="primary">
-              Submit
-            </Button>
-          </div>
-        </Drawer>
-      </React.Fragment>
-    )
-  }
-);
+            <Form {...props} />
+            <div
+              style={{
+                position: 'absolute',
+                left: 0,
+                bottom: 0,
+                width: '100%',
+                borderTop: '1px solid #e9e9e9',
+                padding: '10px 16px',
+                background: '#fff',
+                textAlign: 'right',
+              }}
+            >
+              <Button tabIndex="-1" onClick={hide} style={{marginRight: 8}}>
+                Cancel
+              </Button>
+              <Button onClick={onSubmit} type="primary">
+                Submit
+              </Button>
+            </div>
+          </Drawer>
+        </React.Fragment>
+      )
+    }
+  )
+}
 
 
