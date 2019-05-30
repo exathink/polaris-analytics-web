85ee5582b165b740606a6af81ce1180ec8e38074
Krishna Kumar <kkumar@exathink.com>
2019-05-30 11:08:47 -0500

    Handle top level dashboard redirects for non-admin users. If owner or the viewer has access to more than one org, redirect to account dashboard, otherwise redirect them to the organization dashboard for their organization.
    [story=#166353131 subject=profile-based-landing-page story_url=https://www.pivotaltracker.com/story/show/166353131]

--- src/app/dashboards/landing.js
+++ src/app/dashboards/landing.js
@@ -5,19 +5,36 @@ import {CardGrid} from "../components/cardGrid";
 import {Contexts} from "../meta";
 
 import Accounts from "./accounts/context";
-import OpenSource from "./oss/context";
+import Organizations from "./organizations/context";
 
-export default () => (
-  <CardGrid>
-    <NavCard
-      link={`${Accounts.url_for}`}
-      icon={Accounts.icon}
-      title={"My Account"}
-    />
-    <NavCard
-      link={`${OpenSource.url_for}`}
-      icon={OpenSource.icon}
-      title={"Open Source"}
-    />
-  </CardGrid>
+import OpenSource from "./oss/context";
+import {withViewerContext} from "../framework/viewer/viewerContext";
+import {Redirect} from 'react-router';
+import {url_for_instance} from "../framework/navigation/context/helpers";
+
+export default withViewerContext(
+  ({viewerContext}) => {
+    if (viewerContext.isAdmin()) {
+      return (
+        <CardGrid>
+          <NavCard
+            link={`${Accounts.url_for}`}
+            icon={Accounts.icon}
+            title={"My Account"}
+          />
+          <NavCard
+            link={`${OpenSource.url_for}`}
+            icon={OpenSource.icon}
+            title={"Open Source"}
+          />
+        </CardGrid>
+      );
+    } else {
+      const organizations = viewerContext.getViewerOrganizations();
+      return viewerContext.isAccountOwner() || organizations.length > 1 ?
+        <Redirect to={`${Accounts.url_for}`}/>
+        :
+        <Redirect to={`${url_for_instance(Organizations, organizations[0].name, organizations[0].key)}`}/>
+    }
+  }
 )
