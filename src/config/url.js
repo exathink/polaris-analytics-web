aee1864379108060d7e48ceffccfbcb06700f85e
Krishna Kumar <kkumar@exathink.com>
2019-06-11 08:31:46 -0500

    Add work_tracking_service to urls.
    [story=#166471396 subject=add-project-ui story_url=https://www.pivotaltracker.com/story/show/166471396
    ]

--- src/config/url.js
+++ src/config/url.js
@@ -1,5 +1,5 @@
 export const LOGIN_URL = `${process.env.REACT_APP_AUTH_SERVICE_URL}/?sso`;
 export const LOGOUT_URL = `${process.env.REACT_APP_AUTH_SERVICE_URL}/?slo`;
 export const POST_REGISTER_URL = `${process.env.REACT_APP_ADMIN_SERVICE_URL}/registration/`;
-export const GRAPHQL_ADMIN_URL = `${process.env.REACT_APP_ADMIN_SERVICE_URL}/graphql/`;
+export const GRAPHQL_WORK_TRACKING_URL = `${process.env.REACT_APP_WORK_TRACKING_SERVICE_URL}/graphql/`;
 export const GRAPHQL_ANALYTICS_URL = `${process.env.REACT_APP_ANALYTICS_SERVICE_URL}/graphql/`;
