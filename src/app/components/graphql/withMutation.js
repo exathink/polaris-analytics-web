0796de88626c9e7a2992e67c2f2e6924a07ac5c0
Krishna Kumar <kkumar@exathink.com>
2019-06-12 20:12:46 -0500

    Updated the withMutationHOC to take a mutation object with the {name, mutation, client, notification} structure. Added support for generically checking mutations for doing notifications.
    [story=#166471396 subject=add-project-ui story_url=https://www.pivotaltracker.com/story/show/166471396
    ]

--- src/app/components/graphql/withMutation.js
+++ src/app/components/graphql/withMutation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Mutation} from 'react-apollo';
 
-export const withMutation = (mutation, {name, client}) => {
+export const withMutation = ({name, mutation, client, notification}) => {
   return (Component) => {
     return props => (
       <Mutation
@@ -11,9 +11,17 @@ export const withMutation = (mutation, {name, client}) => {
         {
           (mutate, result) => {
             const injectedProp = {
+
               [`${name}Mutation`]: {
+                // we use the mutation name for the function for
+                // cleaner calling code
                 [name]: mutate,
-                [`${name}Result`]: result
+                // we pass the generic mutate name so that callers
+                // checking for the mutation without knowing which mutation it is can
+                // use this field.
+                mutate: mutate,
+                [`${name}Result`]: result,
+                notify: () => notification && result.data && notification(result.data)
               }
             }
             return <Component { ...injectedProp } {...props} />
