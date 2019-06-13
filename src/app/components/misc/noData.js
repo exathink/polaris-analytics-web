e76cb05b5faa7d3f83937a6ba8c6ed2d10e0be69
Krishna Kumar <kkumar@exathink.com>
2019-06-13 12:07:46 -0500

    Remove loading state from NoData
    [story=#166471396 subject=add-project-ui story_url=https://www.pivotaltracker.com/story/show/166471396
    ]

--- src/app/components/misc/noData.js
+++ src/app/components/misc/noData.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import {Loading} from "../graphql/loading";
 
 export const NoData = ({message, loading}) => (
   <div className={'no-data'}>
-    {
-      loading ? <Loading/> : <span>{message}</span>
-    }
+      <span>{message}</span>
   </div>
 )
