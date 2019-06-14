d80469e893713914068e6ad03770f30aeae09985
Krishna Kumar <kkumar@exathink.com>
2019-06-13 21:50:38 -0500

    Remove all traces of caching last mutation the mutation cache state. It causes all sorts of problems and the intended problem of notification can be solved by adding the onCompleted callback on Mutation in withMutation,
    [story=#166471396 subject=add-project-ui story_url=https://www.pivotaltracker.com/story/show/166471396
    ]

--- src/app/components/graphql/withMutationCache.js
+++ src/app/components/graphql/withMutationCache.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const withMutationCache = (Component) => {
   return class MutationCache extends React.Component {
-    state = {values: null, mutation: null}
+    state = {values: null}
 
     mutationWrapper(mutation, mutate) {
       return (values) => {
@@ -11,24 +11,11 @@ export const withMutationCache = (Component) => {
       }
     }
 
-    notify(mutations) {
-      if (this.state.mutation != null) {
-        for (let i = 0; i < mutations.length; i++) {
-          const mutation = mutations[i];
-          if (mutation.mutate === this.state.mutation.mutate) {
-            mutation.notify && mutation.notify()
-          }
-        }
-      }
-    }
-
     render() {
       return <Component
         mutationCache={{
           mutate: this.mutationWrapper.bind(this),
           lastSubmission: this.state.values,
-          lastMutation: this.state.mutation,
-          notify: this.notify.bind(this)
         }}
         {...this.props}
       />
