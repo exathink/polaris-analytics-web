2387c6ff0c9ed445b69d99d30214693a80b20088
Krishna Kumar <kkumar@exathink.com>
2019-07-09 19:52:27 -0500

    Add a button to trigger manual refetch of projects from the selectProjectsStep.
    [
    story=#167121389 subject=work-tracking-manual-remote-project-refresh story_url=https://www.pivotaltracker.com/story/show/167121389
    ]

--- src/app/dashboards/organizations/projects/manage/selectProjectsStep.js
+++ src/app/dashboards/organizations/projects/manage/selectProjectsStep.js
@@ -5,11 +5,30 @@ import {compose, Query} from "react-apollo";
 import {work_tracking_service} from "../../../../services/graphql/index";
 import gql from "graphql-tag";
 import {withSubmissionCache} from "../../../../components/forms/withSubmissionCache";
+import Button from "../../../../../components/uielements/button";
+import {withMutation} from "../../../../components/graphql/withMutation";
 
 import {Col, Form, Input, Row, Table} from "antd";
 import {createForm} from "../../../../components/forms/createForm";
+
+
 import {NoData} from "../../../../components/misc/noData";
 
+
+
+const REFETCH_PROJECTS_MUTATION = {
+  name: 'refetchProjects',
+  mutation: gql`
+      mutation refreshConnectorProjects($connectorKey: String!) {
+          refreshConnectorProjects(refreshConnectorProjectsInput:{
+              connectorKey: $connectorKey
+          }){
+              success
+          }
+      }
+  `,
+  client: work_tracking_service
+}
 
 export const CONNECTOR_WORK_ITEMS_SOURCES_QUERY = gql`
     query getConnectorWorkItemsSources($connectorKey: String!) {
@@ -43,63 +62,81 @@ export const REFETCH_CONNECTOR_WORK_ITEMS_SOURCES_QUERY = {
 };
 
 
-export class SelectProjectsStep extends React.Component {
+export const SelectProjectsStep =
+  compose(
+    withMutation(REFETCH_PROJECTS_MUTATION)
+  )(
+    class _SelectProjectsStep extends React.Component {
+      render() {
+        const {selectedConnector, selectedProjects, onProjectsSelected} = this.props;
+        const {refetchProjects, refetchProjectsResult} = this.props.refetchProjectsMutation;
 
-  render() {
-    const {selectedConnector, selectedProjects, onProjectsSelected} = this.props;
-
-    return (
-      <Query
-        client={work_tracking_service}
-        query={CONNECTOR_WORK_ITEMS_SOURCES_QUERY}
-        variables={{
-          connectorKey: selectedConnector.key
-        }}
-      >
-        {
-          ({loading, error, data}) => {
-            if (error) return null;
-            let workItemsSources = []
-            if (!loading) {
-              workItemsSources = data.workTrackingConnector.workItemsSources.edges.map(edge => edge.node);
-            }
-            return (
-              <div className={'select-projects'}>
-                <h3>Server: {selectedConnector.baseUrl}</h3>
-                {
-                  workItemsSources.length > 0 ?
-                    <React.Fragment>
-                      <span>{workItemsSources.length} projects available to import</span>
-                      <Table
-                        dataSource={workItemsSources}
-                        loading={loading}
-                        rowKey={record => record.key}
-                        rowSelection={{
-                          selectedRowKeys: selectedProjects.map(project => project.key),
-                          onChange: (selectedKeys, selectedRows) => onProjectsSelected(selectedRows),
-                        }}
-                        pagination={{
-                          total: workItemsSources.length,
-                          defaultPageSize: 5,
-                          hideOnSinglePage: true
-                        }}
-                      >
-                        <Table.Column title={"Name"} dataIndex={"name"} key={"name"}/>
-                        <Table.Column title={"Description"} dataIndex={"description"} key={"description"}/>
-                      </Table>
-                    </React.Fragment>
-                    :
-                    <NoData message={"No projects available to import"}/>
+        return (
+          <Query
+            client={work_tracking_service}
+            query={CONNECTOR_WORK_ITEMS_SOURCES_QUERY}
+            variables={{
+              connectorKey: selectedConnector.key
+            }}
+            pollInterval={5000}
+          >
+            {
+              ({loading, error, data}) => {
+                if (error) return null;
+                let workItemsSources = []
+                if (!loading) {
+                  workItemsSources = data.workTrackingConnector.workItemsSources.edges.map(edge => edge.node);
                 }
-              </div>
-            )
-          }
-        }
-      </Query>
+                return (
+                  <div className={'select-projects'}>
+                    <h3>Server: {selectedConnector.baseUrl}</h3>
+                    {
+                      workItemsSources.length > 0 ?
+                        <React.Fragment>
+                          <span>{workItemsSources.length} projects available to import</span>
+                          <Table
+                            dataSource={workItemsSources}
+                            loading={loading}
+                            rowKey={record => record.key}
+                            rowSelection={{
+                              selectedRowKeys: selectedProjects.map(project => project.key),
+                              onChange: (selectedKeys, selectedRows) => onProjectsSelected(selectedRows),
+                            }}
+                            pagination={{
+                              total: workItemsSources.length,
+                              defaultPageSize: 5,
+                              hideOnSinglePage: true
+                            }}
+                          >
+                            <Table.Column title={"Name"} dataIndex={"name"} key={"name"}/>
+                            <Table.Column title={"Description"} dataIndex={"description"} key={"description"}/>
+                          </Table>
+                        </React.Fragment>
+                        :
+                        <NoData message={"No projects imported"}/>
+                    }
+                    <Button
+                      type={'primary'}
+                      onClick={
+                        () => refetchProjects({
+                          variables: {
+                            connectorKey: selectedConnector.key
+                          }
+                        })}
+                      loading={refetchProjectsResult.loading}
+                    >
+                      Refresh Projects
+                    </Button>
+                  </div>
+                )
+              }
+            }
+          </Query>
 
-    )
-  }
-}
+        )
+      }
+    }
+  )
 
 
 
