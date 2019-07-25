e4a5015278b697c43e0c336718c63caac8133774
omelendrez <omar.melendrez@gmail.com>
2019-07-25 12:21:04 -0300

    Implement repositories table and add it to dashboard [story=#167432326 subject=vcs_onboarding_workflow_scaffold story_url=https://www.pivotaltracker.com/story/show/167432326]

--- src/app/dashboards/organizations/repositories/dashboard.js
+++ src/app/dashboards/organizations/repositories/dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Icon} from "antd";
+
 import {Dashboard, DashboardRow, DashboardWidget} from '../../../framework/viz/dashboard';
 import {Contexts} from "../../../meta/contexts";
 import {DimensionMostActiveChildrenWidget} from "../../shared/widgets/accountHierarchy";
 import {ChildDimensionActivityProfileWidget} from "../../shared/views/activityProfile";
 import {OrganizationDashboard} from "../organizationDashboard";
+import {RepositoriesTableWidget} from "./manage/repositoriesTable";
+
 import Repositories from "../../repositories/context";
+import Button from "../../../../components/uielements/button";
 
 const dashboard_id = 'dashboards.activity.organization.instance';
 
@@ -16,10 +21,30 @@ export default () => (
       <Dashboard
         dashboard={`${dashboard_id}`}
       >
-        <DashboardRow h='22%' title={Contexts.repositories.display()}>
+          <DashboardRow h={"70%"}>
+            <DashboardWidget
+              w={1}
+              name="repositories"
+              title={"Repositories"}
+              controls={[
+                () =>
+                  <Button type="primary" onClick={()=>context.go('.', 'new')}>
+                    <Icon type="plus"/> New Repository
+                  </Button>
+              ]}
+              render={
+                () =>
+                  <RepositoriesTableWidget
+                    organizationKey={organization.key}
+                  />
+              }
+              showDetail={false}
+            />
+          </DashboardRow>
+        <DashboardRow h='22%'>
           <DashboardWidget
             w={1 / 2}
-            name="project-activity-levels"
+            name="respository-activity-levels"
             render={
               ({view}) =>
                 <ChildDimensionActivityProfileWidget
---
+++ src/app/dashboards/organizations/repositories/manage/repositoriesTable.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Query} from 'react-apollo';
+import gql from 'graphql-tag';
+
+import {Table} from 'antd';
+
+import {withViewerContext} from "../../../../framework/viewer/viewerContext";
+import {analytics_service} from '../../../../services/graphql';
+import {withAntPagination} from "../../../../components/graphql/withAntPagination";
+
+
+const {Column} = Table;
+
+const RepositoriesPaginatedTable = ({organizationKey, paging, pageSize, currentCursor, onNewPage, newData}) => (
+  <Query
+    client={analytics_service}
+    query={
+      gql`
+      query organizationRepositories($organizationKey: String!, $pageSize: Int!, $endCursor: String) {
+        organization(key: $organizationKey) {
+            id
+            repositories (first: $pageSize, after: $endCursor){
+                  count
+                  edges {
+                      node {
+                          id
+                          name
+                          key
+                      }
+                  }
+              }
+         }
+        }
+  `
+    }
+    variables={{
+      organizationKey: organizationKey,
+      pageSize: pageSize,
+      endCursor: currentCursor
+    }}
+    fetchPolicy={'cache-and-network'}
+
+  >
+    {
+      ({loading, error, data}) => {
+        if (error) return null;
+        let tableData = [];
+        let totalItems = 0;
+        if (!loading) {
+          tableData = data.organization.repositories.edges.map(edge => edge.node);
+          totalItems = data.organization.repositories.count;
+        }
+        return (
+          <Table
+            dataSource={tableData}
+            loading={loading}
+            rowKey={record => record.id}
+            pagination={{
+              total: totalItems,
+              defaultPageSize: pageSize,
+              hideOnSinglePage: true
+
+            }}
+            onChange={onNewPage}
+          >
+            <Column title={"Name"} dataIndex={"name"} key={"name"}/>
+          </Table>
+        )
+      }
+    }
+  </Query>
+)
+
+
+export const RepositoriesTableWidget = withViewerContext(withAntPagination(RepositoriesPaginatedTable))
