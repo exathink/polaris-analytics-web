e0f2926e6abaa12619a6d5d070d5b06205b0b5ec
omelendrez <omar.melendrez@gmail.com>
2019-07-26 14:02:15 -0300

    “Implement add repository” [story=#167432326 subject=vcs_onboarding_workflow_scaffold story_url=https://www.pivotaltracker.com/story/show/167432326]

---
+++ src/app/dashboards/organizations/repositories/manage/addRepository.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Dashboard, DashboardRow, DashboardWidget} from "../../../../framework/viz/dashboard";
+import {OrganizationDashboard} from "../../organizationDashboard";
+import {AddRepositoryWorkflow} from './addRepositoryWorkflow';
+import {withViewerContext} from "../../../../framework/viewer/viewerContext";
+
+const dashboard_id = 'dashboards.add_projects.organization.instance';
+
+export default withViewerContext(
+  (
+    {
+      viewerContext
+    }
+  ) => (
+  <OrganizationDashboard
+    render={
+      ({organization, context}) => (
+        <Dashboard
+          dashboard={`${dashboard_id}`}
+        >
+          <DashboardRow h={"95%"}>
+            <DashboardWidget
+              w={1}
+              render={() => <AddRepositoryWorkflow organization={organization} viewerContext={viewerContext}/>}
+            />
+          </DashboardRow>
+        </Dashboard>
+      )}
+  />
+))
