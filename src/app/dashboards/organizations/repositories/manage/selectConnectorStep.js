8fe2719ad722d4defb485306a7b31165ef1c8a60
omelendrez <omar.melendrez@gmail.com>
2019-07-26 14:05:39 -0300

    “Add select connector step” [story=#167432326 subject=vcs_onboarding_workflow_scaffold story_url=https://www.pivotaltracker.com/story/show/167432326]

---
+++ src/app/dashboards/organizations/repositories/manage/selectConnectorStep.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Radio } from "antd";
+
+import { SelectConnectorWidget } from "../../../../components/workflow/workTracking/selectConnectorWidget";
+
+
+export const SelectConnectorType = ({ connectorType, onChange }) => (
+  <Radio.Group
+    name="connectorType"
+    value={connectorType}
+    buttonStyle={"solid"}
+    onChange={onChange}
+  >
+    <Radio.Button value={'jira'}>Jira</Radio.Button>
+    <Radio.Button value={'pivotal'}>Pivotal Tracker</Radio.Button>
+    <Radio.Button value={'github'}>Github</Radio.Button>
+  </Radio.Group>
+);
+
+
+function getWorkTrackingType(connector) {
+  const {connectorType} = connector;
+  if(connectorType) {
+    switch(connectorType) {
+      case 'atlassian':
+        return 'jira'; // This could be replaced with productType, but right now the back end does not return this.
+      default:
+        return connectorType
+    }
+  }
+
+}
+export class SelectConnectorStep extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      connectorType: getWorkTrackingType(props.selectedConnector) || 'jira'
+    }
+  }
+
+  onConnectorTypeChanged(e) {
+    this.setState({
+      connectorType: e.target.value
+    })
+  }
+
+
+  render() {
+    return (
+      <div className={'select-connector'}>
+        <SelectConnectorType
+          connectorType={this.state.connectorType}
+          onChange={this.onConnectorTypeChanged.bind(this)}
+        />
+
+        <SelectConnectorWidget
+          connectorType={this.state.connectorType}
+          onConnectorSelected={this.props.onConnectorSelected}
+          organizationKey={this.props.organizationKey}
+        />
+      </div>
+    )
+  }
+}
