eccac38d45fc4e5d325ff3acbfbcec2a22cac5fe
omelendrez <omar.melendrez@gmail.com>
2019-08-08 13:03:41 -0300

    Reduce toolbar to 56px and con sizes for import status [story=#167707361 subject=styling_improvements_import_projects_and_repos story_url=https://www.pivotaltracker.com/story/show/167707361]

--- src/app/containers/topbar/topbar.js
+++ src/app/containers/topbar/topbar.js
@@ -22,7 +22,7 @@ class Topbar extends Component {
       background: customizedTheme.backgroundColor,
       position: 'fixed',
       width: '100%',
-      height: 64
+      height: 56
     };
     return (
       <TopbarWrapper>
