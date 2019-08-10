60cfaabeef56ed550ad55aa73872d67ddc376440
Krishna Kumar <kkumar@exathink.com>
2019-08-10 18:33:53 -0500

    Implemented Project Import Status UX enhancements.
    [story=#167707729 subject=project-import-status-ux story_url=https://www.pivotaltracker.com/story/show/167707729
    
    ]

--- src/app/dashboards/organizations/projects/manage/showImportStateStep.js
+++ src/app/dashboards/organizations/projects/manage/showImportStateStep.js
@@ -5,10 +5,27 @@ import {work_tracking_service} from "../../../../services/graphql/index";
 import gql from "graphql-tag";
 import {withSubmissionCache} from "../../../../components/forms/withSubmissionCache";
 
-import {Col, Form, Input, Row, Table} from "antd";
+import {Col, Form, Input, Row, Table, Spin, Progress} from "antd";
 import {createForm} from "../../../../components/forms/createForm";
 import {NoData} from "../../../../components/misc/noData";
 
+const importStateDisplayMap = {
+    ready: 'queued',
+    importing: 'importing',
+    auto_update: 'complete'
+}
+
+const importStateSortOrder = {
+    queued: 2,
+    complete: 1,
+    importing: 0
+}
+
+const importStateClassIndex = {
+    queued: 'status-queued',
+    importing: 'status-importing',
+    complete: 'status-complete'
+}
 
 const SHOW_IMPORT_STATE_QUERY = gql`
     query showImportState($connectorKey:String!, $projectKeys: [String]!){
@@ -55,18 +72,34 @@ export const ShowImportStateStep = (
                   key: edge.node.key,
                   name: edge.node.name,
                   importDays: edge.node.initialImportDays,
-                  importState: edge.node.importState !== 'auto_update' ? 'Importing' : 'Complete',
+                  importState: importStateDisplayMap[edge.node.importState],
                   workItemCount: edge.node.workItemCount
                 })
+              ).sort(
+                (a,b) =>
+                  importStateSortOrder[a] - importStateSortOrder[b]
               );
             }
+            const numImported=workItemsSources.filter(
+              source => source.importState === 'complete'
+            ).length
+
             return (
               <div className={'show-import-state'}>
+                  <Progress
+                      type={'circle'}
+                      percent={Math.ceil((numImported / workItemsSources.length) * 100)}
+                      format={
+                        () => `${numImported}/${workItemsSources.length}`
+                      }
+                    />
                   <Table
                     dataSource={workItemsSources}
                     loading={loading}
                     rowKey={record => record.key}
-
+                    rowClassName={
+                    (record, index) => importStateClassIndex[record.importState]
+                    }
                     pagination={{
                       total: workItemsSources.length,
                       defaultPageSize: 5,
@@ -76,7 +109,22 @@ export const ShowImportStateStep = (
                     <Table.Column title={"Remote Project"} dataIndex={"name"} key={"name"}/>
                     <Table.Column title={"Import Days"} dataIndex={"importDays"} key={"importDays"}/>
                     <Table.Column title={"Work Items Imported"} dataIndex={"workItemCount"} key={"workItemCount"}/>
-                    <Table.Column title={"Import Status"} dataIndex={"importState"} key={"importState"}/>
+                    <Table.Column
+                      title={"Import Status"}
+                      dataIndex={"importState"}
+                      key={"importState"}
+                      render={
+                          importState =>
+                            importState !== 'complete'?
+                                <Spin tip={importState}/>
+                                :
+                                <Progress
+                                  type='circle'
+                                  width={30}
+                                  percent={100}
+                                />
+                      }
+                    />
                   </Table>
               </div>
             );
