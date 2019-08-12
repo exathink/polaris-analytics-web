884ef2fd8f35560689c7272068a2955555e07af2
omelendrez <omar.melendrez@gmail.com>
2019-08-12 13:07:52 -0300

    Move table style into its own styled component [story=#167707361 subject=styling_improvements_import_projects_and_repos story_url=https://www.pivotaltracker.com/story/show/167707361]

--- src/components/uielements/table.js
+++ src/components/uielements/table.js
@@ -1,2 +1,9 @@
-import { Table } from 'antd';
-export default Table;
+import {Table} from 'antd';
+import {CompactStyle} from './styles/table.style';
+import WithDirection from '../../config/withDirection';
+
+const isoTable = WithDirection(Table);
+const CompactTable = CompactStyle(Table);
+const isoCompactTable = WithDirection(CompactTable);
+
+export {Table, CompactTable, isoTable, isoCompactTable};
