30a1a8ef4732731396ec2738679c56143814e001
Krishna Kumar <kkumar@exathink.com>
2019-08-13 15:57:58 -0500

    NoDataLanding views for org projects and repositories [story=#167837457 subject=org_project_repo_initail_landing_ux story_url=https://www.pivotaltracker.com/story/show/167837457]

--- src/app/dashboards/organizations/repositories/dashboard.js
+++ src/app/dashboards/organizations/repositories/dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Icon} from "antd";
+import {Col, Icon, Row} from "antd";
 import {Dashboard, DashboardRow, DashboardWidget} from '../../../framework/viz/dashboard';
 import {Contexts} from "../../../meta/contexts";
 import {DimensionMostActiveChildrenWidget} from "../../shared/widgets/accountHierarchy";
@@ -9,83 +9,103 @@ import {RepositoriesTableWidget} from "./manage/repositoriesTable";
 
 import Repositories from "../../repositories/context";
 import Button from "../../../../components/uielements/button";
+import {IconCard} from "../../../components/cards/iconCard";
+import Projects from "../../projects/context";
 
 const dashboard_id = 'dashboards.activity.organization.instance';
 
 export default () => (
   <OrganizationDashboard
-    pollInterval={60*1000}
+    pollInterval={60 * 1000}
     render={(
       {organization, context, lastRefresh}) => (
-      <Dashboard
-        dashboard={`${dashboard_id}`}
-      >
-        <DashboardRow
-          h='22%'
-          title={"Repositories"}
-          controls={[
-              () =>
-                <Button type="primary" onClick={()=>context.go('.', 'new')}>
-                  <Icon type="download"/> Import Repositories
-                </Button>
-            ]}
-        >
-          <DashboardWidget
-            w={1 / 2}
-            name="respository-activity-levels"
-            render={
-              ({view}) =>
-                <ChildDimensionActivityProfileWidget
-                  dimension={'organization'}
-                  instanceKey={organization.key}
-                  childDimension={'repositories'}
-                  context={context}
-                  childContext={Repositories}
-                  enableDrillDown={true}
-                  view={view}
-                  referenceDate={lastRefresh}
-                  referenceCount={organization.commitCount}
-                />
-            }
-            showDetail={true}
-          />
-          <DashboardWidget
-            w={1 / 2}
-            name="most-active-repositories"
-            render={
-              ({view}) =>
-                <DimensionMostActiveChildrenWidget
-                  dimension={'organization'}
-                  instanceKey={organization.key}
-                  childConnection={'recentlyActiveRepositories'}
-                  context={context}
-                  childContext={Repositories}
-                  top={10}
-                  latestCommit={organization.latestCommit}
-
-                  days={1}
-                  view={view}
-                />
-            }
-            showDetail={true}
-          />
-        </DashboardRow>
-        <DashboardRow
-            h={"68%"}
+        organization.repositoryCount > 0 ?
+          <Dashboard
+            dashboard={`${dashboard_id}`}
           >
-            <DashboardWidget
-              w={1}
-              name="repositories"
-              render={
+            <DashboardRow
+              h='22%'
+              title={"Repositories"}
+              controls={[
                 () =>
-                  <RepositoriesTableWidget
-                    organizationKey={organization.key}
-                  />
-              }
-              showDetail={false}
-            />
-          </DashboardRow>
-      </Dashboard>
+                  <Button type="primary" onClick={() => context.go('.', 'new')}>
+                    <Icon type="download"/> Import Repositories
+                  </Button>
+              ]}
+            >
+              <DashboardWidget
+                w={1 / 2}
+                name="respository-activity-levels"
+                render={
+                  ({view}) =>
+                    <ChildDimensionActivityProfileWidget
+                      dimension={'organization'}
+                      instanceKey={organization.key}
+                      childDimension={'repositories'}
+                      context={context}
+                      childContext={Repositories}
+                      enableDrillDown={true}
+                      view={view}
+                      referenceDate={lastRefresh}
+                      referenceCount={organization.commitCount}
+                    />
+                }
+                showDetail={true}
+              />
+              <DashboardWidget
+                w={1 / 2}
+                name="most-active-repositories"
+                render={
+                  ({view}) =>
+                    <DimensionMostActiveChildrenWidget
+                      dimension={'organization'}
+                      instanceKey={organization.key}
+                      childConnection={'recentlyActiveRepositories'}
+                      context={context}
+                      childContext={Repositories}
+                      top={10}
+                      latestCommit={organization.latestCommit}
+
+                      days={1}
+                      view={view}
+                    />
+                }
+                showDetail={true}
+              />
+            </DashboardRow>
+            <DashboardRow
+              h={"68%"}
+            >
+              <DashboardWidget
+                w={1}
+                name="repositories"
+                render={
+                  () =>
+                    <RepositoriesTableWidget
+                      organizationKey={organization.key}
+                    />
+                }
+                showDetail={false}
+              />
+            </DashboardRow>
+          </Dashboard>
+          :
+          <NoRepositoriesView context={context}/>
     )}
   />
+);
+
+const NoRepositoriesView = ({context}) => (
+  <div className={'no-data-landing'}>
+    <div style={{padding: '30px'}}>
+      <Row gutter={16}>
+        <Col offset={10} span={24}>
+          <a onClick={() => context.go('.', 'new')}>
+            <IconCard icon={Repositories.icon} title={"Import Repositories"}/>
+          </a>
+        </Col>
+      </Row>
+    </div>
+
+  </div>
 );
