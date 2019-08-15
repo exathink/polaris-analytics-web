048d880ab70db57b9fe5835582a310e4665e4580
Krishna Kumar <kkumar@exathink.com>
2019-08-15 10:40:48 -0500

    Export Column as an attribute for CompactTable.
    [story=#167837457 subject=org_project_repo_initail_landing_ux story_url=https://www.pivotaltracker.com/story/show/167837457]

--- src/app/components/tables/table.js
+++ src/app/components/tables/table.js
@@ -2,5 +2,6 @@ import {Table} from 'antd';
 import {CompactStyle} from './table.style';
 
 const CompactTable = CompactStyle(Table);
+CompactTable.Column = Table.Column
 
 export {Table, CompactTable};
