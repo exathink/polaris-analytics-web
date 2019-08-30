35e77bdc41ea1f542788a200846ffe8dc1898b75
Krishna Kumar <kkumar@exathink.com>
2019-08-30 16:41:17 -0500

    Remove Atlassian as a client side connector type. Get this value from selectedConnector type instead. [ story=#167824245 subject=edit-connector story_url=https://www.pivotaltracker.com/story/show/167824245
    ]

--- src/app/components/workflow/connectors/utility.js
+++ src/app/components/workflow/connectors/utility.js
@@ -1,5 +1,4 @@
 const connectorTypeDisplayName = {
-  'atlassian': 'Jira', // there is a Jira record in database with connectorType 'atlassian' instead of jira.
   'jira': 'Jira',
   'pivotal': 'Pivotal Tracker',
   'github' : 'Github',
