0e9767bc7efb3cc4f92c91ce6a74a7864a0d3c01
Krishna Kumar <kkumar@exathink.com>
2020-04-03 18:57:46 -0500

    Add pick utility function.
    [story=PO-92 subject=cycle-metric-details story_url=https://urjuna.atlassian.net/browse/PO-92
    
    ]

--- src/app/helpers/utility.js
+++ src/app/helpers/utility.js
@@ -215,3 +215,10 @@ export function percentileToText(percentile) {
   }
 
 }
+
+export function pick(o, ...fields) {
+    return fields.reduce((a, x) => {
+        if(o.hasOwnProperty(x)) a[x] = o[x];
+        return a;
+    }, {});
+}
