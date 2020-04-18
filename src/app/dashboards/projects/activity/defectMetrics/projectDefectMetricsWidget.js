f0dfca2129b07be7f322bc58168cfb597b84c293
Krishna Kumar <kkumar@exathink.com>
2020-04-18 10:01:32 -0500

    Refactor projectDefectMetricsSummary into a hook.
    [story=PO-109 subject=defect-cycle-metrics story_url=https://urjuna.atlassian.net/browse/PO-109
    ]

--- src/app/dashboards/projects/activity/defectMetrics/projectDefectMetricsWidget.js
+++ src/app/dashboards/projects/activity/defectMetrics/projectDefectMetricsWidget.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import {useQuery} from '@apollo/react-hooks';
-import gql from 'graphql-tag';
 import {Loading} from "../../../../components/graphql/loading";
-import {analytics_service} from "../../../../services/graphql";
 
+import {useQueryProjectDefectMetricsSummary} from "../hooks/useQueryProjectDefectsSummary";
 import {ProjectDefectMetricsSummaryView} from "./projectDefectMetricsSummaryView";
 
 
@@ -18,60 +16,20 @@ export const ProjectDefectMetricsWidget = (
     pollInterval
   }) => {
 
-  const {loading, error, data} = useQuery(
-    gql`
-           query projectDefectMetricsSummary($key: String!, $days: Int!, $targetPercentile: Float!, $referenceString: String!) {
-            project(key: $key, 
-                    interfaces: [AggregateCycleMetrics, WorkItemStateTypeCounts], 
-                    defectsOnly: true,
-                    cycleMetricsTargetPercentile: $targetPercentile,
-                    closedWithinDays: $days,
-                    referenceString: $referenceString, 
-                    ) {
-                
-                ... on AggregateCycleMetrics {
-                    minLeadTime
-                    avgLeadTime
-                    maxLeadTime
-                    percentileLeadTime
-                    minCycleTime
-                    avgCycleTime
-                    maxCycleTime
-                    percentileCycleTime
-                    workItemsInScope
-                    targetPercentile
-                }
-                ... on WorkItemStateTypeCounts {
-                  workItemStateTypeCounts {
-                    backlog
-                  }
-                }
-            }
-           }
-      `, {
-
-      service: analytics_service,
-      variables: {
-        key: instanceKey,
-        days: days,
-        targetPercentile: targetPercentile,
-        referenceString: latestWorkItemEvent
-
-      },
-      errorPolicy: "all",
-      pollInterval: pollInterval || analytics_service.defaultPollInterval()
-    }
+  const {loading, error, data: projectDefectMetrics} = useQueryProjectDefectMetricsSummary(
+    {instanceKey, days, targetPercentile, ...{referenceString: latestWorkItemEvent}}
   );
+
 
   if (loading) return <Loading/>;
   if (error) return null;
-  const {...projectDefectMetrics} = data['project'];
+  const {...defectMetrics} = projectDefectMetrics['project'];
   return (
     <ProjectDefectMetricsSummaryView
       instanceKey={instanceKey}
       showAll={showAll}
       stateMappingIndex={stateMappingIndex}
-      {...projectDefectMetrics}
+      {...defectMetrics}
     />
   )
 }
---
+++ src/app/dashboards/projects/activity/hooks/useQueryProjectDefectsSummary.js
@@ -0,0 +1,50 @@
+import {useQuery} from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import {analytics_service} from "../../../../services/graphql";
+
+export function useQueryProjectDefectMetricsSummary({instanceKey, days, targetPercentile, referenceString}) {
+  return useQuery(
+    gql`
+           query projectDefectMetricsSummary($key: String!, $days: Int!, $targetPercentile: Float!, $referenceString: String!) {
+            project(key: $key, 
+                    interfaces: [AggregateCycleMetrics, WorkItemStateTypeCounts], 
+                    defectsOnly: true,
+                    cycleMetricsTargetPercentile: $targetPercentile,
+                    closedWithinDays: $days,
+                    referenceString: $referenceString, 
+                    ) {
+                
+                ... on AggregateCycleMetrics {
+                    minLeadTime
+                    avgLeadTime
+                    maxLeadTime
+                    percentileLeadTime
+                    minCycleTime
+                    avgCycleTime
+                    maxCycleTime
+                    percentileCycleTime
+                    workItemsInScope
+                    targetPercentile
+                }
+                ... on WorkItemStateTypeCounts {
+                  workItemStateTypeCounts {
+                    backlog
+                  }
+                }
+            }
+           }
+      `, {
+
+      service: analytics_service,
+      variables: {
+        key: instanceKey,
+        days: days,
+        targetPercentile: targetPercentile,
+        referenceString: referenceString
+
+      },
+      errorPolicy: "all",
+      pollInterval: analytics_service.defaultPollInterval()
+    }
+  )
+}
