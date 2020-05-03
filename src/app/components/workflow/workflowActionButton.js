d9f183db403d6d7bba7cadf5a665a23fcdcaef9f
Krishna Kumar <kkumar@exathink.com>
2020-05-03 14:25:43 -0500

    Go back to using medium square buttons for step actions.
    [story=PO-143 subject=import_work_flow_poor_ux_bug story_url=https://urjuna.atlassian.net/browse/PO-142
    ]

--- src/app/components/workflow/workflowActionButton.js
+++ src/app/components/workflow/workflowActionButton.js
@@ -4,9 +4,7 @@ import "./steps.css";
 
 export const WorkflowActionButton = ({children, disabled,  ...rest}) => (
   <Button
-    size={'small'}
     type="primary"
-    shape={'circle'}
     style={!disabled ? {backgroundColor: '#7824b5', borderColor: '#7824b5'} : {}}
     disabled={disabled}
     {...rest}>
