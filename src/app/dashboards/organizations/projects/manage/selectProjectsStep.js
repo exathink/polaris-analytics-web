ce0bc10159c2d6d62b7b0d9839f8b5ca86603f1d
Krishna Kumar <kkumar@exathink.com>
2020-05-03 14:30:15 -0500

    Move pagination control for the projects table to the top.
    [story=PO-143 subject=import_work_flow_poor_ux_bug story_url=https://urjuna.atlassian.net/browse/PO-142
    ]

--- src/app/dashboards/organizations/projects/manage/selectProjectsStep.js
+++ src/app/dashboards/organizations/projects/manage/selectProjectsStep.js
@@ -10,7 +10,7 @@ import {EDIT_CONNECTOR, TEST_CONNECTOR} from "../../../../components/workflow/co
 import {Table} from "../../../../components/tables";
 import {useSearch, useSelectionHandler} from "../../../../components/tables/hooks";
 import {NoData} from "../../../../components/misc/noData";
-import {compose, capitalizeFirstLetter, lexicographic} from "../../../../helpers/utility";
+import {compose, lexicographic} from "../../../../helpers/utility";
 import {EditConnectorFormButton} from "../../../../components/workflow/connectors/editConnectorFormButton";
 import {withSubmissionCache} from "../../../../components/forms/withSubmissionCache";
 
@@ -99,7 +99,8 @@ const SelectProjectsTable = ({loading, dataSource, selectedProjects, onProjectsS
       pagination={{
         showTotal: total => `${total} Projects`,
         defaultPageSize: 10,
-        hideOnSinglePage: true
+        hideOnSinglePage: true,
+        position: 'top'
       }}
       rowSelection={useSelectionHandler(onProjectsSelected, selectedProjects)}
     >
