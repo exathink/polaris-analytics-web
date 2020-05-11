816aa5e85d9ed7f3d1293063c9e40ac3f76978b4
Krishna Kumar <kkumar@exathink.com>
2020-05-11 16:25:22 -0500

    Add controls and state to allow grouping by state or work item type.
    [story=PO-149 subject=pipeline_groups  story_url=https://urjuna.atlassian.net/browse/PO-149
    
    ]

--- src/app/dashboards/projects/activity/pipeline/projectPipelineStateDetailsView.js
+++ src/app/dashboards/projects/activity/pipeline/projectPipelineStateDetailsView.js
@@ -2,8 +2,10 @@ import React, {useState} from 'react';
 import {withNavigationContext} from "../../../../framework/navigation/components/withNavigationContext";
 import {PipelineStateDistributionChart} from "./pipelineStateDistributionChart";
 import {VizItem, VizRow} from "../../../shared/containers/layout";
-import {WorkItemStateTypeDisplayName, WorkItemStateTypeSortOrder} from "../../../shared/config";
+import {WorkItemStateTypeColor, WorkItemStateTypeDisplayName, WorkItemStateTypeSortOrder} from "../../../shared/config";
 import {GroupingSelector} from "../../../shared/components/groupingSelector/groupingSelector";
+import {Flex} from 'reflexbox';
+import {capitalizeFirstLetter} from "../../../../helpers/utility";
 
 const PipelineStateDetailsView = (
   {
@@ -23,34 +25,55 @@ const PipelineStateDetailsView = (
       },
       {}
     );
-    const groupings = Object.keys(workItemsByStateType).sort(
+    const stateTypes = Object.keys(workItemsByStateType).sort(
       (stateTypeA, stateTypeB) => WorkItemStateTypeSortOrder[stateTypeA] - WorkItemStateTypeSortOrder[stateTypeB]
     )
 
-    const [selectedStateType, setSelectedStateType] = useState(groupings[0]);
+    const [selectedStateType, setSelectedStateType] = useState(stateTypes[0]);
+    const [selectedGrouping, setSelectedGrouping] = useState('state');
 
     if (selectedStateType != null) {
       return (
         <VizRow h={1}>
           <VizItem w={1}>
-          <GroupingSelector
-            label={"Phase"}
-            groupings={
-              groupings.map(
-                grouping => ({
-                  key: grouping,
-                  display: WorkItemStateTypeDisplayName[grouping]
-                })
-              )
-            }
-            initialValue={selectedStateType}
-            onGroupingChanged={setSelectedStateType}
-          />
-          < PipelineStateDistributionChart
-            stateType={selectedStateType}
-            workItems={workItemsByStateType[selectedStateType]}
-            projectCycleMetrics={projectCycleMetrics}
-          />
+            <Flex w={0.95} justify='space-between' >
+              <GroupingSelector
+                label={"Phase"}
+                groupings={
+                  stateTypes.map(
+                    stateType => ({
+                      key: stateType,
+                      display: WorkItemStateTypeDisplayName[stateType],
+                      style: {
+                        backgroundColor: WorkItemStateTypeColor[stateType],
+                        color: stateType === selectedStateType ? "#ffffff" : "#d4d4d4"
+                      }
+                    })
+                  )
+                }
+                initialValue={selectedStateType}
+                onGroupingChanged={setSelectedStateType}
+              />
+              <GroupingSelector
+                label={"Group Work Items By"}
+                groupings={
+                  ['state', 'type'].map(
+                    grouping => ({
+                      key: grouping,
+                      display: capitalizeFirstLetter(grouping)
+                    })
+                  )
+                }
+                initialValue={selectedGrouping}
+                onGroupingChanged={setSelectedGrouping}
+              />
+            </Flex>
+            < PipelineStateDistributionChart
+              stateType={selectedStateType}
+              groupBy={selectedGrouping}
+              workItems={workItemsByStateType[selectedStateType]}
+              projectCycleMetrics={projectCycleMetrics}
+            />
           </VizItem>
         </VizRow>
       )
