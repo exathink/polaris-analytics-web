030e0555462af2c8b29174dec6850db43e3b0225
Krishna Kumar <kkumar@exathink.com>
2020-05-11 10:28:18 -0500

    Use grouping selectors to partition work items by state type in pipeline detail view.
    [story=PO-138 subject=pipeline_tabs story_url=https://urjuna.atlassian.net/browse/PO-138
    
    ]

--- src/app/dashboards/projects/activity/pipeline/projectPipelineStateDetailsView.js
+++ src/app/dashboards/projects/activity/pipeline/projectPipelineStateDetailsView.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {withNavigationContext} from "../../../../framework/navigation/components/withNavigationContext";
 import {PipelineStateDistributionChart} from "./pipelineStateDistributionChart";
-import {VizRow, VizItem} from "../../../shared/containers/layout";
+import {VizItem, VizRow} from "../../../shared/containers/layout";
+import {WorkItemStateTypeDisplayName, WorkItemStateTypeSortOrder} from "../../../shared/config";
+import {GroupingSelector} from "../../../shared/components/groupingSelector/groupingSelector";
 
-const PipelineStateDetailsView = ({
-  workItems,
-  projectCycleMetrics,
-  view
-}) => (
-  <VizRow h={1}>
-    <VizItem w={1}>
-      <PipelineStateDistributionChart
-        workItems={workItems}
-        projectCycleMetrics={projectCycleMetrics}
-      />
-    </VizItem>
-  </VizRow>
-);
+const PipelineStateDetailsView = (
+  {
+    workItems,
+    projectCycleMetrics,
+    view
+  }) => {
+
+    const workItemsByStateType = workItems.reduce(
+      (workItemsByStateType, workItem) => {
+        if (workItemsByStateType[workItem.stateType] != null) {
+          workItemsByStateType[workItem.stateType].push(workItem)
+        } else {
+          workItemsByStateType[workItem.stateType] = [workItem]
+        }
+        return workItemsByStateType
+      },
+      {}
+    );
+    const groupings = Object.keys(workItemsByStateType).sort(
+      (stateTypeA, stateTypeB) => WorkItemStateTypeSortOrder[stateTypeA] - WorkItemStateTypeSortOrder[stateTypeB]
+    )
+
+    const [selectedStateType, setSelectedStateType] = useState(groupings[0]);
+
+    if (selectedStateType != null) {
+      return (
+        <VizRow h={1}>
+          <VizItem w={1}>
+          <GroupingSelector
+            label={"Phase"}
+            groupings={
+              groupings.map(
+                grouping => ({
+                  key: grouping,
+                  display: WorkItemStateTypeDisplayName[grouping]
+                })
+              )
+            }
+            initialValue={selectedStateType}
+            onGroupingChanged={setSelectedStateType}
+          />
+          < PipelineStateDistributionChart
+            workItems={workItemsByStateType[selectedStateType]}
+            projectCycleMetrics={projectCycleMetrics}
+          />
+          </VizItem>
+        </VizRow>
+      )
+    }
+  }
+;
 
 
 export const ProjectPipelineStateDetailsView = withNavigationContext(PipelineStateDetailsView);
