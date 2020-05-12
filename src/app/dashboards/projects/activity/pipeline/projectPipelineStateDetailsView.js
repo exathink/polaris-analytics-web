19f0e739568e37b54a9497633993338f280b72a7
Krishna Kumar <kkumar@exathink.com>
2020-05-12 09:38:16 -0500

    Check for empty pipeline before displaying pipeline detail widget.
    [story=PO-150 subject=pipeline_empty_bug  story_url=https://urjuna.atlassian.net/browse/PO-150
    
    ]

--- src/app/dashboards/projects/activity/pipeline/projectPipelineStateDetailsView.js
+++ src/app/dashboards/projects/activity/pipeline/projectPipelineStateDetailsView.js
@@ -13,70 +13,70 @@ const PipelineStateDetailsView = (
     projectCycleMetrics,
     view
   }) => {
-
-    const workItemsByStateType = workItems.reduce(
-      (workItemsByStateType, workItem) => {
-        if (workItemsByStateType[workItem.stateType] != null) {
-          workItemsByStateType[workItem.stateType].push(workItem)
-        } else {
-          workItemsByStateType[workItem.stateType] = [workItem]
-        }
-        return workItemsByStateType
-      },
-      {}
-    );
-    const stateTypes = Object.keys(workItemsByStateType).sort(
-      (stateTypeA, stateTypeB) => WorkItemStateTypeSortOrder[stateTypeA] - WorkItemStateTypeSortOrder[stateTypeB]
-    )
-
-    const [selectedStateType, setSelectedStateType] = useState(stateTypes[0]);
-    const [selectedGrouping, setSelectedGrouping] = useState('state');
-
-    if (selectedStateType != null) {
-      return (
-        <VizRow h={1}>
-          <VizItem w={1}>
-            <Flex w={0.95} justify='space-between' >
-              <GroupingSelector
-                label={"Phase"}
-                groupings={
-                  stateTypes.map(
-                    stateType => ({
-                      key: stateType,
-                      display: WorkItemStateTypeDisplayName[stateType],
-                      style: {
-                        backgroundColor: WorkItemStateTypeColor[stateType],
-                        color: stateType === selectedStateType ? "#ffffff" : "#d4d4d4"
-                      }
-                    })
-                  )
-                }
-                initialValue={selectedStateType}
-                onGroupingChanged={setSelectedStateType}
-              />
-              <GroupingSelector
-                label={"Group Work Items By"}
-                groupings={
-                  ['state', 'type'].map(
-                    grouping => ({
-                      key: grouping,
-                      display: capitalizeFirstLetter(grouping)
-                    })
-                  )
-                }
-                initialValue={selectedGrouping}
-                onGroupingChanged={setSelectedGrouping}
-              />
-            </Flex>
-            < PipelineStateDistributionChart
-              stateType={selectedStateType}
-              groupBy={selectedGrouping}
-              workItems={workItemsByStateType[selectedStateType]}
-              projectCycleMetrics={projectCycleMetrics}
-            />
-          </VizItem>
-        </VizRow>
+    if (workItems.length > 0) {
+      const workItemsByStateType = workItems.reduce(
+        (workItemsByStateType, workItem) => {
+          if (workItemsByStateType[workItem.stateType] != null) {
+            workItemsByStateType[workItem.stateType].push(workItem)
+          } else {
+            workItemsByStateType[workItem.stateType] = [workItem]
+          }
+          return workItemsByStateType
+        },
+        {}
+      );
+      const stateTypes = Object.keys(workItemsByStateType).sort(
+        (stateTypeA, stateTypeB) => WorkItemStateTypeSortOrder[stateTypeA] - WorkItemStateTypeSortOrder[stateTypeB]
       )
+      const [selectedStateType, setSelectedStateType] = useState(stateTypes[0]);
+      const [selectedGrouping, setSelectedGrouping] = useState('state');
+
+      if (selectedStateType != null) {
+        return (
+          <VizRow h={1}>
+            <VizItem w={1}>
+              <Flex w={0.95} justify='space-between'>
+                <GroupingSelector
+                  label={"Phase"}
+                  groupings={
+                    stateTypes.map(
+                      stateType => ({
+                        key: stateType,
+                        display: WorkItemStateTypeDisplayName[stateType],
+                        style: {
+                          backgroundColor: WorkItemStateTypeColor[stateType],
+                          color: stateType === selectedStateType ? "#ffffff" : "#d4d4d4"
+                        }
+                      })
+                    )
+                  }
+                  initialValue={selectedStateType}
+                  onGroupingChanged={setSelectedStateType}
+                />
+                <GroupingSelector
+                  label={"Group Work Items By"}
+                  groupings={
+                    ['state', 'type'].map(
+                      grouping => ({
+                        key: grouping,
+                        display: capitalizeFirstLetter(grouping)
+                      })
+                    )
+                  }
+                  initialValue={selectedGrouping}
+                  onGroupingChanged={setSelectedGrouping}
+                />
+              </Flex>
+              < PipelineStateDistributionChart
+                stateType={selectedStateType}
+                groupBy={selectedGrouping}
+                workItems={workItemsByStateType[selectedStateType]}
+                projectCycleMetrics={projectCycleMetrics}
+              />
+            </VizItem>
+          </VizRow>
+        )
+      }
     }
   }
 ;
