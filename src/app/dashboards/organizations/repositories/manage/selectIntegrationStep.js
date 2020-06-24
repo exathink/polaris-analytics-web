8a772d228b8056f8e593e0dd32a010f5da6d5d33
Krishna Kumar <kkumar@exathink.com>
2020-06-24 16:40:02 -0500

    Fix anchor-is-valid jsx/ally errors by replacing anchors with more compliant components such as spans and cards.
    [story=PO-199 story_url=https://urjuna.atlassian.net/browse/PO-199
    
    ]

--- src/app/dashboards/organizations/repositories/manage/selectIntegrationStep.js
+++ src/app/dashboards/organizations/repositories/manage/selectIntegrationStep.js
@@ -12,37 +12,35 @@ export class SelectIntegrationStep extends React.Component {
         <div style={{padding: '30px'}}>
           <Row gutter={16}>
             <Col span={8}>
-              <a onClick={() => this.props.onConnectorTypeSelected('github')}>
-                <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}>
-                  <Meta
-                    avatar={
-                      <Avatar src="/images/third-party/GitHub-Mark-120px-plus.png"/>
-                    }
-                    title="Github"
-                    description="Import Repositories and Commits"
-                  />
-                </Card>
-              </a>
+              <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}
+                    onClick={() => this.props.onConnectorTypeSelected('github')}>
+                <Meta
+                  avatar={
+                    <Avatar src="/images/third-party/GitHub-Mark-120px-plus.png"/>
+                  }
+                  title="Github"
+                  description="Import Repositories and Commits"
+                />
+              </Card>
             </Col>
 
             <Col span={8}>
-              <a onClick={() => this.props.onConnectorTypeSelected('bitbucket')}>
-                <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}>
-                  <Meta
-                    avatar={
-                      <Avatar src="/images/third-party/bitbucket-mark-contained-gradient-neutral@2x.png"/>
-                    }
-                    title="Bit Bucket"
-                    description="Import Repositories and Commits"
-                  />
-                </Card>
-              </a>
+              <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}
+                    onClick={() => this.props.onConnectorTypeSelected('bitbucket')}>
+                <Meta
+                  avatar={
+                    <Avatar src="/images/third-party/bitbucket-mark-contained-gradient-neutral@2x.png"/>
+                  }
+                  title="Bit Bucket"
+                  description="Import Repositories and Commits"
+                />
+              </Card>
             </Col>
             {
 
-            <Col span={8}>
-              <a onClick={() => this.props.onConnectorTypeSelected('gitlab')}>
-                <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}>
+              <Col span={8}>
+                <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}
+                      onClick={() => this.props.onConnectorTypeSelected('gitlab')}>
                   <Meta
                     avatar={
                       <Avatar src="/images/third-party/gitlab-icon-rgb.svg"/>
@@ -51,8 +49,7 @@ export class SelectIntegrationStep extends React.Component {
                     description="Import Repositories and Commits"
                   />
                 </Card>
-              </a>
-            </Col>
+              </Col>
             }
           </Row>
         </div>
