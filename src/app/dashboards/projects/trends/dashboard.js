ae3e31cfaaa1e9b98177647e3e1f954e59e9d333
Krishna Kumar <kkumar@exathink.com>
2020-07-14 12:54:20 -0500

    Wire up initial trends dashboard with the ProjectFlowMetricTrend widget.
    [story=PO-205 story_url=https://urjuna.atlassian.net/browse/PO-205
    
    ]

--- src/app/dashboards/projects/trends/dashboard.js
+++ src/app/dashboards/projects/trends/dashboard.js
@@ -1,5 +1,47 @@
 import React from 'react';
+import {ProjectDashboard} from "../projectDashboard";
+import {withViewerContext} from "../../../framework/viewer/viewerContext";
+import {ProjectFlowMetricsTrendsWidget} from "./flowMetrics"
+import {Dashboard, DashboardRow, DashboardWidget} from "../../../framework/viz/dashboard";
 
-export const dashboard = () => null;
-
-export default dashboard;
+const dashboard_id = 'dashboards.trends.projects.dashboard.instance';
+const dashboard = ({viewerContext}) => (
+  <ProjectDashboard
+    pollInterval={1000 * 60}
+    render={
+      ({
+         project: {
+           key,
+           latestWorkItemEvent,
+           latestCommit
+         },
+         context
+       }) => {
+        return (
+          <Dashboard dashboard={`${dashboard_id}`}>
+            <DashboardRow h='30%'>
+              <DashboardWidget
+                w={1}
+                name="activity-summary"
+                title={`Flow Metrics`}
+                render={
+                  ({view}) =>
+                    <ProjectFlowMetricsTrendsWidget
+                      instanceKey={key}
+                      measurementWindow={30}
+                      days={45}
+                      samplingFrequency={7}
+                      targetPercentile={0.7}
+                      context={context}
+                      view={view}
+                      latestWorkItemEvent={latestWorkItemEvent}
+                    />
+                }
+              />
+            </DashboardRow>
+          </Dashboard>
+        );
+      }}
+  />
+)
+export default withViewerContext(dashboard);
