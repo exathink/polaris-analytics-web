f09b833b5af9c7ce18e2244b677f7eaebda859db
Krishna Kumar <kkumar@exathink.com>
2020-07-18 08:14:30 -0500

    Display Trend indicators for flow metrics.
    [story=PO-214 story_url=https://urjuna.atlassian.net/browse/PO-214
    
    ]

--- src/app/components/misc/statistic/statistic.js
+++ src/app/components/misc/statistic/statistic.js
@@ -1,2 +1,22 @@
 import './statistic.css';
-export {Statistic} from 'antd';
+import React from 'react';
+import {Icon} from 'antd'
+export {Statistic, Icon} from 'antd';
+
+// Display the trend indicator only if abs value of the delta is greater than this threshold.
+export const TrendIndicatorDisplayThreshold = 1.0;
+
+export const TrendIndicator = ({firstValue, secondValue, good, deltaThreshold=TrendIndicatorDisplayThreshold}) => {
+  const delta = (firstValue - secondValue)/(1.0*firstValue);
+  return (
+      Math.abs(delta ) > deltaThreshold &&
+        <Icon
+          type={delta > 0 ?  'arrow-up' : 'arrow-down'}
+          style={good(delta)? {color: '#399a15'} : {color: '#9a3727'}}
+
+        />
+  )
+}
+
+TrendIndicator.isPositive = (delta) => delta > 0;
+TrendIndicator.isNegative = (delta) => delta <= 0;
