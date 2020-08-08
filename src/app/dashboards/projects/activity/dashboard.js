6022abaffa57c2927b21fe19f2b87c12346d5de9
Krishna Kumar <kkumar@exathink.com>
2020-08-08 14:50:53 -0500

    Remove project.new-dashboard feature flag and make new-dashboard the default dashboard. Remove classic_dashboard.js (PO-235)

--- src/app/dashboards/projects/activity/dashboard.js
+++ src/app/dashboards/projects/activity/dashboard.js
@@ -1,10 +1,12 @@
-import {dashboard as classic_dashboard} from "./classic_dashboard"
-import {dashboard as new_dashboard} from "./new_dashboard"
-import {PROJECTS_NEW_DASHBOARD} from "../../../../config/featureFlags";
 
+ export {default} from "./new_dashboard"
+
+ /*
 import {withFeatureFlag} from "../../../components/featureFlags/withFeatureFlag";
 
 export default withFeatureFlag(PROJECTS_NEW_DASHBOARD)({
   whenDisabled: classic_dashboard,
   whenEnabled: new_dashboard
 })
+
+  */
