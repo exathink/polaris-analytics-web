1d21c319fcc1cc968f29bb87c256297a3d90a4c9
Krishna Kumar <kkumar@exathink.com>
2020-08-13 14:41:00 -0500

    Add hook for projectPipelineCycleMetrics [story=PO-239  story_url=https://urjuna.atlassian.net/browse/PO-239]

---
+++ src/app/dashboards/projects/activity/hooks/useQueryProjectPipelineCycleMetrics.js
@@ -0,0 +1,48 @@
+import {useQuery} from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import {analytics_service} from "../../../../services/graphql";
+
+export function useQueryProjectPipelineCycleMetrics({instanceKey, days, targetPercentile, specsOnly, defectsOnly, referenceString}) {
+  return useQuery(
+    gql`
+     query projectPipelineCycleMetrics($key: String!, $referenceString: String, $targetPercentile: Float, $specsOnly: Boolean, $defectsOnly : Boolean) {
+      project(
+            key: $key, 
+            interfaces: [PipelineCycleMetrics],
+            pipelineCycleMetricsArgs: {
+              metrics: [
+                avg_cycle_time,
+                work_items_in_scope,
+                work_items_with_commits,
+                percentile_lead_time
+              ],
+              defectsOnly: $defectsOnly,
+              specsOnly: $specsOnly,
+              leadTimeTargetPercentile: $targetPercentile
+            },
+            referenceString: $referenceString
+          ) {
+          pipelineCycleMetrics {
+                avgCycleTime
+                percentileLeadTime
+                targetPercentile
+                workItemsWithCommits
+                workItemsInScope
+         }
+      }
+     }
+  `,
+    {
+      service: analytics_service,
+      variables: {
+        key: instanceKey,
+        targetPercentile: targetPercentile,
+        defectsOnly: defectsOnly,
+        specsOnly: specsOnly,
+        referenceString: referenceString
+      },
+      errorPolicy: "all",
+      pollInterval: analytics_service.defaultPollInterval()
+    }
+  )
+}
