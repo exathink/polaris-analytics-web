6cff81754833574b441744b0d65b63cf501b4c46
Krishna Kumar <kkumar@exathink.com>
2020-08-14 17:59:33 -0500

    Add query hook for fetching work item implementation cost.
    [story=PO-242  story_url=https://urjuna.atlassian.net/browse/PO-242]

---
+++ src/app/dashboards/work_items/activity/hooks/useQueryWorkItemImplementationCost.js
@@ -0,0 +1,29 @@
+import {useQuery} from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import {analytics_service} from "../../../../services/graphql";
+
+export function useQueryWorkItemImplementationCost({instanceKey, referenceString}) {
+  return useQuery(
+    gql`
+      query workItemImplementationCost($key: String!, $referenceString: String) {
+        workItem(key: $key, interfaces: [ImplementationCost], referenceString: $referenceString){
+              id
+              name
+              key
+              implementationCost
+              implementationSpan
+              authorCount
+       }
+    }
+    `, {
+      service: analytics_service,
+      variables: {
+        key: instanceKey,
+        referenceString: referenceString
+
+      },
+      errorPolicy: "all",
+      pollInterval: analytics_service.defaultPollInterval()
+    }
+  )
+}
