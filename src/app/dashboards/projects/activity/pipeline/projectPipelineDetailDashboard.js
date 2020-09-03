3f15aa0dd04736073c7aac836e9351f69ba808d4
Krishna Kumar <kkumar@exathink.com>
2020-09-03 14:18:20 -0500

    Show the workItemScopeSelector control on the PipelineStateDetails dashboard and use it to drive the spec/all behaviour of all the child components.
    [story=PO-276  story_url=https://urjuna.atlassian.net/browse/PO-276]

--- src/app/dashboards/projects/activity/pipeline/projectPipelineDetailDashboard.js
+++ src/app/dashboards/projects/activity/pipeline/projectPipelineDetailDashboard.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Dashboard, DashboardRow, DashboardWidget} from "../../../../framework/viz/dashboard";
 import {ProjectPhaseSummaryWidget} from "./projectPhaseSummaryWidget";
 import {ProjectPipelineStateDetailsWidget} from "./projectPipelineStateDetailsWidget";
+import {Box, Flex} from "reflexbox";
+import {WorkItemScopeSelector} from "../../shared/components/workItemScopeSelector";
+
 
 const dashboard_id = 'dashboards.activity.projects.pipeline.detail';
 
@@ -13,42 +16,67 @@ export const ProjectPipelineDetailDashboard = (
     stateMappingIndex,
     days,
     targetPercentile,
-  }) => (
-  <Dashboard dashboard={dashboard_id}>
-    <DashboardRow h={"15%"}>
-      <DashboardWidget
-        w={1/4}
-        name="project-pipeline-summary-detail-view"
-        render={
-          ({view}) =>
-            <ProjectPhaseSummaryWidget
-              instanceKey={instanceKey}
-              view={view}
-              latestWorkItemEvent={latestWorkItemEvent}
-              stateMappingIndex={stateMappingIndex}
-            />
-        }
-        showDetail={false}
-      />
-    </DashboardRow>
-    <DashboardRow h={"81%"}>
-      <DashboardWidget
-        w={1}
-        name="project-pipeline-state-detail-view"
-        render={
-          ({view}) =>
-            <ProjectPipelineStateDetailsWidget
-              instanceKey={instanceKey}
-              view={view}
-              context={context}
-              latestWorkItemEvent={latestWorkItemEvent}
-              stateMappingIndex={stateMappingIndex}
-              days={days}
-              targetPercentile={targetPercentile}
-            />
-        }
-        showDetail={true}
-      />
-    </DashboardRow>
-  </Dashboard>
-);
+  }) => {
+  const [workItemScope, setWorkItemScope] = useState('specs');
+  const specsOnly = workItemScope === 'specs';
+
+  return (
+    <Dashboard dashboard={dashboard_id}>
+      <DashboardRow
+        h={"15%"}
+        title={`Work In Progress`}
+        controls={[
+          () => (
+            <div style={{padding: "10px"}}>
+              <Flex align={'center'}>
+                <Box pr={2} w={"100%"}>
+                  <WorkItemScopeSelector
+                    workItemScope={workItemScope}
+                    setWorkItemScope={setWorkItemScope}
+                  />
+                </Box>
+              </Flex>
+            </div>
+
+          )
+        ]}
+      >
+        <DashboardWidget
+          w={1 / 4}
+          name="project-pipeline-summary-detail-view"
+          render={
+            ({view}) =>
+              <ProjectPhaseSummaryWidget
+                instanceKey={instanceKey}
+                specsOnly={specsOnly}
+                view={view}
+                latestWorkItemEvent={latestWorkItemEvent}
+                stateMappingIndex={stateMappingIndex}
+              />
+          }
+          showDetail={false}
+        />
+      </DashboardRow>
+      <DashboardRow h={"81%"}>
+        <DashboardWidget
+          w={1}
+          name="project-pipeline-state-detail-view"
+          render={
+            ({view}) =>
+              <ProjectPipelineStateDetailsWidget
+                instanceKey={instanceKey}
+                specsOnly={specsOnly}
+                view={view}
+                context={context}
+                latestWorkItemEvent={latestWorkItemEvent}
+                stateMappingIndex={stateMappingIndex}
+                days={days}
+                targetPercentile={targetPercentile}
+              />
+          }
+          showDetail={true}
+        />
+      </DashboardRow>
+    </Dashboard>
+  )
+};
