79070411bba677d6a0c1c959fafb5701fea56ead
Krishna Kumar <kkumar@exathink.com>
2020-09-09 14:24:01 -0500

    Extract StickerIcon component from sticker-widget.js
    [story=PO-273 story_url=https://urjuna.atlassian.net/browse/PO-273]

--- src/app/dashboards/shared/containers/stickers/simpleSticker/sticker-widget.js
+++ src/app/dashboards/shared/containers/stickers/simpleSticker/sticker-widget.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { StickerWidgetWrapper } from './style';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {StickerWidgetWrapper} from './style';
+import {Link} from 'react-router-dom';
 
 
 export default class StickerWidget extends Component {
   render() {
-    const { fontColor, bgColor, width, icon, number, hoverText, text, link } = this.props;
+    const {fontColor, bgColor, width, icon, number, hoverText, text, link} = this.props;
 
     const textColor = {
       color: fontColor,
@@ -20,8 +20,8 @@ export default class StickerWidget extends Component {
 
     return (
       <StickerWidgetWrapper className="isoStickerWidget" style={widgetStyle} title={hoverText}>
-        <div className="isoIconWrapper" >
-          <i className={icon} style={iconStyle} />
+        <div className="isoIconWrapper">
+          <i className={icon} style={iconStyle}/>
         </div>
 
         <div className="isoContentWrapper">
@@ -40,12 +40,32 @@ export default class StickerWidget extends Component {
 
 const StickerBody = ({number, numberHover, text, textColor}) => (
   <React.Fragment>
-      <h3 className="isoStatNumber" style={textColor}>
-        {number}
-      </h3>
-      <span className="isoLabel" style={textColor}>
+    <h3 className="isoStatNumber" style={textColor}>
+      {number}
+    </h3>
+    <span className="isoLabel" style={textColor}>
         {text}
       </span>
   </React.Fragment>
 
 )
+
+export const StickerIcon = ({fontColor, bgColor, width, icon, number, hoverText, text, link}) => {
+  const textColor = {
+    color: fontColor,
+  };
+  const widgetStyle = {
+    backgroundColor: bgColor,
+    width: width,
+  };
+  const iconStyle = {
+    color: fontColor,
+  };
+  return (
+    <StickerWidgetWrapper className="isoStickerWidget" style={widgetStyle} title={hoverText}>
+      <div className="isoIconWrapper">
+        <i className={icon} style={iconStyle}/>
+      </div>
+    </StickerWidgetWrapper>
+  )
+}
