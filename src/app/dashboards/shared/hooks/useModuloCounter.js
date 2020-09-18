f0d8bd335406cc14f9e1465b9f4a00d02b0183cf
Krishna Kumar <kkumar@exathink.com>
2020-09-18 17:57:22 -0500

    Make the timeout for the useModuloCounter hook a parameter. Use the n parameter in the modulo calc. [story=PO-292 story_url=https://urjuna.atlassian.net/browse/PO-292]

--- src/app/dashboards/shared/hooks/useModuloCounter.js
+++ src/app/dashboards/shared/hooks/useModuloCounter.js
@@ -1,12 +1,12 @@
 import {useEffect, useState} from 'react';
 
-
-export function useModuloCounter(n) {
+// Sets a timer that increments a counter from 0 modulo n and each tick millisecs.
+export function useModuloCounter(n, tick) {
 
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    const timer = window.setInterval(() => setCounter((counter + 1) % 2), 5 * 1000);
+    const timer = window.setInterval(() => setCounter((counter + 1) % n), tick);
     return () => window.clearInterval(timer)
   });
 
