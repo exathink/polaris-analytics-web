4025f66e03a055bffb60d224a2d7dca314e147ec
Krishna Kumar <kkumar@exathink.com>
2020-09-18 17:56:30 -0500

    Add title override prop to PercentileLeadTime and PercentileCycleTime [story=PO-292 story_url=https://urjuna.atlassian.net/browse/PO-292]

--- src/app/dashboards/projects/activity/predictability/projectPredictabilityView.js
+++ src/app/dashboards/projects/activity/predictability/projectPredictabilityView.js
@@ -1,47 +1,98 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {VizItem, VizRow} from "../../../shared/containers/layout";
-import {FlowStatistic} from "../../../shared/components/flowStatistics/flowStatistics";
+import {FlowStatistic, PercentileCycleTime, PercentileLeadTime} from "../../../shared/components/flowStatistics/flowStatistics";
 import {TrendIndicator} from "../../../../components/misc/statistic/statistic";
 import {percentileToText} from "../../../../helpers/utility";
+import {useModuloCounter} from "../../../shared/hooks/useModuloCounter";
 
 export const ProjectPredictabilityView = (
   {
     responseTimeConfidenceTrends,
+    cycleMetricsTrends,
+    leadTimeTarget,
+    cycleTimeTarget,
     leadTimeConfidenceTarget,
     cycleTimeConfidenceTarget
   }
 ) => {
   const current = responseTimeConfidenceTrends[0]
   const previous = responseTimeConfidenceTrends[1]
+
+  const currentCycleMetrics = cycleMetricsTrends[0]
+  const previousCycleMetrics = cycleMetricsTrends[1]
+  const counter = useModuloCounter(4, 5000);
+
+
   return (
     <React.Fragment>
       <VizRow h={"100%"}>
-
         <VizItem w={0.6}>
-          <FlowStatistic
-            title={<span>{'CycleTime'}<sup>{percentileToText(cycleTimeConfidenceTarget)} Target</sup></span>}
-            currentCycleMetrics={current}
-            previousCycleMetrics={previous}
-            metric={'cycleTimeTarget'}
-            uom={'Days'}
-            good={TrendIndicator.isNegative}
-          />
+          {
+              counter < 2 ?
+              <FlowStatistic
+                title={<span>{'CycleTime'}<sup>{percentileToText(cycleTimeConfidenceTarget)} Target</sup></span>}
+                currentCycleMetrics={current}
+                previousCycleMetrics={previous}
+                metric={'cycleTimeTarget'}
+                uom={'Days'}
+
+              />
+              :
+              <FlowStatistic
+                title={<span>{'LeadTime'}<sup>{percentileToText(leadTimeConfidenceTarget)} Target</sup></span>}
+                currentCycleMetrics={current}
+                previousCycleMetrics={previous}
+                metric={'leadTimeTarget'}
+                uom={'Days'}
+
+              />
+          }
         </VizItem>
         <VizItem w={0.4}>
-          <FlowStatistic
-            title={<span>{'Confidence'}</span>}
-            currentCycleMetrics={current}
-            previousCycleMetrics={previous}
-            metric={'cycleTimeConfidence'}
-            display={value => value * 100}
-            uom={'%'}
-            precision={2}
-            target={cycleTimeConfidenceTarget}
-            good={TrendIndicator.isPositive}
+          {
+            [
+              <PercentileCycleTime
+                title={'Actual'}
+                currentCycleMetrics={currentCycleMetrics}
+                previousCycleMetrics={previousCycleMetrics}
+                target={cycleTimeTarget}
+                targetPercentile={cycleTimeConfidenceTarget}
+              />,
 
-          />
+              <FlowStatistic
+                title={<span>{'% at Target'}</span>}
+                currentCycleMetrics={current}
+                previousCycleMetrics={previous}
+                metric={'cycleTimeConfidence'}
+                display={value => value * 100}
+                uom={'%'}
+                precision={2}
+                target={cycleTimeConfidenceTarget}
+                good={TrendIndicator.isPositive}
+
+              />,
+              <PercentileLeadTime
+                title={'Actual'}
+                currentCycleMetrics={currentCycleMetrics}
+                previousCycleMetrics={previousCycleMetrics}
+                target={leadTimeTarget}
+                targetPercentile={leadTimeConfidenceTarget}
+              />,
+              <FlowStatistic
+                title={<span>{'% at Target'}</span>}
+                currentCycleMetrics={current}
+                previousCycleMetrics={previous}
+                metric={'leadTimeConfidence'}
+                display={value => value * 100}
+                uom={'%'}
+                precision={2}
+                target={leadTimeConfidenceTarget}
+                good={TrendIndicator.isPositive}
+
+              />,
+            ][counter]
+          }
         </VizItem>
-
       </VizRow>
     </React.Fragment>
   )
