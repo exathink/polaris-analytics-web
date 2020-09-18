07cfb04e2fcfebb344d20c04c1e7391be545c5e6
Krishna Kumar <kkumar@exathink.com>
2020-09-18 18:13:39 -0500

    Readabilty update for useGenerateTicks.js
    [story=PO-292 story_url=https://urjuna.atlassian.net/browse/PO-292]

--- src/app/dashboards/shared/hooks/useGenerateTicks.js
+++ src/app/dashboards/shared/hooks/useGenerateTicks.js
@@ -3,12 +3,12 @@ import {useEffect, useState} from 'react';
 // Sets a timer that increments a counter from 0 modulo n and each tick millisecs.
 export function useGenerateTicks(n, interval) {
 
-  const [counter, setCounter] = useState(0);
+  const [tick, setTick] = useState(0);
 
   useEffect(() => {
-    const timer = window.setInterval(() => setCounter((counter + 1) % n), interval);
+    const timer = window.setInterval(() => setTick((tick + 1) % n), interval);
     return () => window.clearInterval(timer)
   });
 
-  return counter
+  return tick
 }
