305e198b6be494cb6176405a7dbb065308bee770
Krishna Kumar <kkumar@exathink.com>
2020-10-07 13:20:04 -0500

    Refactor throughput trends detail dashboard to use shared trendsControlBar
    [story_url=https://urjuna.atlassian.net/browse/PO-328]

--- src/app/dashboards/projects/trends/throughput/throughputTrendsDetailDashboard.js
+++ src/app/dashboards/projects/trends/throughput/throughputTrendsDetailDashboard.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Dashboard, DashboardRow, DashboardWidget} from "../../../../framework/viz/dashboard";
 import {ProjectThroughputTrendsWidget} from "./throughputTrendsWidget";
-import {DaysRangeSlider, SIX_MONTHS} from "../../../shared/components/daysRangeSlider/daysRangeSlider";
-
+import {
+  getTrendsControlBarControls,
+  useTrendsControlBarState
+} from "../../../shared/components/trendingControlBar/trendingControlBar";
 
 const dashboard_id = 'dashboards.trends.projects.throughput.detail';
 
@@ -22,10 +24,12 @@ export const ProjectThroughputTrendsDetailDashboard = (
     pollInterval
   }
 ) => {
-  const [daysRange, setDaysRange] = useState(days);
-  const [measurementWindowRange, setMeasurementWindowRange] = useState(measurementWindow);
-  const [frequencyRange, setFrequencyRange] = useState(samplingFrequency);
 
+  const [
+    [daysRange, setDaysRange],
+    [measurementWindowRange, setMeasurementWindowRange],
+    [frequencyRange, setFrequencyRange]
+  ] = useTrendsControlBarState(days, measurementWindow, samplingFrequency);
 
   return (
     <Dashboard
@@ -35,38 +39,15 @@ export const ProjectThroughputTrendsDetailDashboard = (
         h={1}
         title={`Throughput Trends`}
         subTitle={`Last ${daysRange} days`}
-        controls={[
-          () =>
-            <div title="Days" style={{minWidth: "500px"}}>
-              <DaysRangeSlider
-                initialDays={daysRange}
-                setDaysRange={setDaysRange}
-                range={SIX_MONTHS}
-              />
-            </div>
-              ,
-          () =>
-            <div title="Sampling Frequency" style={{minWidth: "200px"}}>
-              <DaysRangeSlider
-                title={'Frequency'}
-                initialDays={frequencyRange}
-                setDaysRange={setFrequencyRange}
-                range={[1,7,14,30]}
-              />
-            </div>
-              ,
-          () =>
-            <div title="Window" style={{minWidth: "200px"}}>
-              <DaysRangeSlider
-                title={'Window'}
-                initialDays={measurementWindowRange}
-                setDaysRange={setMeasurementWindowRange}
-                range={[1,7,14,30]}
-              />
-            </div>
-          ,
-
-        ]}
+        controls={
+          getTrendsControlBarControls(
+            [
+              [daysRange, setDaysRange],
+              [measurementWindowRange, setMeasurementWindowRange],
+              [frequencyRange, setFrequencyRange]
+            ]
+          )
+        }
       >
         <DashboardWidget
           w={1}
