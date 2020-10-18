badf1f137bf936a8393db031920c74de82d5b47d
Krishna Kumar <kkumar@exathink.com>
2020-10-17 22:57:13 -0500

    Add FlowMix widget. Update title based on daysRangeSlider.
    [story_url=https://urjuna.atlassian.net/browse/PO-335]

--- src/app/dashboards/projects/activity/flowMetrics/projectFlowMetricsDetailDashboard.js
+++ src/app/dashboards/projects/activity/flowMetrics/projectFlowMetricsDetailDashboard.js
@@ -5,6 +5,7 @@ import {ProjectDeliveryCycleFlowMetricsWidget} from "./projectDeliveryCycleFlowM
 import {DaysRangeSlider} from "../../../shared/components/daysRangeSlider/daysRangeSlider";
 import {WorkItemScopeSelector} from "../../shared/components/workItemScopeSelector";
 import {Box, Flex} from "reflexbox";
+import {ProjectFlowMixTrendsWidget} from "../../trends/flowMix";
 
 const dashboard_id = 'dashboards.activity.projects.cycleMetrics.detail';
 
@@ -14,6 +15,7 @@ export const ProjectFlowMetricsDetailDashboard = (
     instanceKey,
     context,
     latestWorkItemEvent,
+    latestCommit,
     stateMappingIndex,
     days,
     targetPercentile,
@@ -31,9 +33,8 @@ export const ProjectFlowMetricsDetailDashboard = (
       dashboard={dashboard_id}
     >
       <DashboardRow
-        h={"20%"}
-        title={`Flow Metrics`}
-        subTitle={`Last ${daysRange} days`}
+        h={"30%"}
+        title={`${specsOnly? 'Specs' : 'Work Items' } Closed in Last ${daysRange} days`}
         controls={[
           () => (
             <div style={{minWidth: "300px", padding: "15px"}}>
@@ -45,7 +46,7 @@ export const ProjectFlowMetricsDetailDashboard = (
                   />
                 </Box>
               </Flex>
-              </div>
+            </div>
 
           ),
           () =>
@@ -56,6 +57,8 @@ export const ProjectFlowMetricsDetailDashboard = (
       >
         <DashboardWidget
           w={0.4}
+          title={"Flow Metrics"}
+          subtitle={`${daysRange} days`}
           name="cycle-metrics-summary-detailed"
           render={
             ({view}) =>
@@ -77,8 +80,46 @@ export const ProjectFlowMetricsDetailDashboard = (
           }
           showDetail={false}
         />
+        <DashboardWidget
+          w={0.37}
+          name="alignment"
+          title={'Flow Mix'}
+          subtitle={`${daysRange} days`}
+          styles={{
+            controlContainer: {
+              width: '27%'
+            }
+          }}
+          controls={
+            [
+              ({view}) => (
+                view !== 'detail' &&
+                <span>{specsOnly ? '% of Effort' : '% of Items'}</span>
+              )
+            ]
+          }
+
+          render={
+            ({view}) =>
+              <ProjectFlowMixTrendsWidget
+                instanceKey={instanceKey}
+                measurementWindow={daysRange}
+                days={7}
+                samplingFrequency={7}
+                context={context}
+                view={view}
+                latestWorkItemEvent={latestWorkItemEvent}
+                latestCommit={latestCommit}
+                specsOnly={specsOnly}
+                asStatistic={true}
+
+              />
+          }
+          hideTitlesInDetailView={true}
+          showDetail={true}
+        />
       </DashboardRow>
-      <DashboardRow h={"75%"}>
+      <DashboardRow h={"65%"}>
         <DashboardWidget
           w={1}
           name="cycle-metrics-delivery-details"
