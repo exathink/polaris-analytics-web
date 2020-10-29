70c1e7cd5f3955c639bec95d9a506fc629b9ec82
Krishna Kumar <kkumar@exathink.com>
2020-10-29 17:16:53 -0500

    Export Highcharts from the charts module so that we can require new chart type in the files they are used.
    [story_url=https://urjuna.atlassian.net/browse/PO-351]

--- src/app/framework/viz/charts/index.js
+++ src/app/framework/viz/charts/index.js
@@ -1,7 +1,7 @@
 
 export {tooltipHtml} from './tooltip';
 
-export {default as ChartWrapper} from './chartWrapper';
+export {Highcharts, default as ChartWrapper} from './chartWrapper';
 export {Chart} from './chart';
 
 // This must be the last item imported for some Highcharts magic to work right.
