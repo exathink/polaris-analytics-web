81146dd147baaf2c86f04ef596a24cae79ed44fd
Aman Mavai <aman.mavai@gslab.com>
2020-11-18 16:49:24 +0530

    format with prettier

--- src/app/dashboards/work_items/activity/eventTimeline/workItemEventTimelineChart.js
+++ src/app/dashboards/work_items/activity/eventTimeline/workItemEventTimelineChart.js
@@ -5,156 +5,148 @@ import {Colors, WorkItemStateTypeColor, WorkItemStateTypeDisplayName} from "../.
 import {formatDateTime} from "../../../../i18n";
 
 const workItemEventSymbol = {
-  unmapped: 'triangle',
-  backlog: 'triangle',
-  open: 'diamond',
-  wip: 'diamond',
-  complete: 'diamond',
-  closed: 'triangle-down'
-}
+  unmapped: "triangle",
+  backlog: "triangle",
+  open: "diamond",
+  wip: "diamond",
+  complete: "diamond",
+  closed: "triangle-down",
+};
 export const WorkItemEventsTimelineChart = Chart({
-  chartUpdateProps:
-    (props) => pick(props, 'workItem'),
+  chartUpdateProps: (props) => pick(props, "workItem"),
 
   eventHandler: DefaultSelectionEventHandler,
-  mapPoints: (points, _) => points.map(point => ({
+  mapPoints: (points, _) =>
+    points.map((point) => ({
       workItem: point.workItem,
-      event: point.timelineEvent.eventDate != null ?
-        {
-          type: 'event',
-          timelineEventId: point.timelineEventId,
-        }
-        :
-        {
-            type: 'commit',
-            name: point.timelineEvent.name,
-            key: point.timelineEvent.key
-        }
+      event:
+        point.timelineEvent.eventDate != null
+          ? {
+              type: "event",
+              timelineEventId: point.timelineEventId,
+            }
+          : {
+              type: "commit",
+              name: point.timelineEvent.name,
+              key: point.timelineEvent.key,
+            },
     })),
 
-  getConfig:
-
-    ({workItem, context, intl}) => {
-
-
-      const series_data = [
-        ...workItem.workItemEvents.filter(
+  getConfig: ({workItem, context, intl}) => {
+    const series_data = [
+      ...workItem.workItemEvents
+        .filter(
           /* filter out backlog events for work items that are not in closed state */
-          timelineEvent => workItem.stateType !== 'closed' ? timelineEvent.newStateType !== 'backlog' : true
-        ).map((timelineEvent, index) => {
+          (timelineEvent) => (workItem.stateType !== "closed" ? timelineEvent.newStateType !== "backlog" : true)
+        )
+        .map((timelineEvent, index) => {
           const eventDate = toMoment(timelineEvent.eventDate);
-          const newStateType = timelineEvent.newStateType || 'unmapped';
-          return (
-            {
-              x: eventDate.valueOf(),
-              y: 0,
-              z: 3,
-              marker: {
-                symbol:  workItemEventSymbol[newStateType],
-                radius: 6
-              },
-              color: WorkItemStateTypeColor[newStateType],
-              timelineEvent: timelineEvent,
-              workItem: workItem
-            }
-          )
+          const newStateType = timelineEvent.newStateType || "unmapped";
+          return {
+            x: eventDate.valueOf(),
+            y: 0,
+            z: 3,
+            marker: {
+              symbol: workItemEventSymbol[newStateType],
+              radius: 6,
+            },
+            color: WorkItemStateTypeColor[newStateType],
+            timelineEvent: timelineEvent,
+            workItem: workItem,
+          };
         }),
-        ...workItem.workItemCommits.map((timelineEvent, index) => {
-          const eventDate = toMoment(timelineEvent.commitDate);
-          return (
-            {
-              x: eventDate.valueOf(),
-              y: 1,
-              z: 3,
-              marker: {
-                symbol: 'circle',
-                radius: 4
-              },
-              timelineEvent: timelineEvent,
-              workItem: workItem
-            }
-          )
-        }),
-      ];
+      ...workItem.workItemCommits.map((timelineEvent, index) => {
+        const eventDate = toMoment(timelineEvent.commitDate);
+        return {
+          x: eventDate.valueOf(),
+          y: 1,
+          z: 3,
+          marker: {
+            symbol: "circle",
+            radius: 4,
+          },
+          timelineEvent: timelineEvent,
+          workItem: workItem,
+        };
+      }),
+    ];
 
-
-      return {
-        chart: {
-          type: 'scatter',
-          backgroundColor: Colors.Chart.backgroundColor,
-          zoomType: 'xy',
-          panning: true,
-          panKey: 'shift',
-        },
+    return {
+      chart: {
+        type: "scatter",
+        backgroundColor: Colors.Chart.backgroundColor,
+        zoomType: "xy",
+        panning: true,
+        panKey: "shift",
+      },
+      title: {
+        text: "Timeline",
+        align: "center",
+      },
+      xAxis: {
+        type: "datetime",
         title: {
-          text: 'Timeline',
-          align: 'center'
+          text: null,
         },
-        xAxis: {
-          type: 'datetime',
-          title: {
-            text: null
+      },
+      yAxis: {
+        id: "y-items",
+        title: {
+          text: null,
+        },
+        categories: ["Events", "Commits"],
+        reversed: true,
+        labels: {
+          align: "left",
+          reserveSpace: true,
+        },
+      },
+      tooltip: {
+        useHTML: true,
+        hideDelay: 50,
+        formatter: function () {
+          const event = this.point.timelineEvent;
+          if (event.eventDate != null) {
+            const header = `Phase: ${WorkItemStateTypeDisplayName[event.newStateType || "unmapped"]}`;
+            const transition = `State: ${
+              event.previousState ? `${capitalizeFirstLetter(event.previousState)} -> ` : ``
+            } ${capitalizeFirstLetter(event.newState)} `;
+            return tooltipHtml({
+              header: `${header}<br/>${transition}`,
+              body: [[`Date: `, `${formatDateTime(intl, toMoment(event.eventDate))}`]],
+            });
+          } else {
+            const commit = `Commit: ${event.committer} committed to ${event.repository} on branch ${event.branch}`;
+            return tooltipHtml({
+              header: `${commit}`,
+              body: [
+                [`Message: `, `${elide(event.commitMessage, 60)}`],
+                [`Author: `, `${event.author}`],
+                [`Date: `, `${formatDateTime(intl, toMoment(event.commitDate))}`],
+              ],
+            });
           }
         },
-        yAxis: {
-          id: 'y-items',
-          title: {
-            text: null
-          },
-          categories: ['Events', 'Commits'],
-          reversed: true,
-          labels: {
-            align: 'left',
-            reserveSpace: true,
-          }
+      },
+      series: [
+        {
+          name: "timeline",
+          pointWidth: 20,
+          data: series_data,
+          turboThreshold: 0,
+          allowPointSelect: true,
         },
-        tooltip: {
-          useHTML: true,
-          hideDelay: 50,
-          formatter: function () {
-            const event = this.point.timelineEvent;
-            if (event.eventDate != null) {
-              const header = `Phase: ${WorkItemStateTypeDisplayName[event.newStateType || 'unmapped']}`;
-              const transition = `State: ${event.previousState ? `${capitalizeFirstLetter(event.previousState)} -> ` : ``} ${capitalizeFirstLetter(event.newState)} `;
-              return tooltipHtml({
-                header: `${header}<br/>${transition}`,
-                body: [
-                  [`Date: `, `${formatDateTime(intl, toMoment(event.eventDate))}`],
-                ]
-              })
-            } else {
-              const commit = `Commit: ${event.committer} committed to ${event.repository} on branch ${event.branch}`
-              return tooltipHtml({
-                header: `${commit}`,
-                body: [
-                  [`Message: `, `${elide(event.commitMessage, 60)}`],
-                  [`Author: `, `${event.author}`],
-                  [`Date: `, `${formatDateTime(intl, toMoment(event.commitDate))}`]
-                ]
-              })
-            }
-          },
-        },
-        series: [
-          {
-            name: 'timeline',
-            pointWidth: 20,
-            data: series_data,
-            turboThreshold: 0,
-            allowPointSelect: true,
-          },
-        ],
-        legend: {
-          enabled: false
-        },
-        time: {
-          // Since we are already passing in UTC times we
-          // dont need the chart to translate the time to UTC
-          // This makes sure the tooltips text matches the timeline
-          // on the axis.
-          useUTC: false
-        }
-
-      }
-    }
+      ],
+      legend: {
+        enabled: false,
+      },
+      time: {
+        // Since we are already passing in UTC times we
+        // dont need the chart to translate the time to UTC
+        // This makes sure the tooltips text matches the timeline
+        // on the axis.
+        useUTC: false,
+      },
+    };
+  },
 });
--- src/app/dashboards/work_items/activity/hooks/useQueryWorkItemEventTimeline.js
+++ src/app/dashboards/work_items/activity/hooks/useQueryWorkItemEventTimeline.js
@@ -3,59 +3,59 @@ import gql from "graphql-tag";
 import {analytics_service} from "../../../../services/graphql";
 
 export function useQueryWorkItemEventTimeline({instanceKey, latestWorkItemEvent, latestWorkItemCommit}) {
-  const referenceString = `${(latestWorkItemEvent || '')}${(latestWorkItemCommit || '')}`;
+  const referenceString = `${latestWorkItemEvent || ""}${latestWorkItemCommit || ""}`;
   return useQuery(
     gql`
       query workItem_workItemEvents($key: String!, $referenceString: String) {
-                workItem(key: $key, referenceString: $referenceString){
-                    id
-                    name
-                    displayId
-                    workItemType
-                    state
-                    stateType
-                    workItemEvents{
-                        count
-                        edges {
-                            node {
-                                id
-                                state
-                                eventDate
-                                previousState
-                                previousStateType
-                                newState
-                                newStateType
-                            }
-                        }
-                    }
-                    commits {
-                       count
-                       edges {
-                        node {
-                            name
-                            key
-                            commitHash
-                            commitMessage
-                            commitDate
-                            committer
-                            author
-                            authorDate
-                            branch
-                            repository
-                        }
-                       }
-                    }
-                }
+        workItem(key: $key, referenceString: $referenceString) {
+          id
+          name
+          displayId
+          workItemType
+          state
+          stateType
+          workItemEvents {
+            count
+            edges {
+              node {
+                id
+                state
+                eventDate
+                previousState
+                previousStateType
+                newState
+                newStateType
+              }
             }
-    `, {
+          }
+          commits {
+            count
+            edges {
+              node {
+                name
+                key
+                commitHash
+                commitMessage
+                commitDate
+                committer
+                author
+                authorDate
+                branch
+                repository
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
       service: analytics_service,
       variables: {
         key: instanceKey,
-        referenceString: referenceString
-
+        referenceString: referenceString,
       },
       errorPolicy: "all",
-      pollInterval: analytics_service.defaultPollInterval()
+      pollInterval: analytics_service.defaultPollInterval(),
     }
-  )
+  );
 }
