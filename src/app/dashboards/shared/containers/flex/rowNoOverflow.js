f780cca487e97f943fb6b8e7c80e79f4f4f63c44
Krishna Kumar <kkumar@exathink.com>
2020-11-19 16:00:46 -0600

    Fix some old broken styles that were throwing console errors in dev.
    [story_url=https://urjuna.atlassian.net/browse/PO-321]

--- src/app/dashboards/shared/containers/flex/rowNoOverflow.js
+++ src/app/dashboards/shared/containers/flex/rowNoOverflow.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Flex} from 'reflexbox';
 
 export const RowNoOverflow = props => (
-  <Flex title={props.title} style={{'white-space': 'nowrap', 'overflow': 'hidden', 'text-overflow':'ellipsis'}} {...props}>
+  <Flex title={props.title} style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow:'ellipsis'}} {...props}>
     {props.children}
   </Flex>
 );
