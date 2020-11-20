672eacb059ed511ae9608ce0e3ff7816735cd3f9
Aman Mavai <aman.mavai@gslab.com>
2020-11-20 13:02:07 +0530

    click event handling for pullrequest

--- src/app/dashboards/work_items/activity/eventTimeline/workItemEventTimelineView.js
+++ src/app/dashboards/work_items/activity/eventTimeline/workItemEventTimelineView.js
@@ -10,6 +10,17 @@ export const WorkItemEventTimelineView = (
   }
 ) => {
 
+  function handlePointClick(workItemEvents) {
+    const workItemEvent = workItemEvents.length === 1 ? workItemEvents[0] : null;
+    if (workItemEvent != null) {
+      if (workItemEvent.event.type === "commit") {
+        context.navigate(Commits, workItemEvent.event.name, workItemEvent.event.key);
+      }
+      if (workItemEvent.event.type === "pullRequest") {
+        window.open(workItemEvent.event.webUrl, "_blank");
+      }
+    }
+  }
 
   return (
     <VizRow h={1}>
@@ -18,13 +29,7 @@ export const WorkItemEventTimelineView = (
           workItem={workItem}
           context={context}
           view={view}
-          onSelectionChange={
-            (workItemEvents) => {
-              if (workItemEvents.length === 1 && workItemEvents[0].event.type === 'commit') {
-                context.navigate(Commits, workItemEvents[0].event.name, workItemEvents[0].event.key)
-              }
-            }
-          }
+          onSelectionChange={handlePointClick}
         />
       </VizItem>
     </VizRow>
