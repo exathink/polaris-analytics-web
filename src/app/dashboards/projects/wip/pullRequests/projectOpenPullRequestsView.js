19c31233f88641573d95f34b2fa33be452a65523
Krishna Kumar <kkumar@exathink.com>
2020-11-21 17:07:18 -0600

    Add a stats view of open pull requests and use that for the primary view of open pull requests.
    [ story_url=https://urjuna.atlassian.net/browse/PO-385]

--- src/app/dashboards/projects/wip/pullRequests/projectOpenPullRequestsView.js
+++ src/app/dashboards/projects/wip/pullRequests/projectOpenPullRequestsView.js
@@ -1,27 +1,55 @@
 import React from "react";
-import { VizItem, VizRow } from "../../../shared/containers/layout";
-import { WorkItemScopeSelector } from "../../shared/components/workItemScopeSelector";
-import { PullRequestAgeChart } from "./pullRequestAgeChart";
-import { Flex } from "reflexbox";
-import { navigateToPullRequest } from "../../../shared/navigation/navigate";
+import {VizItem, VizRow} from "../../../shared/containers/layout";
+import {PullRequestAgeChart} from "./pullRequestAgeChart";
+import {navigateToPullRequest} from "../../../shared/navigation/navigate";
+import {TrendIndicator} from "../../../../components/misc/statistic/statistic";
+import {FlowStatistic} from "../../../shared/components/flowStatistics/flowStatistics";
+import {average} from "../../../../helpers/utility";
 
-export const ProjectOpenPullRequestsView = ({
-  pullRequests,
-
-  view,
-}) => (
+const ProjectOpenPullRequestsStatsView = ({title, pullRequests, view}) => (
   <VizRow h={1}>
-    <VizItem w={1}>
-      <PullRequestAgeChart
-        pullRequests={pullRequests}
-        title={"Pending Code Reviews"}
-        view={view}
-        onSelectionChange={(pullRequests) => {
-          if (pullRequests.length === 1) {
-            navigateToPullRequest(pullRequests[0].webUrl)
-          }
-        }}
+    <VizItem w={1 / 2}>
+      <FlowStatistic
+        title={title || <span>{"Pending"}</span>}
+        currentValue={(pullRequests && pullRequests.length) || 0}
+        good={TrendIndicator.isNegative}
+      />
+    </VizItem>
+    <VizItem w={1 / 2}>
+      <FlowStatistic
+        title={
+          title || (
+            <span>
+              {"Age"}
+              <sup> Avg </sup>
+            </span>
+          )
+        }
+        currentValue={average(pullRequests, (pullRequest) => pullRequest.age)}
+        uom={"Days"}
+        precision={1}
+        good={TrendIndicator.isNegative}
       />
     </VizItem>
   </VizRow>
 );
+
+export const ProjectOpenPullRequestsView = ({pullRequests, view}) =>
+  view === "primary" ? (
+    <ProjectOpenPullRequestsStatsView pullRequests={pullRequests} view={view} />
+  ) : (
+    <VizRow h={1}>
+      <VizItem w={1}>
+        <PullRequestAgeChart
+          pullRequests={pullRequests}
+          title={"Pending Code Reviews"}
+          view={view}
+          onSelectionChange={(pullRequests) => {
+            if (pullRequests.length === 1) {
+              navigateToPullRequest(pullRequests[0].webUrl);
+            }
+          }}
+        />
+      </VizItem>
+    </VizRow>
+  );
