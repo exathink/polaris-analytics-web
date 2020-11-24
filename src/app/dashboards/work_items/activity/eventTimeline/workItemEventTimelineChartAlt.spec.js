5d67e0909a7c15d8d581d1923a068a41f3683ad8
Aman Mavai <aman.mavai@gslab.com>
2020-11-24 16:20:07 +0530

    added unit tests for workItems and pullrequests

--- src/app/dashboards/work_items/activity/eventTimeline/workItemEventTimelineChartAlt.spec.js
+++ src/app/dashboards/work_items/activity/eventTimeline/workItemEventTimelineChartAlt.spec.js
@@ -108,6 +108,87 @@ const commitsFixture = [
   },
 ];
 
+const workItemsFixture = [
+  {
+    id: "V29ya0l0ZW1FdmVudDo2NWUwMTVmYS00YjkyLTQ1NTYtYjBmYi05ZjI1NzExMDM2Yzk6MA==",
+    state: "Code-Review-Needed",
+    eventDate: "2020-11-01T22:26:23.731000",
+    previousState: null,
+    previousStateType: null,
+    newState: "created",
+    newStateType: "backlog",
+  },
+  {
+    id: "V29ya0l0ZW1FdmVudDo2NWUwMTVmYS00YjkyLTQ1NTYtYjBmYi05ZjI1NzExMDM2Yzk6MQ==",
+    state: "Code-Review-Needed",
+    eventDate: "2020-11-03T00:18:25.814000",
+    previousState: "created",
+    previousStateType: "backlog",
+    newState: "In Progress",
+    newStateType: "wip",
+  },
+  {
+    id: "V29ya0l0ZW1FdmVudDo2NWUwMTVmYS00YjkyLTQ1NTYtYjBmYi05ZjI1NzExMDM2Yzk6Mg==",
+    state: "Code-Review-Needed",
+    eventDate: "2020-11-09T14:52:29.562000",
+    previousState: "In Progress",
+    previousStateType: "wip",
+    newState: "Code-Review-Needed",
+    newStateType: "wip",
+  },
+];
+
+const pullRequestsFixture = [
+  {
+    id: "UHVsbFJlcXVlc3Q6NWMzNzU5OTYtY2VlZi00MGQ4LTlkY2YtMmI3MWE3ODk3NWYw",
+    name: "Added end_date and closed_at",
+    key: "5c375996-ceef-40d8-9dcf-2b71a78975f0",
+    displayId: "10",
+    state: "merged",
+    age: 1.48609416666667,
+    createdAt: "2020-11-09T12:04:14.517000",
+    endDate: "2020-11-10T23:44:13.505000",
+    repositoryName: "polaris-repos-db",
+    branchName: "PO-357",
+  },
+  {
+    id: "UHVsbFJlcXVlc3Q6YjU2YWEzOTMtN2M3Mi00NDRiLTlhZDItZGEzMmRlZTgxMWQ0",
+    name: "Resolve PO-357",
+    key: "b56aa393-7c72-444b-9ad2-da32dee811d4",
+    displayId: "125",
+    state: "merged",
+    age: 5.36025982638889,
+    createdAt: "2020-11-05T15:03:27.846000",
+    endDate: "2020-11-10T23:42:14.964000",
+    repositoryName: "polaris-analytics-service",
+    branchName: "PO-357",
+  },
+  {
+    id: "UHVsbFJlcXVlc3Q6OWY2ZmY4OWMtMGRhYy00MTNlLTg0NzAtZWMzNmU2OWI3MGMz",
+    name: "Resolve PO-357",
+    key: "9f6ff89c-0dac-413e-8470-ec36e69b70c3",
+    displayId: "11",
+    state: "merged",
+    age: 5.36438644675926,
+    createdAt: "2020-11-05T15:00:27.749000",
+    endDate: "2020-11-10T23:45:11.238000",
+    repositoryName: "polaris-vcs-service",
+    branchName: "PO-357",
+  },
+  {
+    id: "UHVsbFJlcXVlc3Q6ZDljODJlZDktY2Q1NC00NmZlLWJkYjEtMTg3NjI3NWNiMTA4",
+    name: "Added closed_at field to pull request summary message",
+    key: "d9c82ed9-cd54-46fe-bdb1-1876275cb108",
+    displayId: "10",
+    state: "merged",
+    age: 5.36403539351852,
+    createdAt: "2020-11-05T14:59:23.097000",
+    endDate: "2020-11-10T23:43:36.218000",
+    repositoryName: "polaris-messaging",
+    branchName: "PO-357",
+  },
+];
+
 const commonChartProps = {
   backgroundColor: Colors.Chart.backgroundColor,
   zoomType: "xy",
@@ -240,6 +321,315 @@ describe("workItemEventTimelineChart", () => {
       expectSetsAreEqual(
         data.map((point) => point.workItem),
         [workItemWithCommits]
+      );
+    });
+  });
+
+  describe("workItems timeline", () => {
+    const _workItemsFixture = workItemsFixture.filter((e) =>
+      workItemFixture.stateType !== "closed" ? e.newStateType !== "backlog" : true
+    );
+    const workItemWithEvents = {
+      ...workItemFixture,
+      workItemEvents: _workItemsFixture,
+    };
+
+    const {
+      series: [{data}],
+    } = renderedChartConfig(<WorkItemEventsTimelineChart workItem={workItemWithEvents} view={"primary"} />);
+
+    test("it renders a chart with the correct numbers of data points ", () => {
+      expect(data).toHaveLength(_workItemsFixture.length);
+    });
+
+    test("it maps workItem dates to the x axis and sets y = 0 for each workItem", () => {
+      expectSetsAreEqual(
+        data.map((point) => [point.x, point.y]),
+        _workItemsFixture.map((workItem) => [epoch(workItem.eventDate), 0])
+      );
+    });
+
+    test("it sets the marker symbol for  unmapped and backlog workItem dates to triangle ", () => {
+      const workItemWithUnmappedBacklogEvents = {
+        ...workItemFixture,
+        workItemEvents: [
+          {
+            id: "V29ya0l0ZW1FdmVudDo2NWUwMTVmYS00YjkyLTQ1NTYtYjBmYi05ZjI1NzExMDM2Yzk6MA==",
+            state: "Code-Review-Needed",
+            eventDate: "2020-11-01T22:26:23.731000",
+            previousState: null,
+            previousStateType: null,
+            newState: "created",
+            newStateType: "backlog",
+          },
+          {
+            id: "V29ya0l0ZW1FdmVudDo4MGYwOWNjYi0yZTc0LTRjNzAtOTJkNC01ZDM4YmQ1MGRiMGU6MA==",
+            state: "Backlog",
+            eventDate: "2019-12-16T19:37:58.539000",
+            previousState: null,
+            previousStateType: null,
+            newState: "created",
+            newStateType: null // unmapped
+          },
+        ],
+      };
+
+      const {
+        series: [{data}],
+      } = renderedChartConfig(
+        <WorkItemEventsTimelineChart workItem={workItemWithUnmappedBacklogEvents} view={"primary"} />
+      );
+
+      expectSetsAreEqual(
+        data.map((point) => point.marker.symbol),
+        ["triangle"]
+      );
+    });
+
+    test("it sets the marker symbol for  open, wip and complete workItem dates to diamond ", () => {
+      const workItemWithOpenWipCompleteEvents = {
+        ...workItemFixture,
+        workItemEvents: [
+          {
+            id: "V29ya0l0ZW1FdmVudDpkYTI2MmYyYi02YWNkLTQyNjEtOTRjMi02YTI5NjU4MTRiMmM6MQ==",
+            state: "open",
+            eventDate: "2019-02-08T10:31:45",
+            previousState: "created",
+            previousStateType: "backlog",
+            newState: "open",
+            newStateType: "open",
+          },
+          {
+            id: "V29ya0l0ZW1FdmVudDo2NWUwMTVmYS00YjkyLTQ1NTYtYjBmYi05ZjI1NzExMDM2Yzk6MQ==",
+            state: "Code-Review-Needed",
+            eventDate: "2020-11-03T00:18:25.814000",
+            previousState: "created",
+            previousStateType: "backlog",
+            newState: "In Progress",
+            newStateType: "wip",
+          },
+          {
+            id: "V29ya0l0ZW1FdmVudDo5ZWI0MmJjNS01MjYwLTQ3YTEtOGM5Ny05YTk2YWNiNjA0NDI6MQ==",
+            state: "ABANDONED",
+            eventDate: "2020-08-19T16:06:32.793000",
+            previousState: "created",
+            previousStateType: "backlog",
+            newState: "ABANDONED",
+            newStateType: "complete",
+          },
+        ],
+      };
+
+      const {
+        series: [{data}],
+      } = renderedChartConfig(
+        <WorkItemEventsTimelineChart workItem={workItemWithOpenWipCompleteEvents} view={"primary"} />
+      );
+
+      expectSetsAreEqual(
+        data.map((point) => point.marker.symbol),
+        ["diamond"]
+      );
+    });
+
+    test("it sets the marker symbol for  closed workItem dates to triangle-down", () => {
+      const workItemWithClosedEvents = {
+        ...workItemFixture,
+        workItemEvents: [
+          {
+            id: "V29ya0l0ZW1FdmVudDo5ZGM2MDcxZi0yNzQzLTQ2NmUtODE5OC0zNzc0NzZmZTA5Yzk6MQ==",
+            state: "accepted",
+            eventDate: "2019-09-18T16:56:53",
+            previousState: "created",
+            previousStateType: "backlog",
+            newState: "accepted",
+            newStateType: "closed",
+          },
+        ],
+      };
+
+      const {
+        series: [{data}],
+      } = renderedChartConfig(<WorkItemEventsTimelineChart workItem={workItemWithClosedEvents} view={"primary"} />);
+
+      expectSetsAreEqual(
+        data.map((point) => point.marker.symbol),
+        ["triangle-down"]
+      );
+    });
+
+    test("it sets the event type field for each point ", () => {
+      expectSetsAreEqual(
+        data.map((point) => point.eventType),
+        ["WorkItemEvent"]
+      );
+    });
+
+    test("it sets the reference to the work item for each point ", () => {
+      expectSetsAreEqual(
+        data.map((point) => point.workItem),
+        [workItemWithEvents]
+      );
+    });
+  });
+
+  describe("pullRequests timeline", () => {
+    const workItemWithPullRequests = {
+      ...workItemFixture,
+      workItemPullRequests: pullRequestsFixture,
+    };
+
+    const {
+      series: [{data}],
+    } = renderedChartConfig(<WorkItemEventsTimelineChart workItem={workItemWithPullRequests} view={"primary"} />);
+
+    test("it renders a chart with the correct numbers of data points ", () => {
+      expect(data).toHaveLength(
+        pullRequestsFixture.flatMap((p) => [[p.createdAt], ...(p.endDate != null ? [p.endDate] : [])]).length
+      );
+    });
+
+    test("it maps pull request event dates to the x axis and sets y = 2 for each event", () => {
+      expectSetsAreEqual(
+        data.map((point) => [point.x, point.y]),
+        pullRequestsFixture.flatMap((pullRequest) => [
+          [epoch(pullRequest.createdAt), 2],
+          [...(pullRequest.endDate != null ? [epoch(pullRequest.endDate), 2] : [])],
+        ])
+      );
+    });
+
+    test("it sets the marker symbol for  pull request create event dates to triangle", () => {
+      const workItemWithOpenPullRequests = {
+        ...workItemFixture,
+        workItemPullRequests: [
+          {
+            id: "UHVsbFJlcXVlc3Q6MmI1ZTdhMTktNjYwMC00MmIyLTkzYTctODA4OGYwYzQ3OGRk",
+            name: "Resolve PO-364",
+            key: "2b5e7a19-6600-42b2-93a7-8088f0c478dd",
+            displayId: "128",
+            state: "open",
+            age: 13.8181293821991,
+            createdAt: "2020-11-10T10:59:14.586000",
+            endDate: null,
+            repositoryName: "polaris-analytics-service",
+            branchName: "PO-364",
+          },
+        ],
+      };
+
+      const {
+        series: [{data}],
+      } = renderedChartConfig(<WorkItemEventsTimelineChart workItem={workItemWithOpenPullRequests} view={"primary"} />);
+
+      expectSetsAreEqual(
+        data.filter((point) => point.eventType === "PullRequestCreated").map((point) => point.marker.symbol),
+        ["triangle"]
+      );
+    });
+
+    test("it sets the marker symbol for  pull request complete event dates to triangle-down", () => {
+      const workItemWithCompletedPullRequests = {
+        ...workItemFixture,
+        workItemPullRequests: [
+          {
+            id: "UHVsbFJlcXVlc3Q6ZDljODJlZDktY2Q1NC00NmZlLWJkYjEtMTg3NjI3NWNiMTA4",
+            name: "Added closed_at field to pull request summary message",
+            key: "d9c82ed9-cd54-46fe-bdb1-1876275cb108",
+            displayId: "10",
+            state: "merged",
+            age: 5.36403539351852,
+            createdAt: "2020-11-05T14:59:23.097000",
+            endDate: "2020-11-10T23:43:36.218000",
+            repositoryName: "polaris-messaging",
+            branchName: "PO-357",
+          },
+        ],
+      };
+
+      const {
+        series: [{data}],
+      } = renderedChartConfig(
+        <WorkItemEventsTimelineChart workItem={workItemWithCompletedPullRequests} view={"primary"} />
+      );
+
+      expectSetsAreEqual(
+        data.filter((point) => point.eventType === "PullRequestCompleted").map((point) => point.marker.symbol),
+        ["triangle-down"]
+      );
+    });
+
+    test("it sets the color(#c4ab49) of events for pull requests that are still open", () => {
+      const workItemWithOpenPullRequests = {
+        ...workItemFixture,
+        workItemPullRequests: [
+          {
+            id: "UHVsbFJlcXVlc3Q6MmI1ZTdhMTktNjYwMC00MmIyLTkzYTctODA4OGYwYzQ3OGRk",
+            name: "Resolve PO-364",
+            key: "2b5e7a19-6600-42b2-93a7-8088f0c478dd",
+            displayId: "128",
+            state: "open",
+            age: 13.8181293821991,
+            createdAt: "2020-11-10T10:59:14.586000",
+            endDate: null,
+            repositoryName: "polaris-analytics-service",
+            branchName: "PO-364",
+          },
+        ],
+      };
+
+      const {
+        series: [{data}],
+      } = renderedChartConfig(<WorkItemEventsTimelineChart workItem={workItemWithOpenPullRequests} view={"primary"} />);
+
+      expectSetsAreEqual(
+        data.map((point) => point.color),
+        ["#c4ab49"]
+      );
+    });
+
+    test("it sets the color(#7824b5) of events for pull requests that are completed", () => {
+      const workItemWithCompletedPullRequests = {
+        ...workItemFixture,
+        workItemPullRequests: [
+          {
+            id: "UHVsbFJlcXVlc3Q6ZDljODJlZDktY2Q1NC00NmZlLWJkYjEtMTg3NjI3NWNiMTA4",
+            name: "Added closed_at field to pull request summary message",
+            key: "d9c82ed9-cd54-46fe-bdb1-1876275cb108",
+            displayId: "10",
+            state: "merged",
+            age: 5.36403539351852,
+            createdAt: "2020-11-05T14:59:23.097000",
+            endDate: "2020-11-10T23:43:36.218000",
+            repositoryName: "polaris-messaging",
+            branchName: "PO-357",
+          },
+        ],
+      };
+
+      const {
+        series: [{data}],
+      } = renderedChartConfig(
+        <WorkItemEventsTimelineChart workItem={workItemWithCompletedPullRequests} view={"primary"} />
+      );
+
+      expectSetsAreEqual(
+        data.map((point) => point.color),
+        ["#7824b5"]
+      );
+    });
+
+    test("it sets the event type field for each point ", () => {
+      expectSetsAreEqual(
+        data.map((point) => point.eventType),
+        ["PullRequestCreated", "PullRequestCompleted"]
+      );
+    });
+
+    test("it sets the reference to the work item for each point ", () => {
+      expectSetsAreEqual(
+        data.map((point) => point.workItem),
+        [workItemWithPullRequests]
       );
     });
   });
