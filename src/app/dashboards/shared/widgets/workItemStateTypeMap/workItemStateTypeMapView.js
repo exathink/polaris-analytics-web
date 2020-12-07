1c95f52c6fcf44f8f5906134433005023e4e55c4
Aman Mavai <aman.mavai@gslab.com>
2020-12-07 20:07:18 +0530

    added dropdown for workItemsSource selection
    also added save and cancel button.

--- src/app/dashboards/shared/widgets/workItemStateTypeMap/workItemStateTypeMapView.js
+++ src/app/dashboards/shared/widgets/workItemStateTypeMap/workItemStateTypeMapView.js
@@ -1,18 +1,106 @@
 import React from "react";
 import {VizItem, VizRow} from "../../containers/layout";
 import {WorkItemStateTypeMapChart} from "./workItemStateTypeMapChart";
+import {Button, Select} from "antd";
+import "./workItemStateType.css";
+import {updateProjectWorkItemSourceStateMaps} from "../../../projects/shared/hooks/useQueryProjectWorkItemsSourceStateMappings";
+const {Option} = Select;
 
+export const actionTypes = {
+  REPLACE_WORKITEM_SOURCE: "REPLACE_WORKITEM_SOURCE",
+  UPDATE_WORKITEM_SOURCE: "UPDATE_WORKITEM_SOURCE"
+}
 
-export const WorkItemStateTypeMapView = ({initialStateTypeMapping, view, context}) => {
-  const [draftState, setDraftState] = React.useState({});
-  // console.log(draftState); // this draftState captures all the changes.
+function workItemReducer(state, action) {
+  switch (action.type) {
+    case actionTypes.REPLACE_WORKITEM_SOURCE: {
+      return {
+        ...action.payload,
+      };
+    }
+    case actionTypes.UPDATE_WORKITEM_SOURCE: {
+      // mutating in-place, as we don't want to rerender
+      const [[key, value]] = Object.entries(action.payload.keyValuePair);
+      state.workItemStateMappings.forEach((item) => {
+        if (item.state === key) {
+          item.stateType = value;
+        }
+      });
+
+      return state;
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
+export const WorkItemStateTypeMapView = ({workItemSources, instanceKey, view, context}) => {
+  const [mutate] = updateProjectWorkItemSourceStateMaps();
+  
+  function getFreshWorkItemSources() {
+    // as we are not modifying original workItemSources, get fresh copy every time
+    return JSON.parse(JSON.stringify(workItemSources));
+  }
+
+  // set first workitemsource as default
+  const [workItemSource] = getFreshWorkItemSources();
+  const [state, dispatch] = React.useReducer(workItemReducer, workItemSource);
+
+  function handleSaveClick(e) {
+    // call the mutation function to update data from here
+    const payload = [
+      {
+        workItemsSourceKey: state.key,
+        stateMaps: state.workItemStateMappings,
+      },
+    ];
+
+    // call mutation here
+    mutate({variables: {projectKey: instanceKey, workItemsSourceStateMaps: payload}});
+  }
+
+  // Reset state on cancel
+  function handleCancelClick(e) {
+    const workItemSource = getFreshWorkItemSources().find((x) => x.key === state.key);
+    dispatch({type: actionTypes.REPLACE_WORKITEM_SOURCE, payload: workItemSource});
+  }
+
+  // currently not maintaining state when dropdown value for workItemSource change
+  function handleChange(key) {
+    const workItemSource = getFreshWorkItemSources().find((x) => x.key === key);
+    dispatch({type: actionTypes.REPLACE_WORKITEM_SOURCE, payload: workItemSource});
+  }
+
+  function selectDropdown() {
+    return (
+      <div>
+        <div className="workItemSourceLabel">Select WorkItems Source</div>
+        <Select defaultValue={state.name} style={{width: 220}} onChange={handleChange}>
+          {workItemSources.map((source) => (
+            <Option value={source.key}>{source.name}</Option>
+          ))}
+        </Select>
+      </div>
+    );
+  }
+
   return (
     <VizRow h={"100%"}>
       <VizItem w={1}>
         <div style={{width: "100%", height: "100%"}}>
+          <div className="workItemFlex spacex12 my12">
+            {selectDropdown()}
+            <Button onClick={handleSaveClick} className={"workItemSave"}>
+              Save
+            </Button>
+            <Button onClick={handleCancelClick} className={"workItemCancel"}>
+              Cancel
+            </Button>
+          </div>
           <WorkItemStateTypeMapChart
-            initialStateTypeMapping={initialStateTypeMapping}
-            setDraftState={setDraftState}
+            workItemStateMappings={state.workItemStateMappings}
+            updateDraftState={dispatch}
             view={view}
             context={context}
             title={" "}
--- src/app/dashboards/shared/widgets/workItemStateTypeMap/workItemStateTypeMapWidget.js
+++ src/app/dashboards/shared/widgets/workItemStateTypeMap/workItemStateTypeMapWidget.js
@@ -1,36 +1,17 @@
 import React from "react";
+import {useQueryProjectWorkItemsSourceStateMappings} from "../../../projects/shared/hooks/useQueryProjectWorkItemsSourceStateMappings";
 import {WorkItemStateTypeMapView} from "./workItemStateTypeMapView";
+import {Loading} from "../../../../components/graphql/loading";
 
-// get this mapping from api finally
-const initialStateTypeMapping = {
-  workItemsSourceKey: "46694f4f-e003-4430-a7a7-e4f288f40d22",
-  stateMaps: [
-    {state: "created", stateType: "unmapped"},
-    {state: "Backlog", stateType: "unmapped"},
-    {state: "Selected for Development", stateType: "unmapped"},
-    {state: "DESIGN", stateType: "unmapped"},
-    {state: "READY-FOR-DEVELOPMENT", stateType: "unmapped"},
-    {state: "In Progress", stateType: "unmapped"},
-    {state: "Code-Review-Needed", stateType: "unmapped"},
-    {state: "DEV-DONE", stateType: "unmapped"},
-    {state: "ACCEPTED", stateType: "unmapped"},
-    {state: "REJECTED", stateType: "unmapped"},
-    {state: "Done", stateType: "unmapped"},
-    {state: "ABANDONED", stateType: "unmapped"},
-    {state: "DEPLOYED-TO-STAGING", stateType: "unmapped"},
-    {state: "RELEASED", stateType: "unmapped"},
-    {state: "ROADMAP", stateType: "unmapped"},
-    {state: "Closed", stateType: "unmapped"},
-  ],
-  allStateTypes: [
-    {key: "unmapped", displayValue: "Unmapped"},
-    {key: "backlog", displayValue: "Define"},
-    {key: "wip", displayValue: "Build"},
-    {key: "complete", displayValue: "Deliver"},
-    {key: "open", displayValue: "Open"},
-    {key: "closed", displayValue: "Closed"},
-  ],
-};
+// can also come from backend
+export const ALL_STATE_TYPES = [
+  {key: "unmapped", displayValue: "Unmapped"},
+  {key: "backlog", displayValue: "Define"},
+  {key: "wip", displayValue: "Build"},
+  {key: "complete", displayValue: "Deliver"},
+  {key: "open", displayValue: "Open"},
+  {key: "closed", displayValue: "Closed"},
+];
 
 export const WorkItemStateTypeMapWidget = ({
   instanceKey,
@@ -41,5 +22,11 @@ export const WorkItemStateTypeMapWidget = ({
   context,
   pollInterval,
 }) => {
-  return <WorkItemStateTypeMapView initialStateTypeMapping={initialStateTypeMapping} context={context} view={view} />;
+  const {loading, error, data} = useQueryProjectWorkItemsSourceStateMappings({instanceKey});
+  if (loading) return <Loading />;
+  if (error) return "Boom";
+
+  const workItemSources = data["project"]["workItemsSources"]["edges"].map((e) => e.node);
+
+  return <WorkItemStateTypeMapView instanceKey={instanceKey} workItemSources={workItemSources} context={context} view={view} />;
 };
