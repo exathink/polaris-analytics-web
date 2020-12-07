04c3ca60cf4e2e7d102d4c5df87cedcd7691507c
Aman Mavai <aman.mavai@gslab.com>
2020-12-07 20:04:50 +0530

    add updateDraftState instead of setDraftState

--- src/app/dashboards/shared/widgets/workItemStateTypeMap/workItemStateTypeMapChart.js
+++ src/app/dashboards/shared/widgets/workItemStateTypeMap/workItemStateTypeMapChart.js
@@ -2,33 +2,23 @@
 This file is a template for creating new chart components. Not intended to be used directly,
 use it as a guide on how to structure a new chart component file. Copy/Paste modify as needed.
  */
-import {Chart} from "../../../../framework/viz/charts";
+import {Chart, Highcharts} from "../../../../framework/viz/charts";
 import {DefaultSelectionEventHandler} from "../../../../framework/viz/charts/eventHandlers/defaultSelectionHandler";
-import {WorkItemStateTypeColor, Colors} from "../../config";
-
-export const Highcharts = require("highcharts/highstock");
+import {WorkItemStateTypeColor} from "../../config";
+import { actionTypes } from "./workItemStateTypeMapView";
+import {ALL_STATE_TYPES} from "./workItemStateTypeMapWidget";
 require("highcharts/modules/draggable-points")(Highcharts);
 
-function getSeries({stateMaps, allStateTypes}) {
-  const allStateTypeKeys = allStateTypes.map((x) => x.key);
+function getAllStateTypeKeys() {
+  return ALL_STATE_TYPES.map((x) => x.key);
+}
+
+function getSeries(workItemStateMappings) {
+  const allStateTypeKeys = getAllStateTypeKeys();
 
   return [
-    // leaving the commented code here, till its finalized.
-    // {
-    //   name: "UnMapped States",
-    //   type: "column",
-    //   data: stateMaps.map((item) => {
-    //     return {
-    //       name: item.state,
-    //       y: 1.2,
-    //       x: allStateTypeKeys.indexOf(item.stateType),
-    //       color: WorkItemStateTypeColor[item.stateType],
-    //     };
-    //   }),
-    // },
-    
     // creating a series for each item to resolve the stacking issue
-    ...stateMaps.map((item) => {
+    ...workItemStateMappings.map((item) => {
       return {
         name: item.state,
         type: "column",
@@ -47,7 +37,7 @@ function getSeries({stateMaps, allStateTypes}) {
       name: "State Types",
       type: "column",
       showInLegend: false,
-      data: allStateTypes.map(({key, displayValue}, index) => {
+      data: ALL_STATE_TYPES.map(({key, displayValue}, index) => {
         return {
           name: displayValue,
           y: 1.2,
@@ -67,15 +57,30 @@ function getSeries({stateMaps, allStateTypes}) {
   ];
 }
 
+function sanitizeStateMappings(workItemStateMappings) {
+  const allStateTypeKeys = getAllStateTypeKeys();
+  const [{key: unMappedKey}] = ALL_STATE_TYPES;
+
+  return workItemStateMappings.map((x) => {
+    if (!allStateTypeKeys.includes(x.stateType)) {
+      return {...x, stateType: unMappedKey};
+    }
+    return x;
+  });
+}
+
 export const WorkItemStateTypeMapChart = Chart({
   chartUpdateProps: (props) => props,
   eventHandler: DefaultSelectionEventHandler,
   mapPoints: (points, _) => points.map((point) => point),
 
-  getConfig: ({initialStateTypeMapping, setDraftState, title, subtitle, intl, view}) => {
-    const series = getSeries(initialStateTypeMapping);
-    const allStateTypeKeys = initialStateTypeMapping.allStateTypes.map((x) => x.key);
-    const allStateTypeDisplayValues = initialStateTypeMapping.allStateTypes.map((x) => x.displayValue);
+  getConfig: ({workItemStateMappings, updateDraftState, title, subtitle, intl, view}) => {
+    // cleanup workItemStateMappings
+    const stateMappings = sanitizeStateMappings(workItemStateMappings);
+    const series = getSeries(stateMappings);
+
+    const allStateTypeKeys = getAllStateTypeKeys();
+    const allStateTypeDisplayValues = ALL_STATE_TYPES.map((x) => x.displayValue);
 
     return {
       chart: {
@@ -107,18 +112,13 @@ export const WorkItemStateTypeMapChart = Chart({
           point: {
             events: {
               drop: function (e) {
-                // this is workaround till we find better solution.
-                const currentMappingData = e.target.series.chart.series.filter(s => s.name!=="State Types").flatMap(s => s.data).reduce((acc, p) => {
-                  acc[p.name] = p.category;
-                  return acc;
-                }, {});
-
                 const newCategoryIndex = e.newPoint.x;
                 const newPointName = e.newPoints[this.id].point.name;
 
                 // update dropped point here
-                currentMappingData[newPointName] = allStateTypeKeys[newCategoryIndex];
-                setDraftState(currentMappingData);
+                const keyValuePair = {};
+                keyValuePair[newPointName] = allStateTypeKeys[newCategoryIndex];
+                updateDraftState({type: actionTypes.UPDATE_WORKITEM_SOURCE, payload: {keyValuePair}});
               },
             },
           },
