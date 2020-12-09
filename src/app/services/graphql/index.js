e1311dc39a8b756a201f2ee652475683e28fa282
Aman Mavai <aman.mavai@gslab.com>
2020-12-09 23:52:49 +0530

    adding possibleTypes

--- src/app/services/graphql/index.js
+++ src/app/services/graphql/index.js
@@ -1,18 +1,14 @@
 // GraphQL Client Setup
-import React from 'react';
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-} from "@apollo/client";
+import React from "react";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
-import analyticsFragmentTypes from '../../../config/graphql/analyticsFragmentTypes.json';
+import analyticsFragmentTypes from "../../../config/graphql/analyticsFragmentTypes.json";
 import workTrackingFragmentTypes from "../../../config/graphql/workTrackingFragmentTypes.json";
 import vcsFragmentTypes from "../../../config/graphql/vcsFragmentTypes.json";
 
 import {GRAPHQL_ANALYTICS_URL, GRAPHQL_WORK_TRACKING_URL, GRAPHQL_VCS_URL} from "../../../config/url";
 
-export const defaultPollInterval = service => {
+export const defaultPollInterval = (service) => {
   return 0;
 };
 
@@ -21,40 +17,45 @@ export const defaultPollInterval = service => {
     fragmentMatcher is supposed to be replaced by possibleTypes in latest version
     for more details refer: https://github.com/apollographql/apollo-client/pull/5073
  */
+const analyticsPossibleTypes = analyticsFragmentTypes.__schema.types.reduce((acc, item) => {
+  acc[item.name] = item.possibleTypes.map((x) => x.name);
+  return acc;
+}, {});
 export const analytics_service = new ApolloClient({
   cache: new InMemoryCache({
-    // possibleTypes:analyticsFragmentTypes
+    possibleTypes: analyticsPossibleTypes,
   }),
   uri: GRAPHQL_ANALYTICS_URL,
-  credentials: 'include',
+  credentials: "include",
 });
 analytics_service.defaultPollInterval = () => defaultPollInterval(analytics_service);
 
+const workTrackingPossibleTypes = workTrackingFragmentTypes.__schema.types.reduce((acc, item) => {
+  acc[item.name] = item.possibleTypes.map((x) => x.name);
+  return acc;
+}, {});
 export const work_tracking_service = new ApolloClient({
   cache: new InMemoryCache({
-    // possibleTypes: workTrackingFragmentTypes
+    possibleTypes: workTrackingPossibleTypes,
   }),
   uri: GRAPHQL_WORK_TRACKING_URL,
-  credentials: 'include',
+  credentials: "include",
 });
 analytics_service.defaultPollInterval = () => defaultPollInterval(work_tracking_service);
 
+const vcsPossibleTypes = vcsFragmentTypes.__schema.types.reduce((acc, item) => {
+  acc[item.name] = item.possibleTypes.map((x) => x.name);
+  return acc;
+}, {});
 export const vcs_service = new ApolloClient({
   cache: new InMemoryCache({
-    // possibleTypes: vcsFragmentTypes
+    possibleTypes: vcsPossibleTypes,
   }),
   uri: GRAPHQL_VCS_URL,
-  credentials: 'include',
+  credentials: "include",
 });
 vcs_service.defaultPollInterval = () => defaultPollInterval(vcs_service);
 
-
-export const DefaultApolloProvider = props => (
-  <ApolloProvider client={analytics_service}>
-    {props.children}
-  </ApolloProvider>
+export const DefaultApolloProvider = (props) => (
+  <ApolloProvider client={analytics_service}>{props.children}</ApolloProvider>
 );
-
-
-
-
