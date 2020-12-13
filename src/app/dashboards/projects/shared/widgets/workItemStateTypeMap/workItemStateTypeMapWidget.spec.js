8583fb97ed754a5cb46edbd8a21a95e3795bf9ab
Krishna Kumar <kkumar@exathink.com>
2020-12-12 19:23:38 -0600

    Add widget network and graphQl error tests.
    [PO-379]

--- src/app/dashboards/projects/shared/widgets/workItemStateTypeMap/workItemStateTypeMapWidget.spec.js
+++ src/app/dashboards/projects/shared/widgets/workItemStateTypeMap/workItemStateTypeMapWidget.spec.js
@@ -2,8 +2,11 @@ import React from "react";
 import {WorkItemStateTypeMapWidget} from "./workItemStateTypeMapWidget";
 import {GET_STATE_MAPPING_QUERY} from "../../hooks/useQueryProjectWorkItemsSourceStateMappings";
 import "@testing-library/jest-dom/extend-expect";
-import {renderComponentWithMockedProvider} from "../../../../../framework/viz/charts/chart-test-utils";
-import {waitFor, screen} from "@testing-library/react";
+import {renderComponentWithMockedProvider, gqlUtils} from "../../../../../framework/viz/charts/chart-test-utils";
+import {screen, waitFor} from "@testing-library/react";
+import {GraphQLError} from "graphql";
+
+
 
 const mocks = [
   {
@@ -165,7 +168,7 @@ describe("WorkItemStateTypeMapWidget", () => {
       },
     ];
 
-    test('it renders no data', async () => {
+    test("it renders no data", async () => {
       renderComponentWithMockedProvider(
         <WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />,
         mocksWithEmptyWorkItemSources
@@ -173,7 +176,7 @@ describe("WorkItemStateTypeMapWidget", () => {
 
       await screen.findByTestId("loading-spinner");
       await screen.findByTestId("no-data");
-    })
+    });
   });
 
   describe("when there is one work item source", () => {
@@ -204,7 +207,6 @@ describe("WorkItemStateTypeMapWidget", () => {
                       key: "a92d9cc9-25ba-4337-899f-cba7797a6c12",
                       name: "Polaris",
                       workItemStateMappings: [
-
                         {
                           state: "backlog",
                           stateType: "backlog",
@@ -245,36 +247,49 @@ describe("WorkItemStateTypeMapWidget", () => {
     ];
 
     test("it shows a loading spinner", async () => {
-      renderComponentWithMockedProvider(<WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />, mockWithSingleWorkItemSource);
+      renderComponentWithMockedProvider(
+        <WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />,
+        mockWithSingleWorkItemSource
+      );
       await screen.findByTestId("loading-spinner");
     });
 
     test("it does not shows a dropdown to select work item sources", async () => {
-      renderComponentWithMockedProvider(<WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />, mockWithSingleWorkItemSource);
+      renderComponentWithMockedProvider(
+        <WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />,
+        mockWithSingleWorkItemSource
+      );
       // need to wait until the view has rendered to test that the combo box is NOT displayed. Otherwise
       // it will pass trivially.
       await screen.findByTestId("state-type-map-view");
 
       expect(screen.queryByRole("combobox")).toBeNull();
-
     });
 
     test("it shows the chart title", async () => {
-      renderComponentWithMockedProvider(<WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />, mockWithSingleWorkItemSource);
+      renderComponentWithMockedProvider(
+        <WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />,
+        mockWithSingleWorkItemSource
+      );
       await screen.findByText(/Value Stream Mapping/i);
     });
 
     test("it shows the chart sub title", async () => {
-      renderComponentWithMockedProvider(<WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />, mockWithSingleWorkItemSource);
+      renderComponentWithMockedProvider(
+        <WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />,
+        mockWithSingleWorkItemSource
+      );
       await screen.findByText(/Drag/i);
     });
 
     test("it does not show the save/cancel button", async () => {
-      renderComponentWithMockedProvider(<WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />, mockWithSingleWorkItemSource);
-      await screen.findByTestId('state-type-map-view')
+      renderComponentWithMockedProvider(
+        <WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />,
+        mockWithSingleWorkItemSource
+      );
+      await screen.findByTestId("state-type-map-view");
       expect(screen.queryByRole("button")).toBeNull();
     });
-
   });
 
   describe("when there are multiple work item sources", () => {
@@ -311,8 +326,82 @@ describe("WorkItemStateTypeMapWidget", () => {
     test("it does not show the save/cancel button", async () => {
       renderComponentWithMockedProvider(<WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />, mocks);
       // need to wait for the view to render to check the button is NOT rendered. Otherwise it will pass trivially
-      await screen.findByTestId('state-type-map-view')
+      await screen.findByTestId("state-type-map-view");
       expect(screen.queryByRole("button")).toBeNull();
+    });
+  });
+
+  describe("when there are errors", () => {
+
+
+
+    // clear mocks after each test
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    const stateTypeWidgetPropsFixture = {
+      instanceKey: "41af8b92-51f6-4e88-9765-cc3dbea35e1a",
+      context: {},
+      latestWorkItemEvent: "2020-12-05T13:51:14.261000",
+      latestCommit: "2020-12-05T03:32:50",
+      days: 30,
+      view: "detail",
+    };
+    const mockNetworkError = [
+      {
+        request: {
+          query: GET_STATE_MAPPING_QUERY,
+          variables: {
+            projectKey: "41af8b92-51f6-4e88-9765-cc3dbea35e1a",
+          },
+        },
+        error: new Error("A network error Occurred"),
+      },
+    ];
+
+    const mockGraphQlErrors = [
+      {
+        request: {
+          query: GET_STATE_MAPPING_QUERY,
+          variables: {
+            projectKey: "41af8b92-51f6-4e88-9765-cc3dbea35e1a",
+          },
+        },
+        result: {
+          errors: [new GraphQLError("A GraphQL Error Occurred")],
+        },
+      },
+    ];
+
+    test("it renders nothing and logs the error when there is a network error", async () => {
+      const logGraphQlError = jest.spyOn(gqlUtils, 'logGraphQlError');
+
+      renderComponentWithMockedProvider(
+        <WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />,
+        mockNetworkError
+      );
+      await screen.findByTestId("loading-spinner");
+      await waitFor(() => expect(logGraphQlError).toHaveBeenCalled())
+      expect(screen.queryByTestId("state-type-map-view")).toBeNull();
+
+      logGraphQlError.mockRestore()
+
+    });
+
+    test("it renders nothing and logs the error when there is a GraphQl error", async () => {
+      const logGraphQlError = jest.spyOn(gqlUtils, 'logGraphQlError');
+
+      renderComponentWithMockedProvider(
+        <WorkItemStateTypeMapWidget {...stateTypeWidgetPropsFixture} />,
+        mockGraphQlErrors
+      );
+      await screen.findByTestId("loading-spinner");
+      await waitFor(() => expect(logGraphQlError).toHaveBeenCalled())
+      expect(screen.queryByTestId("state-type-map-view")).toBeNull();
+
+      logGraphQlError.mockRestore()
+
     });
   });
 });
