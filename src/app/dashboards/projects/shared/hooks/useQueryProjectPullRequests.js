8d4369d38e17bcf4cf37530073cc279266434479
Aman Mavai <aman.mavai@gslab.com>
2020-12-21 19:18:54 +0530

    add project pull requests widget tests

--- src/app/dashboards/projects/shared/hooks/useQueryProjectPullRequests.js
+++ src/app/dashboards/projects/shared/hooks/useQueryProjectPullRequests.js
@@ -1,55 +1,43 @@
-import { useQuery, gql } from "@apollo/client";
-import { analytics_service } from "../../../../services/graphql";
+import {useQuery, gql} from "@apollo/client";
+import {analytics_service} from "../../../../services/graphql";
 
-export function useQueryProjectPullRequests({
-  instanceKey,
-  activeOnly,
-  referenceString,
-}) {
-  return useQuery(
-    gql`
-      query getProjectPullRequests(
-        $projectKey: String!
-        $activeOnly: Boolean
-        $referenceString: String
-      ) {
-        project(key: $projectKey, referenceString: $referenceString) {
-          id
-          pullRequests(
-            interfaces: [BranchRef, WorkItemsSummaries]
-            activeOnly: $activeOnly
-          ) {
-            edges {
-              node {
-                id
-                name
-                key
-                displayId
-                state
-                repositoryKey
-                repositoryName
-                age
-                webUrl
-                workItemsSummaries {
-                  displayId
-                  key
-                  state
-                  stateType
-                }
-              }
+export const GET_PROJECT_PULL_REQUESTS = gql`
+  query getProjectPullRequests($projectKey: String!, $activeOnly: Boolean, $referenceString: String) {
+    project(key: $projectKey, referenceString: $referenceString) {
+      id
+      pullRequests(interfaces: [BranchRef, WorkItemsSummaries], activeOnly: $activeOnly) {
+        edges {
+          node {
+            id
+            name
+            key
+            displayId
+            state
+            repositoryKey
+            repositoryName
+            age
+            webUrl
+            workItemsSummaries {
+              displayId
+              key
+              state
+              stateType
             }
           }
         }
       }
-    `,
-    {
-      service: analytics_service,
-      variables: {
-        projectKey: instanceKey,
-        activeOnly: activeOnly,
-        referenceString: referenceString,
-      },
-      errorPolicy: "all",
     }
-  );
+  }
+`;
+
+export function useQueryProjectPullRequests({instanceKey, activeOnly, referenceString}) {
+  return useQuery(GET_PROJECT_PULL_REQUESTS, {
+    service: analytics_service,
+    variables: {
+      projectKey: instanceKey,
+      activeOnly: activeOnly,
+      referenceString: referenceString,
+    },
+    errorPolicy: "all",
+  });
 }
