f7e65980798e9941673076e854249e31dc450736
Aman Mavai <aman.mavai@gslab.com>
2020-12-22 19:22:51 +0530

    changed query formatting

--- src/app/dashboards/projects/shared/hooks/useQueryProjectPullRequests.js
+++ src/app/dashboards/projects/shared/hooks/useQueryProjectPullRequests.js
@@ -2,32 +2,39 @@ import {useQuery, gql} from "@apollo/client";
 import {analytics_service} from "../../../../services/graphql";
 
 export const GET_PROJECT_PULL_REQUESTS = gql`
-  query getProjectPullRequests($projectKey: String!, $activeOnly: Boolean, $referenceString: String) {
-    project(key: $projectKey, referenceString: $referenceString) {
-      id
-      pullRequests(interfaces: [BranchRef, WorkItemsSummaries], activeOnly: $activeOnly) {
-        edges {
-          node {
-            id
-            name
-            key
+query getProjectPullRequests(
+  $projectKey: String!
+  $activeOnly: Boolean
+  $referenceString: String
+) {
+  project(key: $projectKey, referenceString: $referenceString) {
+    id
+    pullRequests(
+      interfaces: [BranchRef, WorkItemsSummaries]
+      activeOnly: $activeOnly
+    ) {
+      edges {
+        node {
+          id
+          name
+          key
+          displayId
+          state
+          repositoryKey
+          repositoryName
+          age
+          webUrl
+          workItemsSummaries {
             displayId
+            key
             state
-            repositoryKey
-            repositoryName
-            age
-            webUrl
-            workItemsSummaries {
-              displayId
-              key
-              state
-              stateType
-            }
+            stateType
           }
         }
       }
     }
   }
+}
 `;
 
 export function useQueryProjectPullRequests({instanceKey, activeOnly, referenceString}) {
