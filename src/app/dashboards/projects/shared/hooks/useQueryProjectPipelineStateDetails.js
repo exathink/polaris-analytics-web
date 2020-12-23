866a64e201997b1e62c9eda595604080d41f72ee
Aman Mavai <aman.mavai@gslab.com>
2020-12-23 19:07:28 +0530

    add initial widget tests

--- src/app/dashboards/projects/shared/hooks/useQueryProjectPipelineStateDetails.js
+++ src/app/dashboards/projects/shared/hooks/useQueryProjectPipelineStateDetails.js
@@ -1,60 +1,71 @@
 import {useQuery, gql} from "@apollo/client";
 import {analytics_service} from "../../../../services/graphql";
 
-export function useQueryProjectPipelineStateDetails({instanceKey, specsOnly, referenceString, closedWithinDays, activeOnly}) {
-  return useQuery(
-    gql`
-      query projectPipelineStateDetails($key: String!, $specsOnly: Boolean, $referenceString: String, $closedWithinDays: Int, $activeOnly: Boolean) {
-        project(key: $key, referenceString: $referenceString) {
-          id
-          workItems(
-            activeOnly: $activeOnly
-            closedWithinDays: $closedWithinDays
-            interfaces: [WorkItemStateDetails]
-            specsOnly: $specsOnly
-            referenceString: $referenceString
-          ) {
-            edges {
-              node {
-                id
-                name
-                key
-                displayId
-                workItemType
+export const PROJECT_PIPELINE_STATE_DETAILS = gql`
+  query projectPipelineStateDetails(
+    $key: String!
+    $specsOnly: Boolean
+    $referenceString: String
+    $closedWithinDays: Int
+    $activeOnly: Boolean
+  ) {
+    project(key: $key, referenceString: $referenceString) {
+      id
+      workItems(
+        activeOnly: $activeOnly
+        closedWithinDays: $closedWithinDays
+        interfaces: [WorkItemStateDetails]
+        specsOnly: $specsOnly
+        referenceString: $referenceString
+      ) {
+        edges {
+          node {
+            id
+            name
+            key
+            displayId
+            workItemType
+            state
+            stateType
+            workItemStateDetails {
+              currentStateTransition {
+                eventDate
+              }
+              currentDeliveryCycleDurations {
                 state
                 stateType
-                workItemStateDetails {
-                  currentStateTransition {
-                    eventDate
-                  }
-                  currentDeliveryCycleDurations {
-                    state
-                    stateType
-                    daysInState
-                  }
-                  earliestCommit
-                  latestCommit
-                  commitCount
-                  effort
-                  duration
-                }
+                daysInState
               }
+              earliestCommit
+              latestCommit
+              commitCount
+              effort
+              duration
             }
           }
         }
       }
-    `,
-    {
-      service: analytics_service,
-      variables: {
-        key: instanceKey,
-        specsOnly: specsOnly,
-        referenceString: referenceString,
-        closedWithinDays: closedWithinDays,
-        activeOnly: activeOnly
-      },
-      errorPolicy: "all",
-      pollInterval: analytics_service.defaultPollInterval(),
     }
-  );
+  }
+`;
+
+export function useQueryProjectPipelineStateDetails({
+  instanceKey,
+  specsOnly,
+  referenceString,
+  closedWithinDays,
+  activeOnly,
+}) {
+  return useQuery(PROJECT_PIPELINE_STATE_DETAILS, {
+    service: analytics_service,
+    variables: {
+      key: instanceKey,
+      specsOnly: specsOnly,
+      referenceString: referenceString,
+      closedWithinDays: closedWithinDays,
+      activeOnly: activeOnly,
+    },
+    errorPolicy: "all",
+    pollInterval: analytics_service.defaultPollInterval(),
+  });
 }
