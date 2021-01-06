b88493b30713046819f5013a992633214730ebd3
Aman Mavai <aman.mavai@gslab.com>
2021-01-06 06:25:15 +0530

    minor renaming changes to fix tests

--- src/app/dashboards/projects/shared/widgets/projectWorkItemStateDetails/projectWorkItemStateDetailsWidget.spec.js
+++ src/app/dashboards/projects/shared/widgets/projectWorkItemStateDetails/projectWorkItemStateDetailsWidget.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {GraphQLError} from "graphql";
-import {renderComponentWithMockedProvider, gqlUtils} from "../../../../../framework/viz/charts/chart-test-utils";
+import {renderWithProviders, gqlUtils} from "../../../../../framework/viz/charts/chart-test-utils";
 import {PROJECT_PIPELINE_STATE_DETAILS} from "../../hooks/useQueryProjectPipelineStateDetails";
 import {PROJECT_AGGREGATE_CYCLE_METRICS} from "../../hooks/useQueryProjectCycleMetrics";
 import {ProjectWorkItemStateDetailsWidget} from "./projectWorkItemStateDetailsWidget";
@@ -82,7 +82,7 @@ describe("ProjectWorkItemStateDetailsWidget", () => {
   describe("when there are errors", () => {
     let logGraphQlError;
     beforeEach(() => {
-      logGraphQlError = jest.spyOn(gqlUtils, "logGraphQlError");
+      logGraphQlError = jest.spyOn(gqlUtils, "logGraphQlError").mockImplementation(() => {});
     });
     afterEach(() => {
       logGraphQlError.mockRestore();
@@ -115,14 +115,14 @@ describe("ProjectWorkItemStateDetailsWidget", () => {
     ];
 
     test("it renders nothing and logs the error when there is a network error", async () => {
-      renderComponentWithMockedProvider(<ProjectWorkItemStateDetailsWidget {...propsFixture} />, mockNetworkError);
+      renderWithProviders(<ProjectWorkItemStateDetailsWidget {...propsFixture} />, mockNetworkError);
       await screen.findByTestId("loading-spinner");
       await waitFor(() => expect(logGraphQlError).toHaveBeenCalled());
       expect(screen.queryByText(/work queue/i)).toBeNull();
     });
 
     test("it renders nothing and logs the error when there is a GraphQl error", async () => {
-      renderComponentWithMockedProvider(<ProjectWorkItemStateDetailsWidget {...propsFixture} />, mockGraphQlErrors);
+      renderWithProviders(<ProjectWorkItemStateDetailsWidget {...propsFixture} />, mockGraphQlErrors);
       await screen.findByTestId("loading-spinner");
       await waitFor(() => expect(logGraphQlError).toHaveBeenCalled());
       expect(screen.queryByText(/work queue/i)).toBeNull();
