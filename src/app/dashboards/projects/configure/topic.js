3db5823a9cd32d0327389c6ef56c2ef798cbe0ae
Krishna Kumar <kkumar@exathink.com>
2021-01-17 16:48:52 -0600

    Refactor projects/configure/dashboard.js to export two re-usable sub dashboards. Update topic.js to add direct routes to the sub dashboards. We may or may not end up using these routes, but the change here is mostly to demonstrate the greater modularity of this refactored code.
    [PO-499]

--- src/app/dashboards/projects/configure/topic.js
+++ src/app/dashboards/projects/configure/topic.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {Topics} from "../../../meta/topics";
-
+import ConfigureDashboard, {ValueStreamMappingDashboard, ResponseTimeSLASettingsDashboard} from "./dashboard";
 
 const topic =  {
   ...Topics.configure,
   routes: [
     {
+      match: 'value-stream',
+      component: () => <ValueStreamMappingDashboard/>
+    },
+    {
+      match: 'response-time-sla',
+      component: () => <ResponseTimeSLASettingsDashboard/>
+    },
+    {
       match: '',
-      component: React.lazy(() => import('./dashboard'))
+      component: ()=> <ConfigureDashboard/>
     }
   ]
 };
