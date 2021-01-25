6d8ed6760b7cc71c36670ec087cf2c696636d195
Aman Mavai <aman.mavai@gslab.com>
2021-01-25 10:46:10 +0530

    add constraint between sliders

--- src/app/dashboards/projects/configure/projectAnalysisPeriods/analysisPeriodsSliders.js
+++ src/app/dashboards/projects/configure/projectAnalysisPeriods/analysisPeriodsSliders.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {getAnalysisControlBar} from "../../../shared/components/analysisControlBar/analysisControlBar";
+import {TWO_MONTHS} from "../../../shared/components/daysRangeSlider/daysRangeSlider";
 import {actionTypes, mode} from "./constants";
 
 export function AnalysisPeriodsSliders({
@@ -10,12 +11,30 @@ export function AnalysisPeriodsSliders({
   mode: sliderMode,
   dispatch,
 }) {
+  // [1, 7, 14, 30, 45, 60];
+  let flowDaysMarks = TWO_MONTHS;
+  let trendsDaysMarks = TWO_MONTHS;
+  if (wipPeriod >= 1) {
+    flowDaysMarks = [wipPeriod, ...TWO_MONTHS.filter((x) => x > wipPeriod)];
+  }
+  if (flowPeriod >= 1) {
+    trendsDaysMarks = [flowPeriod, ...TWO_MONTHS.filter((x) => x > flowPeriod)];
+  }
+
   return (
     <div className="analysisSliderWrapper">
       {getAnalysisControlBar([
         [wipPeriod, (newPeriod) => dispatch({type: actionTypes.UPDATE_WIP_PERIOD, payload: newPeriod})],
-        [flowPeriod, (newPeriod) => dispatch({type: actionTypes.UPDATE_FLOW_PERIOD, payload: newPeriod})],
-        [trendsPeriod, (newPeriod) => dispatch({type: actionTypes.UPDATE_TRENDS_PERIOD, payload: newPeriod})],
+        [
+          flowPeriod,
+          (newPeriod) => dispatch({type: actionTypes.UPDATE_FLOW_PERIOD, payload: newPeriod}),
+          flowDaysMarks,
+        ],
+        [
+          trendsPeriod,
+          (newPeriod) => dispatch({type: actionTypes.UPDATE_TRENDS_PERIOD, payload: newPeriod}),
+          trendsDaysMarks,
+        ],
       ]).map((bar, index) => {
         let className = "analysis-slider-bar";
         if (sliderMode === mode.EDITING) {
