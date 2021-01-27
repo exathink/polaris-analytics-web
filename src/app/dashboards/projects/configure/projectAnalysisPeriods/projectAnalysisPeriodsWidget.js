e9a72c4050f646cae22011e1c1e23162a3e6e0c0
Aman Mavai <aman.mavai@gslab.com>
2021-01-27 10:07:15 +0530

    remove cycleTimeTarget

--- src/app/dashboards/projects/configure/projectAnalysisPeriods/projectAnalysisPeriodsWidget.js
+++ src/app/dashboards/projects/configure/projectAnalysisPeriods/projectAnalysisPeriodsWidget.js
@@ -6,7 +6,6 @@ export const ProjectAnalysisPeriodsWidget = ({
   wipAnalysisPeriod,
   flowAnalysisPeriod,
   trendsAnalysisPeriod,
-  cycleTimeTarget
 }) => {
   return (
     <ProjectAnalysisPeriodsView
@@ -14,7 +13,6 @@ export const ProjectAnalysisPeriodsWidget = ({
       wipAnalysisPeriod={wipAnalysisPeriod}
       flowAnalysisPeriod={flowAnalysisPeriod}
       trendsAnalysisPeriod={trendsAnalysisPeriod}
-      cycleTimeTarget={cycleTimeTarget}
     />
   );
 };
