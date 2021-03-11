ee40995c29e3a64d2e0c3afdcdf2ee076d461fa5
Aman Mavai <aman.mavai@gslab.com>
2021-03-11 13:04:06 +0530

    add detail view tests

--- src/app/dashboards/shared/charts/workItemCharts/workItemsEpicEffortChart.spec.js
+++ src/app/dashboards/shared/charts/workItemCharts/workItemsEpicEffortChart.spec.js
@@ -113,33 +113,32 @@ describe("WorkItemsEpicEffortChart", () => {
   });
 
   describe("when there is data for workItems", () => {
-    const {series} = renderedChartConfig(<WorkItemsEpicEffortChart {...propsFixture} />);
-
-    test("renders single series", () => {
-      expect(series).toHaveLength(1);
-    });
-
-    test("renders correct chart config", () => {
-      const expectedChartConfig = {
-        ...fixedChartConfig,
-        series: [
-          {
-            type: "treemap",
-            layoutAlgorithm: "squarified",
-            name: "Closed",
-            dataLabels: {
-              enabled: true,
-              useHTML: true,
-            },
-          },
-        ],
-      };
-      expect(renderedChartConfig(<WorkItemsEpicEffortChart {...propsFixture} />)).toMatchObject(expectedChartConfig);
-    });
-
-    const {workItems} = propsFixture;
-
     describe("primary view", () => {
+      const {series} = renderedChartConfig(<WorkItemsEpicEffortChart {...propsFixture} />);
+
+      test("renders single series", () => {
+        expect(series).toHaveLength(1);
+      });
+
+      test("renders correct chart config", () => {
+        const expectedChartConfig = {
+          ...fixedChartConfig,
+          series: [
+            {
+              type: "treemap",
+              layoutAlgorithm: "squarified",
+              name: "Closed",
+              dataLabels: {
+                enabled: true,
+                useHTML: true,
+              },
+            },
+          ],
+        };
+        expect(renderedChartConfig(<WorkItemsEpicEffortChart {...propsFixture} />)).toMatchObject(expectedChartConfig);
+      });
+
+      const {workItems} = propsFixture;
       describe(`workItems by epic series`, () => {
         const [workItemsByEpicSeries] = series;
 
@@ -154,6 +153,124 @@ describe("WorkItemsEpicEffortChart", () => {
               ["Quality Trends", 1.5],
               ["Contributor Alias UI", 0.3],
               ["Misc UX", 2.8],
+            ]
+          );
+        });
+
+        test("it sets the reference to the workItems for each point ", () => {
+          expectSetsAreEqual(
+            workItemsByEpicSeries.data.map((point) => point.workItems),
+            [[workItems[0], workItems[1]], [workItems[2]], [workItems[3]]]
+          );
+        });
+
+        test("should render the tooltip for point", async () => {
+          const props = {
+            ...propsFixture,
+            workItems: [
+              {
+                name: "Defect Lead Time/ Cycle Time Trends",
+                key: "7f7d0bdd-a862-4b81-a3f0-d3c1266762ce:4177",
+                workItemKey: "7f7d0bdd-a862-4b81-a3f0-d3c1266762ce",
+                displayId: "PO-494",
+                epicName: "Quality Trends",
+                epicKey: "89285627-0dd9-4b5b-8800-740dc68d7428",
+                effort: 0.5,
+              },
+              {
+                name: "Defect Arrival Close Rate - UI",
+                key: "eb5f494e-90f2-4685-a24b-da625a00931b:4265",
+                workItemKey: "eb5f494e-90f2-4685-a24b-da625a00931b",
+                displayId: "PO-550",
+                epicName: "Quality Trends",
+                epicKey: "89285627-0dd9-4b5b-8800-740dc68d7428",
+                effort: 1,
+              },
+            ],
+          };
+          const [actual] = await renderedTooltipConfig(
+            <WorkItemsEpicEffortChart {...props} />,
+            (points) => [points[0]],
+            0
+          );
+
+          expect(actual).toMatchObject({
+            header: `Quality Trends`,
+            body: [
+              [`Effort`, `1.5 Dev-Days`],
+              [`Cards`, `2`],
+            ],
+          });
+        });
+      });
+    });
+
+    describe("detail view", () => {
+      const propsFixtureForDetailView = {...propsFixture, view: "detail", showHierarchy: true};
+      const {series} = renderedChartConfig(<WorkItemsEpicEffortChart {...propsFixtureForDetailView} />);
+
+      test("renders single series", () => {
+        expect(series).toHaveLength(1);
+      });
+
+      test("renders correct chart config", () => {
+        const expectedChartConfig = {
+          ...fixedChartConfig,
+          series: [
+            {
+              type: "treemap",
+              layoutAlgorithm: "stripes",
+              alternateStartingDirection: true,
+              allowPointSelect: true,
+              levels: [
+                {
+                  level: 1,
+                  layoutAlgorithm: "squarified",
+                  dataLabels: {
+                    enabled: true,
+                    align: "center",
+                    verticalAlign: "top",
+                    style: {
+                      fontSize: "14px",
+                      fontWeight: "bold",
+                    },
+                  },
+                },
+                {
+                  level: 2,
+                  dataLabels: {
+                    enabled: true,
+                    align: "right",
+                    verticalAlign: "bottom",
+                    allowOverlap: true,
+                    style: {
+                      fontSize: "10px",
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        };
+        expect(renderedChartConfig(<WorkItemsEpicEffortChart {...propsFixtureForDetailView} />)).toMatchObject(expectedChartConfig);
+      });
+
+      const {workItems} = propsFixtureForDetailView;
+      describe(`workItems by epic series`, () => {
+        const [workItemsByEpicSeries] = series;
+
+        test(`renders a chart with the correct number of data points`, () => {
+          expect(workItemsByEpicSeries.data).toHaveLength(7);
+        });
+
+        test("it maps epic names to the x axis and sets y to a consolidated effort value", () => {
+          expectSetsAreEqual(
+            workItemsByEpicSeries.data.map((point) => [point.name, point.value]),
+            [
+              ["Quality Trends", 1.5],
+              ["Contributor Alias UI", 0.3],
+              ["Misc UX", 2.8],
+              ...propsFixture.workItems.map(x => ([x.name, x.effort]))
             ]
           );
         });
