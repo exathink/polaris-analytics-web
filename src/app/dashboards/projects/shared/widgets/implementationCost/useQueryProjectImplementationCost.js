4d3156fefed3b565b66011225fad30bc930f732a
Aman Mavai <aman.mavai@gslab.com>
2021-03-16 20:29:59 +0530

    update query to includeSubtasks: false

--- src/app/dashboards/projects/shared/widgets/implementationCost/useQueryProjectImplementationCost.js
+++ src/app/dashboards/projects/shared/widgets/implementationCost/useQueryProjectImplementationCost.js
@@ -56,6 +56,7 @@ export function useQueryImplementationCostTable({instanceKey, days, referenceStr
             interfaces: [ImplementationCost, EpicNodeRef, DevelopmentProgress]
             includeEpics: true
             activeWithinDays: $days
+            includeSubtasks: false
           ) {
             edges {
               node {
