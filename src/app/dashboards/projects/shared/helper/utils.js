ea9eb92af62abc9e567b5257f5ac370e65f57754
Aman Mavai <aman.mavai@gslab.com>
2021-04-07 21:50:37 +0530

    fix issue with getServerDate
    when date is null need to return null instead of current date
    null case is handled at the server side.

--- src/app/dashboards/projects/shared/helper/utils.js
+++ src/app/dashboards/projects/shared/helper/utils.js
@@ -1,5 +1,5 @@
-import {formatDate, getServerDate} from "../../../../helpers/utility";
+import {formatDate, getTodayDate} from "../../../../helpers/utility";
 
-export function getFlowMetricsRowTitle(measurementWindow, before = getServerDate()) {
+export function getFlowMetricsRowTitle(measurementWindow, before = getTodayDate()) {
   return before ? `${measurementWindow} days ending ${formatDate(before, "MM/DD/YYYY")} ` : ``;
 }
