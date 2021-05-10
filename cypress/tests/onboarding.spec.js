4ee3dc91313c65efd29b7769a70cd0ca30c3e7f0
Aman Mavai <aman.mavai@gslab.com>
2021-05-10 21:47:44 +0530

    add init setup of onboarding flow for trello

---
+++ cypress/tests/onboarding.spec.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+describe('Onboarding flows', () => {
+
+    it('Import Board flow for Trello', () => {
+      const [username, password] = [Cypress.env("testusername"), Cypress.env("testpassword")];
+      cy.loginByApi(username, password);
+
+      cy.visit('/');
+
+      // assume there is no existing connectors setup already
+      cy.contains("Connect Work Tracking System").click();
+      cy.contains("Connect Remote Projects").click();
+
+      // Provider Specific
+      cy.contains("Trello").click();
+      cy.contains("Create Trello Connector").click();
+
+      cy.contains("Next").click();
+
+      cy.get("input#name").type("Polaris Test");
+
+      const [trelloApiKey, trelloAccessToken] = [Cypress.env("trelloApiKey"), Cypress.env("trelloAccessToken")];
+      cy.get("input#trelloApiKey").type(trelloApiKey);
+      cy.get("input#trelloAccessToken").type(trelloAccessToken);
+      
+      cy.contains("Register").click();
+
+      cy.get("button.ant-btn").contains(/select/i).click();
+
+      cy.contains(/fetch available boards/i).click();
+
+      cy.get("input[type=checkbox]").check()
+
+      cy.contains("Next").click();
+
+      cy.get("button.ant-btn").contains(/Import Board/i).click();
+
+      cy.get("button.ant-btn").contains(/done/i).click();
+    })
+  })
