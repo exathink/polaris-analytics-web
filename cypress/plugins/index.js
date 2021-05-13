da469c6e0b12bb7ca4f252a84de1ee1e6f2d08eb
Aman Mavai <aman.mavai@gslab.com>
2021-05-13 22:10:34 +0530

    add dotenv to handle environment vars

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,8 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
+const dotenv = require("dotenv");
+dotenv.config();
 /**
  * @type {Cypress.PluginConfig}
  */
