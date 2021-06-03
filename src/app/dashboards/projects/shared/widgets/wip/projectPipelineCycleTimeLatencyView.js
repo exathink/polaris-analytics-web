dd4b7dc2262a5664a6739b552e73f3da3822c984
Aman Mavai <aman.mavai@gslab.com>
2021-06-03 18:08:34 +0530

    fix issue with zoom reset

--- src/app/dashboards/projects/shared/widgets/wip/projectPipelineCycleTimeLatencyView.js
+++ src/app/dashboards/projects/shared/widgets/wip/projectPipelineCycleTimeLatencyView.js
@@ -3,11 +3,13 @@ import WorkItems from "../../../../work_items/context";
 import {WorkItemsCycleTimeVsLatencyChart} from "../../../../shared/charts/workItemCharts/workItemsCycleTimeVsLatencyChart";
 import {VizItem, VizRow} from "../../../../shared/containers/layout";
 import {useGenerateTicks} from "../../../../shared/hooks/useGenerateTicks";
+import {isObjectEmpty} from "../../helper/utils";
+import {WorkItemStateTypeDisplayName} from "../../../../shared/config";
 
 export const ProjectPipelineCycleTimeLatencyView = (
   {
     stageName,
-    workItems,
+    data,
     stateTypes,
     groupByState,
     cycleTimeTarget,
@@ -16,10 +18,29 @@ export const ProjectPipelineCycleTimeLatencyView = (
     tooltipType,
     view,
     context,
-    callBacks
+    callBacks,
+    appliedFilters = {}
   }
 ) => {
   const tick = useGenerateTicks(2, 60000);
+
+  const applyFiltersTest = React.useCallback((node) => {
+    const newNode = {...node, stateType: WorkItemStateTypeDisplayName[node.stateType]};
+    if (isObjectEmpty(appliedFilters)) {
+      return true;
+    } else {
+      const entries = Object.entries(appliedFilters).filter(([_, filterVals]) => filterVals != null);
+      return entries.every(([filterKey, filterVals]) =>
+        filterVals.some((filterVal) => {
+          const re = new RegExp(filterVal, "i");
+          return newNode[filterKey].indexOf(filterVal) === 0 || newNode[filterKey].match(re);
+        })
+      );
+    }
+  }, [appliedFilters])
+
+  const workItems = React.useMemo(() => data['project']['workItems']['edges'].map(edge => edge.node).filter(applyFiltersTest), [data, applyFiltersTest]);
+
   return (
     <VizRow h={1}>
       <VizItem w={1}>
