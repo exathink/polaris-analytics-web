701f391b8043c2886e5ad08a3560f24bc93ffbd3
Aman Mavai <aman.mavai@gslab.com>
2021-06-15 16:51:22 +0530

    PO-695 fix styling issue in value dashboard

--- src/app/dashboards/projects/value/dashboard.js
+++ src/app/dashboards/projects/value/dashboard.js
@@ -1,37 +1,28 @@
 import React from "react";
-import { ProjectDashboard } from "../projectDashboard";
-import { withViewerContext } from "../../../framework/viewer/viewerContext";
-import { Dashboard, DashboardRow, DashboardWidget } from "../../../framework/viz/dashboard";
-import { ImplementationCostTableWidget, ProjectImplementationCostWidget } from "../shared/widgets/implementationCost";
-import { injectIntl } from "react-intl";
-import styles from "../flow/dashboard.module.css";
-import { ProjectFlowMixTrendsWidget } from "../shared/widgets/flowMix";
-import { DaysRangeSlider, ONE_YEAR } from "../../shared/components/daysRangeSlider/daysRangeSlider";
+import {ProjectDashboard} from "../projectDashboard";
+import {withViewerContext} from "../../../framework/viewer/viewerContext";
+import {Dashboard, DashboardRow, DashboardWidget} from "../../../framework/viz/dashboard";
+import {ImplementationCostTableWidget, ProjectImplementationCostWidget} from "../shared/widgets/implementationCost";
+import {injectIntl} from "react-intl";
+import styles from "./dashboard.module.css";
+import {ProjectFlowMixTrendsWidget} from "../shared/widgets/flowMix";
+import {DaysRangeSlider, ONE_YEAR} from "../../shared/components/daysRangeSlider/daysRangeSlider";
 
 const dashboard_id = "dashboards.value.projects.dashboard.instance";
 
 function ValueDashboard({
-                          project: { key, latestWorkItemEvent, latestCommit, settingsWithDefaults },
-                          context,
-                          viewerContext,
-                          intl
-                        }) {
-  const {
-    leadTimeTarget,
-    cycleTimeTarget,
-    responseTimeConfidenceTarget,
-    leadTimeConfidenceTarget,
-    cycleTimeConfidenceTarget,
-    flowAnalysisPeriod,
-    includeSubTasksFlowMetrics,
-    includeSubTasksWipInspector
-  } = settingsWithDefaults;
+  project: {key, latestWorkItemEvent, latestCommit, settingsWithDefaults},
+  context,
+  viewerContext,
+  intl,
+}) {
+  const {flowAnalysisPeriod, includeSubTasksFlowMetrics} = settingsWithDefaults;
 
   const specsOnly = true;
   const [activeWithinDays, setActiveWithinDays] = React.useState(flowAnalysisPeriod);
 
   return (
-    <Dashboard dashboard={`${dashboard_id}`}>
+    <Dashboard dashboard={`${dashboard_id}`} className={styles.valueDashboard} gridLayout={true}>
       <DashboardRow
         h={"50%"}
         title={""}
@@ -39,15 +30,20 @@ function ValueDashboard({
         controls={[
           () => (
             <div style={{minWidth: "500px"}}>
-                <DaysRangeSlider title={"Days"} initialDays={activeWithinDays} setDaysRange={setActiveWithinDays} range={ONE_YEAR}/>
+              <DaysRangeSlider
+                title={"Days"}
+                initialDays={activeWithinDays}
+                setDaysRange={setActiveWithinDays}
+                range={ONE_YEAR}
+              />
             </div>
-          )
+          ),
         ]}
       >
         <DashboardWidget
           name="epic-flow-mix-closed"
           className={styles.valueBookClosed}
-          render={({ view }) => (
+          render={({view}) => (
             <ProjectImplementationCostWidget
               instanceKey={key}
               context={context}
@@ -64,7 +60,7 @@ function ValueDashboard({
         <DashboardWidget
           name="flow-type-flow-mix"
           className={styles.valueMixChart}
-          render={({ view }) => (
+          render={({view}) => (
             <ProjectFlowMixTrendsWidget
               instanceKey={key}
               measurementWindow={7}
@@ -76,7 +72,7 @@ function ValueDashboard({
               latestCommit={latestCommit}
               specsOnly={specsOnly}
               showCounts={true}
-              chartOptions={{ alignTitle: "left" }}
+              chartOptions={{alignTitle: "left"}}
               includeSubTasks={includeSubTasksFlowMetrics}
             />
           )}
@@ -104,7 +100,8 @@ function ValueDashboard({
         <DashboardWidget
           w={1}
           name="implementation-cost-table-widget"
-          render={({ view }) => (
+          className={styles.valueBookTable}
+          render={({view}) => (
             <ImplementationCostTableWidget
               instanceKey={key}
               activeWithinDays={activeWithinDays}
@@ -126,7 +123,7 @@ function ValueDashboard({
 const dashboard = ({viewerContext, intl}) => (
   <ProjectDashboard
     pollInterval={1000 * 60}
-    render={(props) => <ValueDashboard {...props} viewerContext={viewerContext} intl={intl}/>}
+    render={(props) => <ValueDashboard {...props} viewerContext={viewerContext} intl={intl} />}
   />
 );
 export default withViewerContext(injectIntl(dashboard));
