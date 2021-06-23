6445c7a484272c706452e4a585d065b082655cfd
Aman Mavai <aman.mavai@gslab.com>
2021-06-23 22:20:50 +0530

    remove unnecessary columns

--- src/app/dashboards/organizations/contributors/teams/orgTeamsTable.js
+++ src/app/dashboards/organizations/contributors/teams/orgTeamsTable.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {PaginatedTable} from "../../../../components/tables/tableUtils";
-import Button from "../../../../../components/uielements/button";
 
 export function useOrgTeamsTableColumns() {
   const columns = [
@@ -17,32 +16,10 @@ export function useOrgTeamsTableColumns() {
       width: "10%",
     },
     {
-      title: "Value Streams",
-      dataIndex: "project",
-      key: "project",
-      width: "10%",
-    },
-    {
       title: "Last Commit",
       dataIndex: "latestCommit",
       key: "latestCommit",
       width: "15%",
-    },
-    {
-      title: "Last Project Activity",
-      dataIndex: "lastProjectActivity",
-      key: "lastProjectActivity",
-      width: "15%",
-    },
-    {
-      title: "",
-      key: "actions",
-      width: "10%",
-      render: (text, record) => (
-        <Button type={"primary"} size={"small"}>
-          Select
-        </Button>
-      ),
     },
   ];
 
