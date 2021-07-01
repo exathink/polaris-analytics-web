30a61704e96699406b28b63f9ee6802e1d7e1666
Aman Mavai <aman.mavai@gslab.com>
2021-07-01 20:46:05 +0530

    add init setup for team dashboard

---
+++ src/app/dashboards/teams/context.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {FormattedMessage} from 'react-intl.macro';
+
+import FourZeroFour from "../../../containers/Page/404";
+import type {Context} from '../../framework/navigation/context/context';
+
+import Activity from './activity/topic';
+
+import {Contexts} from "../../meta/contexts";
+import {instanceMatchPattern} from "../../framework/navigation/context/helpers";
+
+
+const messages = {
+  instanceDisplay: (instanceName) => (
+    <FormattedMessage
+      id="contexts.teams.instance"
+      defaultMessage="Team: {instance}"
+      values={{instance: instanceName}}/>
+  )
+};
+
+const context : Context = {
+  ...Contexts.teams,
+  hidden: true,
+  routes: [
+    {
+      match: `${instanceMatchPattern('team')}`,
+      context: {
+        ...Contexts.teams,
+        display: match => messages.instanceDisplay(match.params.team),
+        routes: [
+          {
+            match: 'activity',
+            topic: Activity
+          },
+          {
+            match: '',
+            redirect: 'activity'
+          }
+        ]
+      }
+    },
+    {
+      match: '',
+      component: FourZeroFour
+    }
+  ]
+};
+
+export default context;
+
