d916a16c829cb51e3208040b34eb4665f4a045d4
Krishna Kumar <kkumar@exathink.com>
2021-07-02 18:12:43 -0500

    Add WipFlowMetricsWidget to teams/activity dashboard.
    [PO-746]

--- src/app/dashboards/teams/activity/dashboard.js
+++ src/app/dashboards/teams/activity/dashboard.js
@@ -5,6 +5,7 @@ import {DimensionFlowMetricsWidget} from "../../shared/widgets/work_items/closed
 import {withViewerContext} from "../../../framework/viewer/viewerContext";
 import styles from "../../projects/wip/dashboard.module.css";
 import { DimensionCommitsNavigatorWidget, HeaderMetrics } from "../../shared/widgets/accountHierarchy";
+import { DimensionWipFlowMetricsWidget } from "../../shared/widgets/work_items/wip";
 
 const dashboard_id = "dashboards.activity.teams.instance";
 
@@ -44,7 +45,7 @@ function WipDashboard({
         <DashboardWidget
           name="flow-metrics"
           title={"Flow"}
-          w={1}
+          w={1/2}
           subtitle={`Last ${wipAnalysisPeriod} days`}
           hideTitlesInDetailView={true}
           render={({ view }) => (
@@ -66,6 +67,33 @@ function WipDashboard({
             />
           )}
           showDetail={true}
+        />
+        <DashboardWidget
+          name="pipeline"
+          w={1/2}
+          title={"Work In Progress"}
+          videoConfig={DimensionWipFlowMetricsWidget.videoConfig}
+          render={({view}) => (
+            <DimensionWipFlowMetricsWidget
+              dimension={'team'}
+              instanceKey={key}
+              display={"flowboardSummary"}
+              latestCommit={latestCommit}
+              latestWorkItemEvent={latestWorkItemEvent}
+              days={wipAnalysisPeriod}
+              targetPercentile={responseTimeConfidenceTarget}
+              leadTimeTargetPercentile={leadTimeConfidenceTarget}
+              cycleTimeTargetPercentile={cycleTimeConfidenceTarget}
+              cycleTimeTarget={cycleTimeTarget}
+              wipLimit={wipLimit}
+              view={view}
+              specsOnly={specsOnly}
+              context={context}
+              includeSubTasks={includeSubTasksWipInspector}
+            />
+          )}
+          showDetail={true}
+          hideTitlesInDetailView={true}
         />
       </DashboardRow>
       <DashboardRow h="30%" title={"TBD"}>
