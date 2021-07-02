6f670b45cd63eef9ae268e069d3096136972726b
Krishna Kumar <kkumar@exathink.com>
2021-07-02 12:51:18 -0500

    Cleanup nesting of dashboards.
    [PO-746]

--- src/app/dashboards/teams/activity/dashboard.js
+++ src/app/dashboards/teams/activity/dashboard.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
-import {FormattedMessage} from "react-intl.macro";
+import React, {useState} from "react";
 import {Dashboard, DashboardRow, DashboardWidget} from "../../../framework/viz/dashboard";
 import {TeamDashboard} from "../teamDashboard";
 import {DimensionFlowMetricsWidget} from "../../shared/widgets/work_items/closed/flowMetrics";
-import { withViewerContext } from "../../../framework/viewer/viewerContext";
+import {withViewerContext} from "../../../framework/viewer/viewerContext";
 
 const dashboard_id = "dashboards.activity.teams.instance";
-const messages = {
-  topRowTitle: <FormattedMessage id={`${dashboard_id}.topRowTitle`} defaultMessage="Activity Overview" />,
-};
+
+
+export const dashboard = (
+  {viewerContext}
+) =>
+  <TeamDashboard
+    pollInterval={1000 * 60}
+    render={
+      props => <WipDashboard viewerContext={viewerContext}  {...props}/>
+    }
+  />;
 
 function WipDashboard({
   team: {key, latestWorkItemEvent, latestCommit, latestPullRequestEvent, settings, settingsWithDefaults},
@@ -17,57 +24,51 @@ function WipDashboard({
 }) {
   const [workItemScope, setWorkItemScope] = useState("all");
   const specsOnly = workItemScope === "specs";
+  const {
+    leadTimeTarget,
+    cycleTimeTarget,
+    responseTimeConfidenceTarget,
+    leadTimeConfidenceTarget,
+    cycleTimeConfidenceTarget,
+    wipLimit,
+    wipAnalysisPeriod,
+    includeSubTasksWipInspector,
+    includeSubTasksFlowMetrics,
+    latencyTarget
+  } = settingsWithDefaults;
   return (
-    <TeamDashboard
-      pollInterval={60 * 1000}
-      render={({team, context}) => {
-        const {
-          leadTimeTarget,
-          cycleTimeTarget,
-          responseTimeConfidenceTarget,
-          leadTimeConfidenceTarget,
-          cycleTimeConfidenceTarget,
-          wipLimit,
-          wipAnalysisPeriod,
-          includeSubTasksWipInspector,
-          includeSubTasksFlowMetrics,
-          latencyTarget,
-        } = settingsWithDefaults;
-        return (
-          <Dashboard dashboard={`${dashboard_id}`}>
-            <DashboardRow h="15%">
-              <DashboardWidget
-                name="flow-metrics"
-                title={"Flow"}
-                w={1/3}
-                subtitle={`Last ${wipAnalysisPeriod} days`}
-                hideTitlesInDetailView={true}
-                render={({view}) => (
-                  <DimensionFlowMetricsWidget
-                    dimension={"team"}
-                    instanceKey={key}
-                    view={view}
-                    display={"performanceSummary"}
-                    context={context}
-                    specsOnly={specsOnly}
-                    days={wipAnalysisPeriod}
-                    measurementWindow={wipAnalysisPeriod}
-                    targetPercentile={responseTimeConfidenceTarget}
-                    leadTimeTarget={leadTimeTarget}
-                    cycleTimeTarget={cycleTimeTarget}
-                    leadTimeConfidenceTarget={leadTimeConfidenceTarget}
-                    cycleTimeConfidenceTarget={cycleTimeConfidenceTarget}
-                    includeSubTasks={includeSubTasksFlowMetrics}
-                  />
-                )}
-                showDetail={true}
-              />
-            </DashboardRow>
-          </Dashboard>
-        );
-      }}
-    />
+    <Dashboard dashboard={`${dashboard_id}`}>
+      <DashboardRow h="15%">
+        <DashboardWidget
+          name="flow-metrics"
+          title={"Flow"}
+          w={1 / 3}
+          subtitle={`Last ${wipAnalysisPeriod} days`}
+          hideTitlesInDetailView={true}
+          render={({ view }) => (
+            <DimensionFlowMetricsWidget
+              dimension={"team"}
+              instanceKey={key}
+              view={view}
+              display={"performanceSummary"}
+              context={context}
+              specsOnly={specsOnly}
+              days={wipAnalysisPeriod}
+              measurementWindow={wipAnalysisPeriod}
+              targetPercentile={responseTimeConfidenceTarget}
+              leadTimeTarget={leadTimeTarget}
+              cycleTimeTarget={cycleTimeTarget}
+              leadTimeConfidenceTarget={leadTimeConfidenceTarget}
+              cycleTimeConfidenceTarget={cycleTimeConfidenceTarget}
+              includeSubTasks={includeSubTasksFlowMetrics}
+            />
+          )}
+          showDetail={true}
+        />
+      </DashboardRow>
+    </Dashboard>
   );
-};
-export const dashboard = ({viewerContext}) => <TeamDashboard pollInterval={1000 * 60} render={props => <WipDashboard viewerContext={viewerContext}  {...props}/>} />;
-export default withViewerContext(dashboard);
+}
+
+export default withViewerContext(dashboard)
+
