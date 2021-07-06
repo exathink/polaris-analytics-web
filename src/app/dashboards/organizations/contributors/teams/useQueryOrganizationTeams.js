d6d95e8d8777305cb64dfd9306c30840728291a8
Krishna Kumar <kkumar@exathink.com>
2021-07-06 18:43:38 -0500

    Show latest commit in organization teams table.
    [PO-747]

--- src/app/dashboards/organizations/contributors/teams/useQueryOrganizationTeams.js
+++ src/app/dashboards/organizations/contributors/teams/useQueryOrganizationTeams.js
@@ -4,12 +4,13 @@ import {analytics_service} from "../../../../services/graphql";
 export const GET_ORGANIZATION_TEAMS_QUERY = gql`
   query getOrganizationTeamsInfo($organizationKey: String!) {
     organization(key: $organizationKey) {
-      teams (interfaces: [ContributorCount]){
+      teams (interfaces: [ContributorCount, CommitSummary]){
         edges {
           node {
             name
             key
             contributorCount
+            latestCommit
           }
         }
       }
