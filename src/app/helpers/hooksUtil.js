db17f8f6a6f4450c73cd7222af82737464a1b51e
Krishna Kumar <kkumar@exathink.com>
2021-07-13 16:34:00 -0500

    Add a hookUtil to compose hook states from parent to child.
    [PO-783]

--- src/app/helpers/hooksUtil.js
+++ src/app/helpers/hooksUtil.js
@@ -12,3 +12,16 @@ export function useOnlyRunOnUpdate(cb, deps) {
     }
   }, deps);
 }
+
+// A utility to set a child component take a parent hook state and override it locally
+// if it is not provided. We use this for example in overriding specs/All control state in
+// a child widget while allowing it to be overridden by parent control.
+// See: dashboard/widgets/work_items/valueStreamPhaseDetail/dimensionValueStreamPhaseDetailWidget.js
+// for a sample use in this manner
+
+export function useChildState(parentState, parentSetState, defaultValue) {
+  const [childState, childSetState] = React.useState(parentState || defaultValue);
+  const effectiveState = parentState || childState;
+  const effectiveSetState = parentSetState || childSetState;
+  return [effectiveState, effectiveSetState];
+}
