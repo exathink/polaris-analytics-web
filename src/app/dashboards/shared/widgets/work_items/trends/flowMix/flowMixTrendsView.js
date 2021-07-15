9eacb9e6c6278cd92a1764524897471c756d4d40
Aman Mavai <aman.mavai@gslab.com>
2021-07-15 18:33:11 +0530

    update stats view of flow mix widget

--- src/app/dashboards/shared/widgets/work_items/trends/flowMix/flowMixTrendsView.js
+++ src/app/dashboards/shared/widgets/work_items/trends/flowMix/flowMixTrendsView.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {FlowStatistic} from "../../../../components/flowStatistics/flowStatistics";
-import {VizItem, VizRow} from "../../../../containers/layout";
 import {percentage} from "../../../../../../helpers/utility";
 
 import {FlowMixTrendsChart} from "./flowMixTrendsChart";
+import styles from "./flowMix.module.css";
 
 export const reduceFlowMix = (result, item) => {
   result[item.category] = item;
@@ -25,27 +25,38 @@ export const ProjectFlowMixTrendsStatsView = (
     showCounts,
     asStatistic,
     target,
+    selectedMetricState
   }) => {
   const [current, previous] = flowMixTrends;
   const metric = specsOnly ? 'totalEffort' : 'workItemCount';
   const currentMix = current.flowMix.reduce(reduceFlowMix, {metric: metric});
   const previousMix = previous.flowMix.reduce(reduceFlowMix, {metric: metric});
 
-
+  let [selectedMetricFeatureProps, selectedMetricDefectsProps, selectedMetricTasksProps] = [{}, {}, {}];
+  if (selectedMetricState) {
+    selectedMetricFeatureProps = {selectedMetric: selectedMetricState[0], setSelectedMetric: () => selectedMetricState[1]("features")};
+    selectedMetricDefectsProps = {selectedMetric: selectedMetricState[0], setSelectedMetric: () => selectedMetricState[1]("defects")};
+    selectedMetricTasksProps = {selectedMetric: selectedMetricState[0], setSelectedMetric: () => selectedMetricState[1]("tasks")};
+  }
   return (
-    <VizRow h={"100%"}>
-      <VizItem w={0.25}>
+    <div className={styles.flowMixWrapper}>
+      <div className={styles.features}>
         <FlowStatistic
           title={'Features'}
           currentValue={currentMix.feature ? percentage(currentMix.feature[metric], currentMix.total) : 0}
           previousValue={previousMix.feature ? percentage(previousMix.feature[metric], previousMix.total) : 0}
-
           uom={'%'}
           precision={2}
           target={target}
+          metric="features"
+          asCard={true}
+          currentMeasurement={current}
+          previousMeasurement={previous}
+          {...selectedMetricFeatureProps}
+          info={{headline: "", content: ""}}
         />
-      </VizItem>
-      <VizItem w={0.25}>
+      </div>
+      <div className={styles.defects}>
         <FlowStatistic
           title={'Defects'}
           currentValue={currentMix.defect ? percentage(currentMix.defect[metric], currentMix.total) : 0}
@@ -53,18 +64,30 @@ export const ProjectFlowMixTrendsStatsView = (
           uom={'%'}
           precision={2}
           target={target}
+          metric="defects"
+          asCard={true}
+          currentMeasurement={current}
+          previousMeasurement={previous}
+          {...selectedMetricDefectsProps}
+          info={{headline: "", content: ""}}
         />
-      </VizItem>
-      <VizItem w={0.25}>
+      </div>
+      <div className={styles.tasks}>
         <FlowStatistic
           title={'Tasks'}
           currentValue={currentMix.task ? percentage(currentMix.task[metric], currentMix.total) : 0}
           previousValue={previousMix.task ? percentage(previousMix.task[metric], previousMix.total) : 0}
           uom={'%'}
           precision={2}
+          metric="tasks"
+          asCard={true}
+          currentMeasurement={current}
+          previousMeasurement={previous}
+          {...selectedMetricTasksProps}
+          info={{headline: "", content: ""}}
         />
-      </VizItem>
-    </VizRow>
+      </div>
+    </div>
 
   )
 }
