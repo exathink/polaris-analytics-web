27f754f735011e8cd83b84279fdb9c55e8915f98
Aman Mavai <aman.mavai@gslab.com>
2021-07-29 18:49:40 +0530

    initial migration

---
+++ src/app/dashboards/shared/widgets/work_items/responseTime/responseTimeDetailDashboard.js
@@ -0,0 +1,179 @@
+import React from "react";
+import {TeamDashboard} from "../teamDashboard";
+import {withViewerContext} from "../../../framework/viewer/viewerContext";
+import {DimensionResponseTimeTrendsWidget} from "../../shared/widgets/work_items/trends/responseTime";
+
+import {Dashboard, DashboardRow, DashboardWidget} from "../../../framework/viz/dashboard";
+import {DaysRangeSlider, THREE_MONTHS} from "../../shared/components/daysRangeSlider/daysRangeSlider";
+import styles from "./dashboard.module.css";
+import {DimensionFlowMetricsWidget} from "../../shared/widgets/work_items/closed/flowMetrics";
+import {DimensionDeliveryCycleFlowMetricsWidget} from "../../shared/widgets/work_items/closed/flowMetrics/dimensionDeliveryCycleFlowMetricsWidget";
+import {GroupingSelector} from "../../shared/components/groupingSelector/groupingSelector";
+
+const dashboard_id = "dashboards.trends.projects.dashboard.instance";
+
+const dashboard = ({viewerContext}) => (
+  <TeamDashboard
+    pollInterval={1000 * 60}
+    render={({team, ...rest}) => (
+      <DimensionResponseTimeDashboard dimension={"team"} dimensionData={team} {...rest} viewerContext={viewerContext} />
+    )}
+  />
+);
+
+const metricMapping = {
+  avgCycleTime: "cycleTime",
+  avgLeadTime: "leadTime",
+  avgDuration: "duration",
+  avgEffort: "effort",
+  avgLatency: "latency",
+};
+
+function DimensionResponseTimeDashboard({
+  dimension,
+  dimensionData: {key, latestWorkItemEvent, latestCommit, settingsWithDefaults},
+  context,
+  viewerContext,
+}) {
+  const {
+    leadTimeTarget,
+    cycleTimeTarget,
+    leadTimeConfidenceTarget,
+    cycleTimeConfidenceTarget,
+    responseTimeConfidenceTarget,
+    wipAnalysisPeriod,
+    includeSubTasksFlowMetrics,
+  } = settingsWithDefaults;
+
+  const [daysRange, setDaysRange] = React.useState(wipAnalysisPeriod);
+  const [selectedMetric, setSelectedMetric] = React.useState("avgCycleTime");
+  const [yAxisScale, setYAxisScale] = React.useState("logarithmic");
+  const [chartToggle, setChartToggle] = React.useState("trend");
+
+  return (
+    <Dashboard dashboard={`${dashboard_id}`} className={styles.responseTimeDashboard} gridLayout={true}>
+      <DashboardRow
+        h="45%"
+        className={styles.rangeSlider}
+        controls={[
+          () => (
+            <div style={{minWidth: "500px"}}>
+              <DaysRangeSlider initialDays={daysRange} setDaysRange={setDaysRange} range={THREE_MONTHS} />
+            </div>
+          ),
+        ]}
+      >
+        <DashboardWidget
+          name="flow-metrics"
+          title={`Spec Response Time`}
+          hideTitlesInDetailView={true}
+          className={styles.responseTimeMetrics}
+          render={({view}) => (
+            <DimensionFlowMetricsWidget
+              dimension={"team"}
+              instanceKey={key}
+              view={view}
+              display={"responseTimeDetail"}
+              displayProps={{
+                initialSelection: selectedMetric,
+                onSelectionChanged: (metric) => setSelectedMetric(metric),
+              }}
+              initialSelection={""}
+              twoRows={true}
+              context={context}
+              specsOnly={true}
+              latestWorkItemEvent={latestWorkItemEvent}
+              days={daysRange}
+              measurementWindow={daysRange}
+              targetPercentile={responseTimeConfidenceTarget}
+              leadTimeTarget={leadTimeTarget}
+              cycleTimeTarget={cycleTimeTarget}
+              leadTimeConfidenceTarget={leadTimeConfidenceTarget}
+              cycleTimeConfidenceTarget={cycleTimeConfidenceTarget}
+              includeSubTasks={includeSubTasksFlowMetrics}
+            />
+          )}
+          showDetail={false}
+        />
+      </DashboardRow>
+      <DashboardRow
+        h="46%"
+        className={styles.chartsRow}
+        controls={[
+          () => (
+            <GroupingSelector
+              label={" "}
+              value={chartToggle}
+              groupings={[
+                {
+                  key: "trend",
+                  display: "Trend",
+                },
+                {
+                  key: "cardDetail",
+                  display: "Card Detail",
+                },
+              ]}
+              initialValue={"trend"}
+              onGroupingChanged={setChartToggle}
+            />
+          ),
+        ]}
+      >
+        <DashboardWidget
+          name="cycle-time"
+          className={chartToggle === "trend" ? styles.responseTimeDetail : styles.responseTimeDetailHidden}
+          render={({view}) => (
+            <DimensionResponseTimeTrendsWidget
+              dimension={"team"}
+              instanceKey={key}
+              measurementWindow={7}
+              days={daysRange}
+              samplingFrequency={7}
+              leadTimeTarget={leadTimeTarget}
+              cycleTimeTarget={cycleTimeTarget}
+              leadTimeConfidenceTarget={leadTimeConfidenceTarget}
+              cycleTimeConfidenceTarget={cycleTimeConfidenceTarget}
+              targetPercentile={cycleTimeConfidenceTarget}
+              context={context}
+              view={view}
+              latestWorkItemEvent={latestWorkItemEvent}
+              defaultSeries={["all"]}
+              includeSubTasks={includeSubTasksFlowMetrics}
+            />
+          )}
+          showDetail={true}
+        />
+
+        <DashboardWidget
+          title={""}
+          name="flow-metrics-delivery-details"
+          className={chartToggle === "cardDetail" ? styles.responseTimeDetail : styles.responseTimeDetailHidden}
+          render={({view}) => (
+            <DimensionDeliveryCycleFlowMetricsWidget
+              dimension={dimension}
+              instanceKey={key}
+              specsOnly={true}
+              view={view}
+              context={context}
+              showAll={true}
+              latestWorkItemEvent={latestWorkItemEvent}
+              days={daysRange}
+              initialMetric={metricMapping[selectedMetric]}
+              leadTimeTarget={leadTimeTarget}
+              cycleTimeTarget={cycleTimeTarget}
+              leadTimeConfidenceTarget={leadTimeConfidenceTarget}
+              cycleTimeConfidenceTarget={cycleTimeConfidenceTarget}
+              includeSubTasks={includeSubTasksFlowMetrics}
+              yAxisScale={yAxisScale}
+              setYAxisScale={setYAxisScale}
+            />
+          )}
+          showDetail={false}
+        />
+      </DashboardRow>
+    </Dashboard>
+  );
+}
+
+export default withViewerContext(dashboard);
---
+++ src/app/dashboards/shared/widgets/work_items/responseTime/responseTimeWidget.js
@@ -0,0 +1,86 @@
+import React from "react";
+import {Loading} from "../../../../../../components/graphql/loading";
+import {AggregateFlowMetricsView} from "./aggregateFlowMetricsView";
+import {DimensionFlowMetricsDetailDashboard} from "./dimensionFlowMetricsDetailDashboard";
+import {useQueryDimensionFlowMetrics} from "./useQueryDimensionFlowMetrics";
+import {getReferenceString} from "../../../../../helpers/utility";
+
+export const DimensionFlowMetricsWidget = ({
+  dimension,
+  instanceKey,
+  specsOnly,
+  view,
+  context,
+  display,
+  twoRows,
+  latestWorkItemEvent,
+  latestCommit,
+  days,
+  measurementWindow,
+  samplingFrequency,
+  leadTimeTarget,
+  cycleTimeTarget,
+  leadTimeConfidenceTarget,
+  cycleTimeConfidenceTarget,
+  stateMappingIndex,
+  pollInterval,
+  includeSubTasks,
+  displayProps,
+}) => {
+  const limitToSpecsOnly = specsOnly != null ? specsOnly : true;
+  const {loading, error, data} = useQueryDimensionFlowMetrics({
+    dimension,
+    instanceKey,
+    leadTimeTarget,
+    cycleTimeTarget,
+    leadTimeTargetPercentile: leadTimeConfidenceTarget,
+    cycleTimeTargetPercentile: cycleTimeConfidenceTarget,
+    days: days,
+    measurementWindow: measurementWindow,
+    samplingFrequency: samplingFrequency || 7,
+    specsOnly: limitToSpecsOnly,
+    includeSubTasks: includeSubTasks,
+    referenceString: getReferenceString(latestWorkItemEvent, latestCommit),
+  });
+  if (loading) return <Loading />;
+  if (error) return null;
+  const {cycleMetricsTrends, contributorCount} = data[dimension];
+
+  if (view === "primary") {
+    return (
+      <AggregateFlowMetricsView
+        instanceKey={instanceKey}
+        display={display}
+        displayProps={displayProps}
+        twoRows={twoRows}
+        specsOnly={limitToSpecsOnly}
+        leadTimeTargetPercentile={leadTimeConfidenceTarget}
+        cycleTimeTargetPercentile={cycleTimeConfidenceTarget}
+        cycleTimeTarget={cycleTimeTarget}
+        leadTimeTarget={leadTimeTarget}
+        latestCommit={latestCommit}
+        contributorCount={contributorCount}
+        cycleMetricsTrends={cycleMetricsTrends}
+      />
+    );
+  } else {
+    return (
+      <DimensionFlowMetricsDetailDashboard
+        dimension={dimension}
+        instanceKey={instanceKey}
+        specsOnly={limitToSpecsOnly}
+        view={view}
+        context={context}
+        latestWorkItemEvent={latestWorkItemEvent}
+        days={days}
+        leadTimeTarget={leadTimeTarget}
+        cycleTimeTarget={cycleTimeTarget}
+        leadTimeConfidenceTarget={leadTimeConfidenceTarget}
+        cycleTimeConfidenceTarget={cycleTimeConfidenceTarget}
+        stateMappingIndex={stateMappingIndex}
+        includeSubTasks={includeSubTasks}
+        latestCommit={latestCommit}
+      />
+    );
+  }
+};
