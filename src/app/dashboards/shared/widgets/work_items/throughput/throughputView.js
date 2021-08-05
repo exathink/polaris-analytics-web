abe0cf2fda1b692ad90e972ae5b9280f93e2441c
Aman Mavai <aman.mavai@gslab.com>
2021-08-05 18:04:02 +0530

    migrate throughtput statistic view to grid

--- src/app/dashboards/shared/widgets/work_items/throughput/throughputView.js
+++ src/app/dashboards/shared/widgets/work_items/throughput/throughputView.js
@@ -7,7 +7,7 @@ import {
   LatestClosed,
   Volume,
 } from "../../../components/flowStatistics/flowStatistics";
-import {VizItem, VizRow} from "../../../containers/layout";
+import styles from "./dashboard.module.css"
 
 export const ThroughputView = ({
   contributorCount,
@@ -19,24 +19,24 @@ export const ThroughputView = ({
     return null;
   }
   return (
-    <VizRow>
-      <VizItem w={0.2}>
+    <div className={styles.throughputStatistic}>
+      <div className={styles.firstCol}>
         <ContributorCount contributorCount={contributorCount} />
-      </VizItem>
-      <VizItem w={0.2}>
+      </div>
+      <div className={styles.secondCol}>
         <Volume normalized={true} contributorCount={contributorCount} currentMeasurement={current} previousMeasurement={previous} specsOnly={specsOnly} />
-      </VizItem>
+      </div>
 
-      <VizItem w={0.3}>
+      <div className={styles.thirdCol}>
         <EffortOUT normalized={true} contributorCount={contributorCount} currentMeasurement={current} previousMeasurement={previous} specsOnly={specsOnly} />
-      </VizItem>
+      </div>
 
-      <VizItem w={0.3}>
+      <div className={styles.fourthCol}>
         <ComponentCarousel tickInterval={3000}>
           <LatestClosed currentMeasurement={current} />
           <Cadence currentMeasurement={current} previousMeasurement={previous} />
         </ComponentCarousel>
-      </VizItem>
-    </VizRow>
+      </div>
+    </div>
   );
 };
