334ae49971c08825a4c2487b1316c1a7da7a6a5f
Aman Mavai <aman.mavai@gslab.com>
2021-08-10 23:19:07 +0530

    add className to SelectDropdown api

--- src/app/dashboards/shared/components/teams/selectTeam.js
+++ src/app/dashboards/shared/components/teams/selectTeam.js
@@ -1,6 +1,7 @@
 import {Select} from "antd";
 import React from "react";
 import styles from "./selectDropdown.module.css";
+import classNames from "classnames";
 
 const DEFAULT_WIDTH = 150;
 
@@ -18,9 +19,9 @@ export function useSelect({uniqueItems, defaultVal}) {
  *
  * uniqueItems: array of {key, name} pair objects
  */
-export function SelectDropdown({title, uniqueItems, testId, handleChange, width = DEFAULT_WIDTH}) {
+export function SelectDropdown({title, uniqueItems, testId, handleChange, width = DEFAULT_WIDTH, className}) {
   return (
-    <div data-testid={testId} className={styles.selectWrapper}>
+    <div data-testid={testId} className={classNames(styles.selectWrapper, className)}>
       {title && <div className={styles.title}>{title}</div>}
       <Select defaultValue={0} onChange={handleChange} className={styles.selectControl} style={{width: width}}>
         {uniqueItems.map((item, index) => (
