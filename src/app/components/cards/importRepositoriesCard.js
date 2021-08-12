47650d152e5a1924e484ea940e5c734d52486b46
Aman Mavai <aman.mavai@gslab.com>
2021-08-12 19:35:18 +0530

    update usage of new component Launcher

--- src/app/components/cards/importRepositoriesCard.js
+++ src/app/components/cards/importRepositoriesCard.js
@@ -1,22 +1,13 @@
 import React from "react";
-import {LauncherCard} from "./launcher/launcherCard";
-
+import {Launcher} from "./launcher/launcherCard";
+import {VersionControlIcon} from "./launcher/versionControlIcon"
 export const ImportRepositoriesCard = ({onClick, title, style, compact}) => (
-    <LauncherCard
+    <Launcher
       onClick={onClick}
       title={title || "Import Repositories"}
-      cover={
-        <img
-          alt="example"
-          src="/images/third-party/git-logomark-black@2x.png"
-        />
-      }
-      style={style || {marginTop: 50, marginLeft: 10}}
-      {
-        ...compact ?
-            {}
-            :
-            {iconType: 'download', description: "Import from Github, Bitbucket etc.."}
+      description={"Import from Github, Bitbucket etc.."}
+      icon={
+        <VersionControlIcon />
       }
       />
 
