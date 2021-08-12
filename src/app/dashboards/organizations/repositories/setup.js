99bec0dcfc06a2a3827fbe76fb8b6c033b9390b0
Aman Mavai <aman.mavai@gslab.com>
2021-08-12 23:03:50 +0530

    update styling on the page RepositoriesDashboardSetup

--- src/app/dashboards/organizations/repositories/setup.js
+++ src/app/dashboards/organizations/repositories/setup.js
@@ -2,24 +2,27 @@ import React from "react";
 
 import {Col, Row} from "antd";
 import Button from "../../../../components/uielements/button";
+import styles from "./repository.module.css";
+import fontStyles from "../../../framework/styles/fonts.module.css";
+import classNames from "classnames";
 
 export const RepositoriesDashboardSetup = ({context}) => (
   <div className={'no-repositories'}>
     <div style={{padding: '30px'}}>
       <Row>
-        <Col offset={6} span={12}>
-          <h1>Connect your Version Control System</h1>
-          <p>
+        <Col offset={6} span={12} className={styles.textCenter}>
+          <h1 className={fontStyles["text-2xl"]}>Connect your Version Control System</h1>
+          <p className={classNames(fontStyles["font-normal"], fontStyles["text-base"])}>
             Connecting your version control system allows Polaris Flow to analyze the changes in the code that goes into your product.
             This analysis gives valuable insights into the day to day engineering practices on your team and connects code deliverables
             to product deliverables.
           </p>
-          <p>
+          <p className={classNames(fontStyles["font-normal"], fontStyles["text-base"])}>
             You may import any number of Git repositories at a time from a supported provider. The import process is designed to efficiently
             extract and analyze the full commit history of each repository on initial import, and then keep the analysis updated automatically
             in real-time as new commits are made.
           </p>
-          <p>
+          <p className={classNames(fontStyles["font-normal"], fontStyles["text-base"])}>
             Note: <em>Each git repository is cloned and analyzed in an ephemeral container that is destroyed permanently along with any disk resources used
             to persist copies of your source code, immediately after each analysis of a repository. Polaris Flow only retains the analyzed commit history and never makes copies
             of your source code other than when it is under active analysis.</em>
