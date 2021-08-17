c8f199db02758e926dd8feb9eaa38ba2fa365eab
Aman Mavai <aman.mavai@gslab.com>
2021-08-17 16:28:57 +0530

    fix form item textbox styles

--- src/app/components/forms/editableTable.js
+++ src/app/components/forms/editableTable.js
@@ -46,7 +46,7 @@ class EditableCell extends React.Component {
     const { children, dataIndex, record, title } = this.props;
     const { editing } = this.state;
     return editing ? (
-      <Form.Item style={{ margin: 0 }}>
+      <Form.Item style={{ margin: "0 0 0 auto", width: "70px", display: "flex"}}>
         {form.getFieldDecorator(dataIndex, {
           rules: [
             {
@@ -55,7 +55,7 @@ class EditableCell extends React.Component {
             },
           ],
           initialValue: record[dataIndex],
-        })(<Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />)}
+        })(<Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} style={{textAlign: "center"}} />)}
       </Form.Item>
     ) : (
       <div
