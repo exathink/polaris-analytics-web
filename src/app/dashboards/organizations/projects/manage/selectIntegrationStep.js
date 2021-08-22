723891d91c541f4023d77f093da1536d17e3d4b9
Krishna Kumar <kkumar@exathink.com>
2021-08-21 20:20:54 -0500

    Value Stream landing page:
    Select Provider Step: Numerous text and stylistic cleanup.
    
    [PO-833]

--- src/app/dashboards/organizations/projects/manage/selectIntegrationStep.js
+++ src/app/dashboards/organizations/projects/manage/selectIntegrationStep.js
@@ -1,95 +1,95 @@
 import React from "react";
 import {Card, Avatar} from "antd";
-import {TRELLO_CONNECTOR} from "../../../../../config/featureFlags";
+
 import "./addProjectsWorkflow.css";
 import Button from "../../../../../components/uielements/button";
+import classNames from "classnames";
+import fontStyles from "../../../../framework/styles/fonts.module.css";
 const {Meta} = Card;
 
 export class SelectIntegrationStep extends React.Component {
   render() {
-    const {viewerContext} = this.props;
+
 
     return (
-      <div className={'select-connector'}>
+
+
+      <div className={"select-connector"}>
+        <div className={classNames(fontStyles["font-normal"], fontStyles["text-sm"], "step-description")}>
+          <p>Securely import projects in your work tracking system into a Polaris Value Stream.</p>
+        </div>
         <div className={"select-connector-cards"}>
-          
-            
-              <Card hoverable={true} bordered={true} style={{width: 300, marginTop: 16}}
-                    onClick={() => this.props.onConnectorTypeSelected('jira')}>
-                <Meta
-                  avatar={
-                    <Avatar src="/images/third-party/jira-mark-contained-gradient-neutral@2x.png"/>
-                  }
-                  title="Jira"
-                  description="Import Projects and Issues"
-                />
-                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Install</Button>
-              </Card>
-            
-            
-              <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}
-                    onClick={() => this.props.onConnectorTypeSelected('github')}>
-                <Meta
-                  avatar={
-                    <Avatar src="/images/third-party/GitHub-Mark-120px-plus.png"/>
-                  }
-                  title="Github"
-                  description="Import Repository Issues"
-                />
-                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Install</Button>
-              </Card>
-            
-            
-              <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}
-                    onClick={() => this.props.onConnectorTypeSelected('pivotal')}>
-                <Meta
-                  avatar={
-                    <Avatar src="/images/third-party/Tracker_Icon.svg"/>
-                  }
-                  title="Pivotal Tracker"
-                  description="Import Projects and Stories"
-                />
-                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Install</Button>
-              </Card>
-            
-          
-          
-            
-              <Card
-                bordered={true}
-                hoverable={true}
-                style={{width: 300, marginTop: 16}}
-                onClick={() => this.props.onConnectorTypeSelected("gitlab")}
-              >
-                <Meta
-                  avatar={<Avatar src="/images/third-party/gitlab-icon-rgb.svg" />}
-                  title="GitLab"
-                  description="Import Projects and Issues"
-                />
-                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Install</Button>
-              </Card>
-            
-            {
-              viewerContext.isFeatureFlagActive(TRELLO_CONNECTOR) ?
-                
-                  <Card bordered={true} hoverable={true} style={{width: 300, marginTop: 16}}
-                        onClick={() => this.props.onConnectorTypeSelected('trello')}>
-                    <Meta
-                      avatar={
-                        <Avatar src="/images/third-party/trello.png"/>
-                      }
-                      title="Trello"
-                      description="Import Boards and Cards"
-                    />
-                    <Button type="secondary" size="small" style={{marginTop: "10px"}}>Install</Button>
-                  </Card>
-                
-                :
-                null
-            }
-          
+
+
+          <Card hoverable={true} bordered={true} style={{ width: 300, marginTop: 16 }}
+                onClick={() => this.props.onConnectorTypeSelected("jira")}>
+            <Meta
+              avatar={
+                <Avatar src="/images/third-party/jira-mark-contained-gradient-neutral@2x.png" />
+              }
+              title="Jira"
+              description="Sync Projects and Issues"
+            />
+            <Button type="secondary" size="small" style={{ marginTop: "10px" }}>Connect</Button>
+          </Card>
+
+
+          <Card bordered={true} hoverable={true} style={{ width: 300, marginTop: 16 }}
+                onClick={() => this.props.onConnectorTypeSelected("github")}>
+            <Meta
+              avatar={
+                <Avatar src="/images/third-party/GitHub-Mark-120px-plus.png" />
+              }
+              title="Github"
+              description="Sync Repository Issues"
+            />
+            <Button type="secondary" size="small" style={{ marginTop: "10px" }}>Connect</Button>
+          </Card>
+
+
+          <Card bordered={true} hoverable={true} style={{ width: 300, marginTop: 16 }}
+                onClick={() => this.props.onConnectorTypeSelected("pivotal")}>
+            <Meta
+              avatar={
+                <Avatar src="/images/third-party/Tracker_Icon.svg" />
+              }
+              title="Pivotal Tracker"
+              description="Sync Projects and Stories"
+            />
+            <Button type="secondary" size="small" style={{ marginTop: "10px" }}>Connect</Button>
+          </Card>
+
+
+          <Card
+            bordered={true}
+            hoverable={true}
+            style={{ width: 300, marginTop: 16 }}
+            onClick={() => this.props.onConnectorTypeSelected("gitlab")}
+          >
+            <Meta
+              avatar={<Avatar src="/images/third-party/gitlab-icon-rgb.svg" />}
+              title="GitLab"
+              description="Sync Projects and Issues"
+            />
+            <Button type="secondary" size="small" style={{ marginTop: "10px" }}>Connect</Button>
+          </Card>
+
+
+          <Card bordered={true} hoverable={true} style={{ width: 300, marginTop: 16 }}
+                onClick={() => this.props.onConnectorTypeSelected("trello")}>
+            <Meta
+              avatar={
+                <Avatar src="/images/third-party/trello.png" />
+              }
+              title="Trello"
+              description="Sync Boards and Cards"
+            />
+            <Button type="secondary" size="small" style={{ marginTop: "10px" }}>Connect</Button>
+          </Card>
+
         </div>
       </div>
+
     )
   }
 }
