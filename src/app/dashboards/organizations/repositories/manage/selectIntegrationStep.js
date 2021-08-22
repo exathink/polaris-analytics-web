228743895fde91faf9014a5f0f2f78bfd8a0e1af
Krishna Kumar <kkumar@exathink.com>
2021-08-21 20:22:28 -0500

    Reposiitories landing page:
    Select Provider Step: Numerous text and stylistic updates.
    
    [PO-833]

--- src/app/dashboards/organizations/repositories/manage/selectIntegrationStep.js
+++ src/app/dashboards/organizations/repositories/manage/selectIntegrationStep.js
@@ -2,6 +2,8 @@ import React from "react";
 import {Avatar, Card} from "antd";
 import Button from "../../../../../components/uielements/button";
 import styles from "./addRepositoryWorkflow.module.css";
+import classNames from "classnames";
+import fontStyles from "../../../../framework/styles/fonts.module.css";
 
 const {Meta} = Card;
 
@@ -11,6 +13,9 @@ export class SelectIntegrationStep extends React.Component {
   render() {
     return (
       <div className={styles.selectConnector}>
+        <div className={classNames(fontStyles["font-normal"], fontStyles["text-sm"], styles["flex-center"], styles.subTitle)}>
+          <p>Securely connect to Git repositories in your DevOps platform</p>
+        </div>
         <div className={styles.selectConnectorCards}>
           
             
@@ -23,9 +28,9 @@ export class SelectIntegrationStep extends React.Component {
                 <Meta
                   avatar={<Avatar src="/images/third-party/GitHub-Mark-120px-plus.png" />}
                   title="Github"
-                  description="Import Repositories and Commits"
+                  description="Sync Commits & Pull Requests"
                 />
-                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Install</Button>
+                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Connect</Button>
               </Card>
             
 
@@ -39,9 +44,9 @@ export class SelectIntegrationStep extends React.Component {
                 <Meta
                   avatar={<Avatar src="/images/third-party/bitbucket-mark-contained-gradient-neutral@2x.png" />}
                   title="Bit Bucket"
-                  description="Import Repositories and Commits"
+                  description="Sync Commits & Pull Requests"
                 />
-                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Install</Button>
+                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Connect</Button>
               </Card>
             
 
@@ -55,12 +60,20 @@ export class SelectIntegrationStep extends React.Component {
                 <Meta
                   avatar={<Avatar src="/images/third-party/gitlab-icon-rgb.svg" />}
                   title="GitLab"
-                  description="Import Repositories and Commits"
+                  description="Sync Commits & Merge Requests"
                 />
-                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Install</Button>
+                <Button type="secondary" size="small" style={{marginTop: "10px"}}>Connect</Button>
               </Card>
             
           
+        </div>
+        <div style={{marginTop: "30px"}}>
+          <p className={classNames(fontStyles["font-normal"], fontStyles["text-xs"], styles.subTitle)}>
+              <em>The security of your source code is important to us. Polaris only processes the git metadata that records changes being made to your source code.  It never scans your actual source files at any point.
+                Each git repository is cloned and analyzed in an ephemeral container that is destroyed permanently along with any disk resources used,
+              immediately after each analysis. Polaris only retains the analyzed metadata.
+              </em>
+            </p>
         </div>
       </div>
     );
