241aa63feab4753663c59bdf3006c022e5c2d63b
Aman Mavai <aman.mavai@gslab.com>
2021-09-07 21:06:22 +0530

    add custom cypress commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,6 +25,35 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+import {aliasQuery, aliasMutation} from "./utils";
+
+// custom command to make taking snapshots with full name
+// formed from the test title + suffix easier
+// cy.visualSnapshot() // default full test title
+// cy.visualSnapshot('clicked') // full test title + ' - clicked'
+// also sets the width and height to the current viewport
+Cypress.Commands.add("visualSnapshot", (maybeName) => {
+  // @ts-ignore
+  let snapshotTitle = cy.state("runnable").fullTitle();
+  if (maybeName) {
+    snapshotTitle = snapshotTitle + " - " + maybeName;
+  }
+  cy.percySnapshot(snapshotTitle, {
+    // @ts-ignore
+    widths: [cy.state("viewportWidth")],
+    // @ts-ignore
+    minHeight: cy.state("viewportHeight"),
+  });
+});
+
+Cypress.Commands.add("getBySel", (selector, ...args) => {
+  return cy.get(`[data-test=${selector}]`, ...args);
+});
+
+Cypress.Commands.add("getBySelLike", (selector, ...args) => {
+  return cy.get(`[data-test*=${selector}]`, ...args);
+});
+
 Cypress.Commands.add("loginByCSRF", (csrfToken, username, password) => {
   cy.request({
     method: "POST",
@@ -61,10 +90,12 @@ Cypress.Commands.add("loginByApi", (username, password) => {
     });
 });
 
-Cypress.Commands.add("interceptGraphQl", (opName) => {
+Cypress.Commands.add("interceptGraphQl", () => {
   cy.intercept("POST", "/graphql", (req) => {
-    if (req.body?.operationName.includes(opName)) {
-      req.alias = opName;
-    }
+    // Queries
+    aliasQuery(req, "getAccountConnectors");
+
+    // Mutations
+    aliasMutation(req, "createConnector");
   });
 });
