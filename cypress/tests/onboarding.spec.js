8474e1b1b9f7f7a81a81a437b78217a50f5036a0
Aman Mavai <aman.mavai@gslab.com>
2021-09-09 22:04:26 +0530

    generalize provider test and move dynamic data to config

--- cypress/tests/onboarding.spec.js
+++ cypress/tests/onboarding.spec.js
@@ -1,75 +1,61 @@
 /// <reference types="cypress" />
 
+const importProjectFlowConfig = [
+  {
+    provider: "Trello",
+    cardId: "trello-card",
+    connectorName: "Trello Test",
+    credentialPairs: [
+      ["input#trelloApiKey", Cypress.env("trelloApiKey")],
+      ["input#trelloAccessToken", Cypress.env("trelloAccessToken")],
+    ],
+  },
+  {
+    provider: "Github",
+    cardId: "github-card",
+    connectorName: "Github Test",
+    credentialPairs: [
+      ["input#githubOrganization", Cypress.env("githubOrganization")],
+      ["input#githubAccessToken", Cypress.env("githubAccessToken")],
+    ],
+  },
+];
+
 describe("Onboarding flows", () => {
   beforeEach(() => {
     cy.aliasGraphQlRequests();
   });
 
-  it("Import project flow for - Trello", () => {
-    const [username, password] = [Cypress.env("testusername"), Cypress.env("testpassword")];
-    cy.loginByApi(username, password);
+  importProjectFlowConfig.forEach((config) => {
+    it(`Import project flow for - ${config.provider}`, () => {
+      const [username, password] = [Cypress.env("testusername"), Cypress.env("testpassword")];
+      cy.loginByApi(username, password);
 
-    cy.visit("/");
+      cy.visit("/");
 
-    // assume there are no existing connectors setup already
+      // assume there are no existing connectors setup already
 
-    // 0th Step: Start import project flow
-    cy.getBySel("import-project").click();
-    cy.location("pathname").should("include", "/value-streams/new");
+      // 0th Step: Start import project flow
+      cy.getBySel("import-project").click();
+      cy.location("pathname").should("include", "/value-streams/new");
 
-    // 1st Step
-    cy.SelectProvider({cardId: "trello-card"});
+      // 1st Step
+      cy.SelectProvider({cardId: config.cardId});
 
-    // 2nd Step
-    cy.SelectConnector({
-      connectorName: "Trello Test",
-      credentialPairs: [
-        ["input#trelloApiKey", Cypress.env("trelloApiKey")],
-        ["input#trelloAccessToken", Cypress.env("trelloAccessToken")],
-      ],
+      // 2nd Step
+      cy.SelectConnector({
+        connectorName: config.connectorName,
+        credentialPairs: config.credentialPairs,
+      });
+
+      // 3rd Step
+      cy.SelectProjects();
+
+      // 4th Step
+      cy.ConfigureImport();
+
+      // 5th Step
+      cy.ImportProjectStatus();
     });
-
-    // 3rd Step
-    cy.SelectProjects();
-
-    // 4th Step
-    cy.ConfigureImport();
-
-    // 5th Step
-    cy.ImportProjectStatus();
-  });
-
-  it("Import project flow for - Github", () => {
-    const [username, password] = [Cypress.env("testusername"), Cypress.env("testpassword")];
-    cy.loginByApi(username, password);
-
-    cy.visit("/");
-    
-    // assume there are no existing connectors setup already
-
-    // 0th Step: Start import project flow
-    cy.getBySel("import-project").click();
-    cy.location("pathname").should("include", "/value-streams/new");
-
-    // 1st Step
-    cy.SelectProvider({cardId: "github-card"});
-
-    // 2nd Step
-    cy.SelectConnector({
-      connectorName: "Github Test",
-      credentialPairs: [
-        ["input#githubOrganization", Cypress.env("githubOrganization")],
-        ["input#githubAccessToken", Cypress.env("githubAccessToken")],
-      ],
-    });
-
-    // 3rd Step
-    cy.SelectProjects();
-
-    // 4th Step
-    cy.ConfigureImport();
-
-    // 5th Step
-    cy.ImportProjectStatus();
   });
 });
