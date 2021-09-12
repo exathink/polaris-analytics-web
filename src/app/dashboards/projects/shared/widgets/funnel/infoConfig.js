2a8f27662d9571f5b22b56adaeab8f6a82e8226a
Krishna Kumar <kkumar@exathink.com>
2021-09-12 14:47:57 -0500

    More copy updates for Funnel.
    [PO-851]

--- src/app/dashboards/projects/shared/widgets/funnel/infoConfig.js
+++ src/app/dashboards/projects/shared/widgets/funnel/infoConfig.js
@@ -33,8 +33,6 @@ export function PipelineFunnelWidgetInfoDetail() {
     <>
       <PipelineFunnelWidgetInfoSummary />
 
-
-
       <SubSection heading={"Unmapped Cards"}>
         <p>
           Cards whose current workflow state is unmapped will show up at the top of the funnel. Time spent in unmapped
@@ -50,8 +48,8 @@ export function PipelineFunnelWidgetInfoDetail() {
         <p>
           A delivery cycle is completed every time a card transitions to the Closed phase and a new one starts when the
           card transitions out of the Closed phase to one of the other active phases. So, if a card was delivered
-          multiple times during the flow analysis period, it may appear in counts in both the closed phase and one of
-          the active phases.
+          multiple times during the flow analysis period, it may be counted multiple times in the closed phase and may
+          also appear in the counts of one of the active phases.
         </p>
       </SubSection>
 
@@ -63,33 +61,34 @@ export function PipelineFunnelWidgetInfoDetail() {
           will also be reflected in other metrics like low <em>cycle time</em>.
         </p>
         <p>
-          Significant imbalances the sizes of the Open, Make or Deliver areas of the funnel indicate the work is queueing up in one
-          or more of those phases. This will typically also be reflected in the <em>age</em> and <em>latency</em> of work in
-          progress. You can dig deeper using these diagnostic metrics from the Flow Dashboard.
+          Significant imbalances the sizes of the Open, Make or Deliver areas of the funnel indicate the work is
+          queueing up in one or more of those phases. This will typically also be reflected in the <em>age</em> and{" "}
+          <em>latency</em> of work in progress. You can dig deeper using these diagnostic metrics from the Flow
+          Dashboard.
         </p>
       </Section>
       <Section heading={"Lead Time and the Backlog"}>
         <p>
-          The size of the Closed phase is proportional to the current pace of delivery, and Polaris shows this in real
-          time as delivery cycles complete. You can use the relative sizes of the Closed and Define phase to understand
-          how large an active Backlog you need to maintain in order to keep up with this pace of delivery. If you have a
+          The size of the Closed phase is proportional to the current pace of delivery. Polaris shows this in real time
+          as delivery cycles complete. You can use the relative sizes of the Closed and Define phase to understand how
+          large an active backlog you need to maintain in order to keep up with this pace of delivery. If you have a
           significantly larger number of items in the Define phase compared to the Closed phase, your have much more
           work queued up than you can reasonably deliver in the near term.
         </p>
         <SubSection heading={"Backlog Management"}>
           <p>
-            The concept of delivery cycles in Polaris, gives you an easy way to optimize the size of your active backlog: create a
-            workflow state called ROADMAP and map this to the
+            The concept of delivery cycles in Polaris, gives you an easy technique to optimize the size of your active
+            backlog: create a workflow state called ROADMAP and map this to the
             <em> Closed </em> phase in Polaris.
           </p>
           <ul>
             <li>
               If you think a given card will be not prioritized for delivery within a couple of flow analysis periods,
               move the card to the ROADMAP state and communicate this to your customer. This will reset your customer's
-              expectations, and also close the delivery cycle and stop the lead and cycle time clocks for the card. You
-              can move it back to your backlog state when it is closer to being worked on and the lead time clock will
-              start ticking again. This way your lead time becomes a useful metric to measure how well you are doing
-              against your customer commitments.
+              expectations. Polaris also closes the delivery cycle and stops the lead and cycle time clocks for the
+              card. You can move it back to your backlog state when it is closer to being worked on and the lead time
+              clock will start ticking again. This way your lead time becomes a useful metric to measure how well you
+              are doing against your customer commitments.
             </li>
             <li>
               If you dont think a card will be addressed in the next few periods, delete it or move it out of your work
@@ -97,7 +96,7 @@ export function PipelineFunnelWidgetInfoDetail() {
             </li>
           </ul>
           <p>
-            This way lead time time does not simply accumulate because a card was sitting in the backlog indefinitely.
+            This way, lead time time does not simply accumulate because a card was sitting in the backlog indefinitely.
             When this happens, it is hard to separate signal from noise in the lead time metric and it becomes much less
             useful.
           </p>
