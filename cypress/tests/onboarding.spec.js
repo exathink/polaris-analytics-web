7f05a3226f09ce0b6ceff21e65446d998da206fc
Aman Mavai <aman.mavai@gslab.com>
2021-09-13 18:16:16 +0530

    move login logic to beforeEach

--- cypress/tests/onboarding.spec.js
+++ cypress/tests/onboarding.spec.js
@@ -32,13 +32,13 @@ const importProjectFlowConfig = [
 describe("Onboarding flows", () => {
   beforeEach(() => {
     cy.aliasGraphQlRequests();
+
+    const [username, password] = [Cypress.env("testusername"), Cypress.env("testpassword")];
+    cy.loginByApi(username, password); 
   });
 
   importProjectFlowConfig.forEach((config) => {
     it(`Import project flow for - ${config.provider}`, () => {
-      const [username, password] = [Cypress.env("testusername"), Cypress.env("testpassword")];
-      cy.loginByApi(username, password);
-
       cy.visit("/");
 
       // assume there are no existing connectors setup already
