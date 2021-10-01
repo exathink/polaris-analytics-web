7acfa87aa05d4f999fa54fd9c342c3c0648eb9dc
Krishna Kumar <kkumar@exathink.com>
2021-10-01 11:05:07 -0500

    Change the default settings tab for team model dashboard.
    [PO-743]

--- src/app/dashboards/teams/configure/dashboard.js
+++ src/app/dashboards/teams/configure/dashboard.js
@@ -7,7 +7,7 @@ import {ConfigSelector, CONFIG_TABS} from "../../shared/widgets/configure/config
 
 const dashboard_id = "dashboards.teams.configure";
 export default withViewerContext(({viewerContext}) => {
-  const [configTab, setConfigTab] = React.useState(CONFIG_TABS.RESPONSE_TIME_SLA);
+  const [configTab, setConfigTab] = React.useState(CONFIG_TABS.MEASUREMENT_SETTINGS);
 
   return (
     <Dashboard dashboard={`${dashboard_id}`} gridLayout={true}>
