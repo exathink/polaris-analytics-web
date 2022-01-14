5aa6b681ce44dd8cd8e369ba5a20244546651f5e
Aman Mavai <aman.mavai@gslab.com>
2022-01-14 21:35:50 +0530

    update subtask image

--- src/app/dashboards/projects/shared/helper/renderers.js
+++ src/app/dashboards/projects/shared/helper/renderers.js
@@ -1,13 +1,19 @@
 import {Tag} from "antd";
 import React from "react";
 import {Highlighter} from "../../../../components/misc/highlighter";
+import { WorkItemStateTypeColor } from "../../../shared/config";
 import styles from "./renderers.module.css";
 
 const workItemTypeImageMap = {
   story: <img src="/images/icons/story.svg" alt="#" style={{width: "16px", height: "16px"}} />,
   task: <img src="/images/icons/task.svg" alt="#" style={{width: "16px", height: "16px"}} />,
+  subtask: <img src="/images/icons/subtask.svg" alt="#" style={{width: "16px", height: "16px"}} />,
   bug: <img src="/images/icons/bug.svg" alt="#" style={{width: "16px", height: "16px"}} />,
 };
+
+export function getStateTypeIcon(workItemType) {
+  return <div style={{width: "16px", height: "16px", backgroundColor: WorkItemStateTypeColor[workItemType], borderRadius: "0.2rem", marginRight: "0.5rem"}}></div>
+}
 
 export function comboColumnTitleRender(setShowPanel, setWorkItemKey) {
   return (text, record, searchText) =>
@@ -36,5 +42,21 @@ export function comboColumnTitleRender(setShowPanel, setWorkItemKey) {
           )}
         </div>
       </div>
+    );
+}
+
+export function customColumnRender({setShowPanel, setWorkItemKey, colRender = (text) => text, className}) {
+  return (text, record, searchText) =>
+    text && (
+      <span
+        onClick={() => {
+          setShowPanel(true);
+          setWorkItemKey(record.workItemKey);
+        }}
+        style={{cursor: "pointer"}}
+        className={className}
+      >
+        {colRender(text, record)}
+      </span>
     );
 }
