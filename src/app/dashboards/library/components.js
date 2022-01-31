d2923d08546694e50e4f6d3fb727f3badd965e4e
Aman Mavai <aman.mavai@gslab.com>
2022-01-31 19:37:28 +0530

    improve styling for detail view

--- src/app/dashboards/library/components.js
+++ src/app/dashboards/library/components.js
@@ -8,13 +8,13 @@ export const VideoDetailView = withRouter(({match, mapping}) => {
   const videoUrl = mapping.find((x) => x.name === match.params.selected)?.videoUrl;
   return (
     <div className={styles.detailViewWrapper}>
-      <div style={{padding: "56.25% 0 0 0", position: "relative"}}>
+      <div style={{padding: "50.25% 0 0 0", position: "relative"}}>
         <iframe
           title="video-guidance"
           src={videoUrl}
           allowfullscreen
           frameborder="0"
-          style={{position: "absolute", top: 0, left: 0, width: "100%", height: "90%"}}
+          style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}}
         ></iframe>
       </div>
     </div>
