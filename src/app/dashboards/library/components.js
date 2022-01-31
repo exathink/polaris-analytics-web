e9131ccdb5843512ce981cd933a0b7044d9609a6
Aman Mavai <aman.mavai@gslab.com>
2022-01-31 19:27:32 +0530

    add support for detail view

--- src/app/dashboards/library/components.js
+++ src/app/dashboards/library/components.js
@@ -1,9 +1,11 @@
 import styles from "./components.module.css";
 import {Card} from "antd";
 import {PlayCircleOutlined} from "@ant-design/icons";
+import {withRouter} from "react-router";
 const {Meta} = Card;
 
-export function VideoDetailView({videoUrl}) {
+export const VideoDetailView = withRouter(({match, mapping}) => {
+  const videoUrl = mapping.find((x) => x.name === match.params.selected)?.videoUrl;
   return (
     <div className={styles.detailViewWrapper}>
       <div style={{padding: "56.25% 0 0 0", position: "relative"}}>
@@ -17,26 +19,18 @@ export function VideoDetailView({videoUrl}) {
       </div>
     </div>
   );
-}
+});
 
-export function VideoCard({view, title, description, thumbnail, videoUrl}) {
-  function handleCardClick() {
-    
-  }
-  if (view === "detail") {
-    return <VideoDetailView videoUrl={videoUrl} />;
-  }
+export const VideoCard = ({name, description, thumbnail, onCardClick}) => {
   return (
     <Card
       hoverable
       style={{width: 290, objectFit: "cover"}}
-      cover={<img alt="example" src={thumbnail} style={{width: 290, height: 180, objectFit: "cover"}}/>}
-      onClick={handleCardClick}
-      actions={[
-        <PlayCircleOutlined  key="playcircle" style={{fontSize: "1.5rem"}}/>,
-    ]}
+      cover={<img alt="example" src={thumbnail} style={{width: 290, height: 180, objectFit: "cover"}} />}
+      onClick={() => onCardClick(name)}
+      actions={[<PlayCircleOutlined key="playcircle" style={{fontSize: "1.5rem"}} />]}
     >
       <Meta title={""} description={description} />
     </Card>
   );
-}
+};
--- src/app/dashboards/library/dashboard.js
+++ src/app/dashboards/library/dashboard.js
@@ -1,33 +1,47 @@
 import React from "react";
-import {VideoCard} from "./components";
+import {VideoCard, VideoDetailView} from "./components";
 import styles from "./dashboard.module.css";
 import img1 from "../../../image/1.jpg";
 import img2 from "../../../image/2.jpg";
 import img3 from "../../../image/3.jpg";
 import img4 from "../../../image/4.jpg";
 import img5 from "../../../image/5.jpg";
+import img6 from "../../../image/6.jpg";
+import {withDetailRoutes} from "../../framework/viz/dashboard/withDetailRoutes";
+import {withNavigationContext} from "../../framework/navigation/components/withNavigationContext";
 
-export default function VideoDashboard() {
+const allCardsData = [
+  {name: "title1", description: "Description", thumbnail: img1, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+  {name: "title2", description: "Description", thumbnail: img2, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+  {name: "title3", description: "Description", thumbnail: img3, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+  {name: "title4", description: "Description", thumbnail: img4, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+  {name: "title5", description: "Description", thumbnail: img5, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+  {name: "title6", description: "Description", thumbnail: img6, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+];
+
+export function VideoDashboard({dashboardUrl, itemSelected, navigate, context}) {
+  function onCardClick(name) {
+    itemSelected
+      ? navigate.push(`${dashboardUrl}${context.search}`)
+      : navigate.push(`${dashboardUrl}/${name}${context.search}`);
+  }
+
+  // find the selected VideoCard and render that or show all video cards
+  if (itemSelected) {
+    return <VideoDetailView mapping={allCardsData} />;
+  }
+
   return (
     <div className={styles.videoDashboard}>
-      <div className={styles.card}>
-        <VideoCard title={"Title"} description={"Description"} thumbnail={img1} videoUrl={"https://vimeo.com/showcase/8025646/embed"} />
-      </div>
-      <div className={styles.card}>
-        <VideoCard title={"Title"} description={"Description"} thumbnail={img2} videoUrl={"https://vimeo.com/showcase/8025646/embed"} />
-      </div>
-      <div className={styles.card}>
-        <VideoCard title={"Title"} description={"Description"} thumbnail={img3} videoUrl={"https://vimeo.com/showcase/8025646/embed"} />
-      </div>
-      <div className={styles.card}>
-        <VideoCard title={"Title"} description={"Description"} thumbnail={img4} videoUrl={"https://vimeo.com/showcase/8025646/embed"} />
-      </div>
-      <div className={styles.card}>
-        <VideoCard title={"Title"} description={"Description"} thumbnail={img5} videoUrl={"https://vimeo.com/showcase/8025646/embed"} />
-      </div>
-      <div className={styles.card}>
-        <VideoCard title={"Title"} description={"Description"} thumbnail={img1} videoUrl={"https://vimeo.com/showcase/8025646/embed"} />
-      </div>
+      {allCardsData.map((video) => {
+        return (
+          <div key={video.name} className={styles.card}>
+            <VideoCard {...video} dashboardUrl={dashboardUrl} onCardClick={onCardClick} itemSelected={itemSelected} />
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+export default withNavigationContext(withDetailRoutes(VideoDashboard));
