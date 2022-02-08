ffb3140fa22f36c9700b54b50641238268723f06
Aman Mavai <aman.mavai@gslab.com>
2022-02-08 12:11:26 +0530

    update project query to have additional required fields

--- src/app/dashboards/organizations/projects/manage/useQueryOrganizationProjects.js
+++ src/app/dashboards/organizations/projects/manage/useQueryOrganizationProjects.js
@@ -5,18 +5,39 @@ export const GET_PROJECTS_QUERY = gql`
   query organizationProjects($organizationKey: String!) {
     organization(key: $organizationKey) {
       id
-      projects(interfaces: [CommitSummary, RepositoryCount, WorkItemEventSpan]) {
+      projects(
+        interfaces: [CommitSummary, RepositoryCount, WorkItemEventSpan, CycleMetricsTrends, ContributorCount]
+        cycleMetricsTrendsArgs: {
+          days: 30
+          measurementWindow: 30
+          samplingFrequency: 30
+          metrics: [avg_lead_time, avg_cycle_time, total_effort, work_items_in_scope]
+        }
+        contributorCountDays: 30
+      ) {
         count
         edges {
           node {
             id
             name
             key
+            contributorCount
+            cycleMetricsTrends {
+              avgLeadTime
+              avgCycleTime
+              totalEffort
+              workItemsInScope
+            }
             repositoryCount
             latestCommit
             latestWorkItemEvent
             workItemsSources {
               count
+              edges {
+                node {
+                  name
+                }
+              }
             }
           }
         }
