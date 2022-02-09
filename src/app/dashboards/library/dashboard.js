1199ab39dbb11d02bfc9fe789f327e60c491a40b
Aman Mavai <aman.mavai@gslab.com>
2022-02-09 21:18:50 +0530

    init setup of video card design

--- src/app/dashboards/library/dashboard.js
+++ src/app/dashboards/library/dashboard.js
@@ -1,9 +1,7 @@
 import React from "react";
 import {VideoCard, VideoDetailView} from "./components";
 import styles from "./dashboard.module.css";
-import ux_conventions_img from "./images/ux-conventions-5.png";
-import img2 from "../../../image/2.jpg";
-import img3 from "../../../image/3.jpg";
+import video_bg_img from "./images/video_bg.jpg";
 import img4 from "../../../image/4.jpg";
 import img5 from "../../../image/5.jpg";
 import img6 from "../../../image/6.jpg";
@@ -11,9 +9,9 @@ import {withDetailRoutes} from "../../framework/viz/dashboard/withDetailRoutes";
 import {withNavigationContext} from "../../framework/navigation/components/withNavigationContext";
 
 const allCardsData = [
-  {name: "UX Conventions", description: "Perspectives, Topics and Navigation", thumbnail: ux_conventions_img, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
-  {name: "title2", description: "Description", thumbnail: img2, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
-  {name: "title3", description: "Description", thumbnail: img3, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+  {name: "UX Conventions", description: "Perspectives, Topics and Navigation", thumbnail: video_bg_img, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+  {name: "title2", description: "Description", thumbnail: video_bg_img, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
+  {name: "title3", description: "Description", thumbnail: video_bg_img, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
   {name: "title4", description: "Description", thumbnail: img4, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
   {name: "title5", description: "Description", thumbnail: img5, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
   {name: "title6", description: "Description", thumbnail: img6, videoUrl: "https://vimeo.com/showcase/8025646/embed"},
