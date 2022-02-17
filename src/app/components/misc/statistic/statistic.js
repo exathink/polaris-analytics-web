21a2bb5cf536266285416c7a1402c0f81ed79931
Aman Mavai <aman.mavai@gslab.com>
2022-02-17 13:24:38 +0530

    extract utility function for trend indicator and its value

--- src/app/components/misc/statistic/statistic.js
+++ src/app/components/misc/statistic/statistic.js
@@ -1,48 +1,55 @@
-import './statistic.css';
-import React from 'react';
-import { Icon as LegacyIcon } from '@ant-design/compatible';
+import "./statistic.css";
+import React from "react";
+import {Icon as LegacyIcon} from "@ant-design/compatible";
 import {ArrowDownOutlined, ArrowUpOutlined} from "@ant-design/icons";
 import {Tooltip} from "antd";
 import {TOOLTIP_COLOR} from "../../../helpers/utility";
-export {Statistic} from 'antd';
+export {Statistic} from "antd";
 
 function getDelta(firstValue, secondValue) {
   return ((firstValue - secondValue) / (1.0 * firstValue)) * 100;
 }
 
-function getTrendIndicatorIcon(delta, good) {
+export function getTrendIndicatorUtils({firstValue, secondValue, good}) {
+  const delta = getDelta(firstValue, secondValue);
   const style = good ? (good(delta) ? "good" : "bad") : "neutral";
-  return delta > 0 ? (
-    <span className={`${style}IndicatorArrow`}>
-      <ArrowUpOutlined size="small" />
-    </span>
-  ) : (
-    <span className={`${style}IndicatorArrow`}>
-      <ArrowDownOutlined size={"small"} />
-    </span>
-  );
+  const trendIndicatorIcon =
+    delta > 0 ? (
+      <span className={`${style}IndicatorArrow`}>
+        <ArrowUpOutlined size="small" />
+      </span>
+    ) : (
+      <span className={`${style}IndicatorArrow`}>
+        <ArrowDownOutlined size={"small"} />
+      </span>
+    );
+
+  const absDelta = Math.abs(delta);
+  const trendValue = <span className={`${style}IndicatorArrow`}>{absDelta.toFixed(2)}%</span>;
+  return {trendIndicatorIcon, trendValue, delta, absDelta};
 }
 
 // Display the trend indicator only if abs value of the delta is greater than this threshold.
 export const TrendIndicatorDisplayThreshold = 2;
 
-export const TrendIndicator = ({firstValue, secondValue, good, deltaThreshold=TrendIndicatorDisplayThreshold}) => {
+export const TrendIndicator = ({firstValue, secondValue, good, deltaThreshold = TrendIndicatorDisplayThreshold}) => {
   if (firstValue && secondValue) {
     const delta = getDelta(firstValue, secondValue);
-    return Math.abs(delta) > deltaThreshold &&
-    <LegacyIcon
-      type={delta > 0 ? 'arrow-up' : 'arrow-down'}
-      style={good ? good(delta) ? {color: '#399a15'} : {color: '#9a3727'} : {color: '#c1c1c6'}}
-
-    />;
+    return (
+      Math.abs(delta) > deltaThreshold && (
+        <LegacyIcon
+          type={delta > 0 ? "arrow-up" : "arrow-down"}
+          style={good ? (good(delta) ? {color: "#399a15"} : {color: "#9a3727"}) : {color: "#c1c1c6"}}
+        />
+      )
+    );
   } else {
     return null;
   }
-}
+};
 
 TrendIndicator.isPositive = (delta) => delta > 0;
 TrendIndicator.isNegative = (delta) => delta < 0;
-
 
 export const TrendIndicatorNew = ({
   firstValue,
@@ -51,17 +58,15 @@ export const TrendIndicatorNew = ({
   deltaThreshold = TrendIndicatorDisplayThreshold,
   samplingFrequency,
 }) => {
-  function getTrendIndicator(delta, good) {
-    const absDelta = Math.abs(delta);
-    const style = good ? good(delta) ? "good" : "bad" : "neutral";
-    const icon = getTrendIndicatorIcon(delta, good);
+  if (firstValue && secondValue) {
+    const {trendIndicatorIcon, trendValue, absDelta} = getTrendIndicatorUtils({firstValue, secondValue, good});
 
     return (
       // show indicator only if absDelta greater than the indicator display threshold
       absDelta > deltaThreshold && (
         <div>
-          <div className={`${style}Indicator`}>
-            {icon} <span>{absDelta.toFixed(2)}%</span>
+          <div>
+            {trendIndicatorIcon} {trendValue}
           </div>
           <div>
             <span className={"comparisonWindow"}>Compared to prior {samplingFrequency} days.</span>
@@ -69,12 +74,6 @@ export const TrendIndicatorNew = ({
         </div>
       )
     );
-  }
-
-  if (firstValue && secondValue) {
-    const delta = getDelta(firstValue, secondValue);
-
-    return getTrendIndicator(delta, good);
   } else {
     return null;
   }
@@ -88,18 +87,15 @@ export function TrendWithTooltip({
   samplingFrequency,
 }) {
   if (firstValue && secondValue) {
-    const delta = getDelta(firstValue, secondValue);
-    const absDelta = Math.abs(delta);
-    const style = good ? (good(delta) ? "good" : "bad") : "neutral";
-    const icon = getTrendIndicatorIcon(delta, good);
-  
+    const {trendIndicatorIcon, trendValue, absDelta} = getTrendIndicatorUtils({firstValue, secondValue, good});
+
     return (
       absDelta > deltaThreshold && (
         <Tooltip
           title={
             <div>
-              <div className={`${style}Indicator`}>
-                {icon} <span>{absDelta.toFixed(2)}%</span>
+              <div>
+                {trendIndicatorIcon} {trendValue}
               </div>
               <div>
                 <span className={"textXs"}>Compared to prior {samplingFrequency} days.</span>
@@ -108,11 +104,10 @@ export function TrendWithTooltip({
           }
           color={TOOLTIP_COLOR}
         >
-          <div className={`${style}Indicator trendCursor`}>{icon}</div>
+          <div className={`trendCursor`}>{trendIndicatorIcon}</div>
         </Tooltip>
       )
     );
   }
   return null;
 }
-
