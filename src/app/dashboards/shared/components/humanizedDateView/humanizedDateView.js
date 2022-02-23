1412ee4d3c9e03ce9962abfa9226247cbc8f98c0
Krishna Kumar <kkumar@exathink.com>
2022-02-23 14:10:50 -0600

    [PP-72]Update the font size for humanizedDateView.js

--- src/app/dashboards/shared/components/humanizedDateView/humanizedDateView.js
+++ src/app/dashboards/shared/components/humanizedDateView/humanizedDateView.js
@@ -41,10 +41,10 @@ export class HumanizedDateView extends React.Component {
         <Statistic
               title={this.props.title}
               value={' '}
-              prefix={<span className={"textXs"}>{this.state.text}</span>}
+              prefix={<span className={"textNormal"}>{this.state.text}</span>}
               precision={0}
               valueStyle={{ color: '#3f8600'}}
-              formatter={value => <span className="textSm">{value}</span>}
+              formatter={value => <span>{value}</span>}
             />
             :
         <StickerWidget
