5626dc01b32b629e12507e728fc8616a3fb7e1b1
Krishna Kumar <kkumar@exathink.com>
2022-02-28 09:20:25 -0600

    [PP-84] Rename display of "Uncategorized" to "No Epic" in chart and table.

--- src/app/dashboards/shared/charts/workItemCharts/workItemsEpicEffortChart.js
+++ src/app/dashboards/shared/charts/workItemCharts/workItemsEpicEffortChart.js
@@ -6,7 +6,7 @@ import {Colors} from "../../config";
 
 require("highcharts/modules/treemap")(Highcharts);
 
-const UNCATEGORIZED = {key: "uncategorized", displayValue: "Uncategorized", color: "#a2c0de"};
+const UNCATEGORIZED = {key: "uncategorized", displayValue: "No Epic", color: "#a2c0de"};
 const EFFORT_LIMIT = 0.5;
 const TEXT_LIMIT = 37;
 const colors = ['#2f7ed8', '#732855', '#8bbc21', '#964b4b', '#1aadce',
