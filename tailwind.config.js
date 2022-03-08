c308b8e2d560643a024449ec5957e6c52edd1d30
Aman Mavai <aman.mavai@gslab.com>
2022-03-08 15:45:06 +0530

    initial setup of design tokens which are suitable for this project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,30 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx,jsx,js}"],
   prefix: "tw-",
   theme: {
+    colors: ({colors}) => ({
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+      gray: {
+        100: colors.gray["100"],
+        200: colors.gray["200"],
+        300: colors.gray["300"],
+      },
+    }),
+    fontSize: {
+      xs: ["0.75rem", {lineHeight: "1rem"}],
+      sm: ["0.875rem", {lineHeight: "1.25rem"}],
+      base: ["1rem", {lineHeight: "1.5rem"}],
+      lg: ["1.125rem", {lineHeight: "1.75rem"}],
+      xl: ["1.25rem", {lineHeight: "1.75rem"}],
+      "2xl": ["1.5rem", {lineHeight: "2rem"}],
+    },
+    fontWeight: {
+      light: "300",
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+    },
     extend: {},
   },
   corePlugins: {
