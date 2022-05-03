27f6b215ad60a87278dcb5cae3cd60552218045e
Aman Mavai <aman.mavai@gslab.com>
2022-05-03 10:13:05 +0530

    setup tab selection

--- src/app/dashboards/shared/widgets/pullRequests/closedPullRequests/closedPullRequestsView.js
+++ src/app/dashboards/shared/widgets/pullRequests/closedPullRequests/closedPullRequestsView.js
@@ -1,11 +1,14 @@
+import React from "react";
 import {useIntl} from "react-intl";
 import {getHistogramSeries} from "../../../../projects/shared/helper/utils";
 import {PullRequestsDetailHistogramChart} from "../../../charts/workItemCharts/pullRequestsDetailHistogramChart";
+import {GroupingSelector} from "../../../components/groupingSelector/groupingSelector";
 import {ResponseTimeMetricsColor} from "../../../config";
 const COL_WIDTH_BOUNDARIES = [1, 3, 7, 14, 30, 60, 90];
 
 export function ClosedPullRequestsView({pullRequests, closedWithinDays}) {
   const intl = useIntl();
+  const [tabSelection, setTab] = React.useState("histogram");
   const seriesAvgAge = getHistogramSeries({
     id: "pull-request",
     intl,
@@ -13,15 +16,37 @@ export function ClosedPullRequestsView({pullRequests, closedWithinDays}) {
     points: pullRequests.map((x) => x["age"]),
     name: "Cycle Time",
     visible: true,
-    color: ResponseTimeMetricsColor.duration
+    color: ResponseTimeMetricsColor.duration,
   });
   // show histogram view
   return (
-    <PullRequestsDetailHistogramChart
-      chartSubTitle={`${pullRequests.length} pull requests closed within last ${closedWithinDays} days`}
-      selectedMetric={"pullRequestAvgAge"}
-      colWidthBoundaries={COL_WIDTH_BOUNDARIES}
-      series={[seriesAvgAge]}
-    />
+    <div className="tw-h-full">
+      <div className="tw-flex tw-items-center tw-justify-end">
+        <GroupingSelector
+          label={"View"}
+          className={"groupCardsBySelector"}
+          groupings={[
+            {key: "histogram", display: `Histogram`},
+            {key: "table", display: "Card Detail"},
+          ].map((item) => ({
+            key: item.key,
+            display: item.display,
+          }))}
+          initialValue={tabSelection}
+          value={tabSelection}
+          onGroupingChanged={setTab}
+          layout="col"
+        />
+      </div>
+      <div className={tabSelection === "table" ? "tw-hidden" : "tw-h-full tw-w-full"}>
+        <PullRequestsDetailHistogramChart
+          chartSubTitle={`${pullRequests.length} pull requests closed within last ${closedWithinDays} days`}
+          selectedMetric={"pullRequestAvgAge"}
+          colWidthBoundaries={COL_WIDTH_BOUNDARIES}
+          series={[seriesAvgAge]}
+        />
+      </div>
+      {tabSelection === "table" && <div className="tw-h-full">TABLE</div>}
+    </div>
   );
 }
