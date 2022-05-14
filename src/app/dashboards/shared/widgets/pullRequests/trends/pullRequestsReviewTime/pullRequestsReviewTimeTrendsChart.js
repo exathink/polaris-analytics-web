a46c0e9428031642fbaea98669a37dd03f31a87e
Krishna Kumar <kkumar@exathink.com>
2022-05-14 10:10:00 -0500

    [PP-157] Use a combination of line and area spline using cycle time color to render the Review Time metrics. Update the titles to use review time instead of time to close.

--- src/app/dashboards/shared/widgets/pullRequests/trends/pullRequestsReviewTime/pullRequestsReviewTimeTrendsChart.js
+++ src/app/dashboards/shared/widgets/pullRequests/trends/pullRequestsReviewTime/pullRequestsReviewTimeTrendsChart.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {i18nDate, i18nNumber} from "../../../../../../helpers/utility";
 import {MeasurementTrendLineChart} from "../../../../views/measurementTrend/measurementTrendLineChart";
+import { ResponseTimeMetricsColor } from "../../../../config";
 
 function getSelectedMetricDisplay(measurement, seriesKey, intl) {
   const seriesKeys = {
-    avgAge: ["Avg Time to Close: ", `${intl.formatNumber(measurement.avgAge)} Days`],
-    maxAge: ["Max Time to Close: ", `${intl.formatNumber(measurement.maxAge)} Days`],
+    avgAge: ["Avg Time to Review: ", `${intl.formatNumber(measurement.avgAge)} Days`],
+    maxAge: ["Max Time to Review: ", `${intl.formatNumber(measurement.maxAge)} Days`],
   };
 
   return seriesKeys[seriesKey];
@@ -21,19 +22,19 @@ export const PullRequestsReviewTimeTrendsChart = ({
   <MeasurementTrendLineChart
     measurements={pullRequestMetricsTrends}
     metrics={[
-      {key: "maxAge", displayName: "Max", visible: true, type: "spline"},
-      {key: "avgAge", displayName: "Avg", visible: true, type: "spline"},
+      {key: "maxAge", displayName: "Max", visible: true, type: "spline", color: ResponseTimeMetricsColor.cycleTime},
+      {key: "avgAge", displayName: "Avg", visible: true, type: "areaspline", color: ResponseTimeMetricsColor.cycleTime},
     ]}
     measurementPeriod={measurementPeriod}
     measurementWindow={measurementWindow}
     onSelectionChange={onSelectionChange}
     config={{
-      title: "Time to Close",
+      title: "Review Time",
       yAxisUom: "Days",
       plotBands: {
         metric: "avgAge",
       },
-      legendText: "Time to Close",
+      legendText: "Review Time",
       yAxisNormalization: {
         metric: "maxAge",
         minScale: 0,
