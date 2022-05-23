8229d481d6c1a5c696a05f8ac5d7ae84fbd7d892
Aman Mavai <aman.mavai@gslab.com>
2022-05-23 22:03:09 +0530

    remove viewer context as its not needed anymore

--- src/app/admin/account/users/editUserForm.js
+++ src/app/admin/account/users/editUserForm.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import "@ant-design/compatible/assets/index.css";
 import {Checkbox, Col, Drawer, Form, Input, Row, Select} from "antd";
-import {withViewerContext} from "../../../framework/viewer/viewerContext";
 import Button from "../../../../components/uielements/button";
 const {Option} = Select;
 
@@ -11,104 +10,98 @@ const ALL_ORG_ROLES = [
   {key: "member", name: "Member"},
 ];
 
-export const EditUserForm = withViewerContext(
-  ({initialValues, onSubmit, viewerContext: {viewer, getViewerOrganizations}}) => {
-    // const organizations = getViewerOrganizations("owner");
-    const [visible, setVisible] = React.useState(false);
-    function onClose() {
-      setVisible(false);
-    }
-
-    return (
-      <div>
-        <Button type="primary" onClick={() => setVisible(true)}>
-          Edit
-        </Button>
-        <Drawer key={visible} title={"Edit User"} width={720} onClose={onClose} visible={visible}>
-          <Form layout="vertical" hideRequiredMark onFinish={onSubmit} initialValues={initialValues}>
-            <Row gutter={16}>
-              <Col span={24}>
-                <Form.Item
-                  label="Email"
-                  name={"email"}
-                  rules={[
-                    {type: "email", message: "The input is not a valid email"},
-                    {required: true, message: "Email is required"},
-                  ]}
-                >
-                  <Input placeholder="email" />
-                </Form.Item>
-              </Col>
-              <Col span={24}>
-                <Form.Item
-                  name={"firstName"}
-                  label="First name"
-                  rules={[{required: true, message: "First name is required"}]}
-                >
-                  <Input placeholder="first name" />
-                </Form.Item>
-              </Col>
-              <Col span={24}>
-                <Form.Item
-                  name={"lastName"}
-                  label="Last name"
-                  rules={[{required: true, message: "Last name is required"}]}
-                >
-                  <Input placeholder="last name" />
-                </Form.Item>
-              </Col>
-              {initialValues.role && (
-                <Col span={24}>
-                  <Form.Item name={"role"} label="Roles and Access" valuePropName="checked">
-                    <Checkbox defaultChecked={initialValues.role}>Account Owner</Checkbox>
-                  </Form.Item>
-                </Col>
-              )}
-              <Col span={24}>
-                {initialValues.organizationRoles.map((org) => {
-                  return (
-                    <Form.Item name={org.organizationKey} label={org.organizationName} key={org.organizationKey}>
-                      <Select
-                        style={{width: 150}}
-                        defaultValue={initialValues[org.organizationKey]}
-                      >
-                        {ALL_ORG_ROLES.map((a) => {
-                          return (
-                            <Option key={a.key} value={a.key}>
-                              {a.name}
-                            </Option>
-                          );
-                        })}
-                      </Select>
-                    </Form.Item>
-                  );
-                })}
-              </Col>
-            </Row>
-
-            <div
-              style={{
-                position: "absolute",
-                left: 0,
-                bottom: 0,
-                width: "100%",
-                borderTop: "1px solid #e9e9e9",
-                padding: "10px 16px",
-                background: "#fff",
-                textAlign: "right",
-              }}
-            >
-              <Button onClick={onClose} style={{marginRight: 8}}>
-                Cancel
-              </Button>
-
-              <Button htmlType="submit" type="primary">
-                Save
-              </Button>
-            </div>
-          </Form>
-        </Drawer>
-      </div>
-    );
+export const EditUserForm = ({initialValues, onSubmit}) => {
+  const [visible, setVisible] = React.useState(false);
+  function onClose() {
+    setVisible(false);
   }
-);
+
+  return (
+    <div>
+      <Button type="primary" onClick={() => setVisible(true)}>
+        Edit
+      </Button>
+      <Drawer key={visible} title={"Edit User"} width={720} onClose={onClose} visible={visible}>
+        <Form layout="vertical" hideRequiredMark onFinish={onSubmit} initialValues={initialValues}>
+          <Row gutter={16}>
+            <Col span={24}>
+              <Form.Item
+                label="Email"
+                name={"email"}
+                rules={[
+                  {type: "email", message: "The input is not a valid email"},
+                  {required: true, message: "Email is required"},
+                ]}
+              >
+                <Input placeholder="email" />
+              </Form.Item>
+            </Col>
+            <Col span={24}>
+              <Form.Item
+                name={"firstName"}
+                label="First name"
+                rules={[{required: true, message: "First name is required"}]}
+              >
+                <Input placeholder="first name" />
+              </Form.Item>
+            </Col>
+            <Col span={24}>
+              <Form.Item
+                name={"lastName"}
+                label="Last name"
+                rules={[{required: true, message: "Last name is required"}]}
+              >
+                <Input placeholder="last name" />
+              </Form.Item>
+            </Col>
+            {initialValues.role && (
+              <Col span={24}>
+                <Form.Item name={"role"} label="Roles and Access" valuePropName="checked">
+                  <Checkbox defaultChecked={initialValues.role}>Account Owner</Checkbox>
+                </Form.Item>
+              </Col>
+            )}
+            <Col span={24}>
+              {initialValues.organizationRoles.map((org) => {
+                return (
+                  <Form.Item name={org.organizationKey} label={org.organizationName} key={org.organizationKey}>
+                    <Select style={{width: 150}} defaultValue={initialValues[org.organizationKey]}>
+                      {ALL_ORG_ROLES.map((a) => {
+                        return (
+                          <Option key={a.key} value={a.key}>
+                            {a.name}
+                          </Option>
+                        );
+                      })}
+                    </Select>
+                  </Form.Item>
+                );
+              })}
+            </Col>
+          </Row>
+
+          <div
+            style={{
+              position: "absolute",
+              left: 0,
+              bottom: 0,
+              width: "100%",
+              borderTop: "1px solid #e9e9e9",
+              padding: "10px 16px",
+              background: "#fff",
+              textAlign: "right",
+            }}
+          >
+            <Button onClick={onClose} style={{marginRight: 8}}>
+              Cancel
+            </Button>
+
+            <Button htmlType="submit" type="primary">
+              Save
+            </Button>
+          </div>
+        </Form>
+      </Drawer>
+    </div>
+  );
+};
