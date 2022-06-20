2391a8b158b2e675e2976b6cb0869c6efe9c55ac
Aman Mavai <aman.mavai@gslab.com>
2022-06-20 22:12:24 +0530

    fix uom for throughput

--- src/app/dashboards/shared/widgets/work_items/wip/flowMetrics/wipFlowMetricsSummaryView.js
+++ src/app/dashboards/shared/widgets/work_items/wip/flowMetrics/wipFlowMetricsSummaryView.js
@@ -263,7 +263,7 @@ export function WorkInProgressFlowMetricsView({data, dimension, cycleTimeTarget,
   const items = currentTrend[specsOnly ? "workItemsWithCommits" : "workItemsInScope"];
   const throughput = getMetricUtils({
     value: i18nNumber(intl, items, 2),
-    uom: `${itemsLabel} / Day`,
+    uom: `${itemsLabel}`,
     good: TrendIndicator.isPositive,
     precision: items > 10 ? 1 : 2,
     valueRender: (text) => text,
