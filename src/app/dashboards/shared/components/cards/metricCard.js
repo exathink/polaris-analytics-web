966caef6a64401deaf8bc00707f0fef71cf9a05a
Aman Mavai <aman.mavai@gslab.com>
2022-07-05 14:50:26 +0530

    implement trends and details popovers

--- src/app/dashboards/shared/components/cards/metricCard.js
+++ src/app/dashboards/shared/components/cards/metricCard.js
@@ -1,8 +1,20 @@
+import {Popover} from "antd";
 import classNames from "classnames";
 import {PlusIcon, TrendsIcon} from "../../../../components/misc/customIcons";
 import {InfoCard} from "../../../../components/misc/info";
 
-export function MetricCard({title, subTitle, value, suffix, info, trendIndicator, target, className}) {
+export function MetricCard({
+  title,
+  subTitle,
+  value,
+  suffix,
+  info,
+  trendIndicator,
+  target,
+  detailsDraft,
+  trendsDraft,
+  className,
+}) {
   return (
     <div
       className={classNames(
@@ -11,16 +23,34 @@ export function MetricCard({title, subTitle, value, suffix, info, trendIndicator
       )}
     >
       <div className="title tw-col-span-4">
-        <div className="tw-text-base tw-text-gray-300 tw-tracking-wide">{title}</div>
-        <div className="tw-text-[0.5rem] tw-tracking-tight tw-leading-3">{subTitle}</div>
+        <div className="tw-text-base tw-tracking-wide tw-text-gray-300">{title}</div>
+        <div className="tw-text-[0.5rem] tw-leading-3 tw-tracking-tight">{subTitle}</div>
       </div>
       <div className="icons tw-col-span-3 tw-col-start-5 tw-flex tw-justify-end tw-space-x-2">
-        <div className="trendIcon tw-cursor-pointer tw-rounded-full">
-          <TrendsIcon />
-        </div>
-        <div className="detailIcon tw-cursor-pointer tw-rounded-full">
-          <PlusIcon />
-        </div>
+        {trendsDraft && (
+          <div className="trendIcon tw-cursor-pointer tw-rounded-full">
+            <Popover
+              placement={trendsDraft.placement}
+              title={trendsDraft.title}
+              content={trendsDraft.content}
+              trigger="click"
+            >
+              <TrendsIcon />
+            </Popover>
+          </div>
+        )}
+        {detailsDraft && (
+          <div className="detailIcon tw-cursor-pointer tw-rounded-full">
+            <Popover
+              placement={detailsDraft.placement}
+              title={detailsDraft.title}
+              content={detailsDraft.content}
+              trigger="click"
+            >
+              <PlusIcon />
+            </Popover>
+          </div>
+        )}
         <div className="infoIcon tw-cursor-pointer tw-rounded-full">
           {info && <InfoCard title={info.title} content={info.content} drawerContent={info.drawerContent} />}
         </div>
