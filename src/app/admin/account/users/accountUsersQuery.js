8c5110dfba4c72c08111a3e342dba309b25650df
Aman Mavai <aman.mavai@gslab.com>
2022-07-20 15:39:24 +0530

    migrate to useQuery hook

---
+++ src/app/admin/account/users/accountUsersQuery.js
@@ -0,0 +1,43 @@
+import {useQuery, gql} from "@apollo/client";
+import {analytics_service} from "../../../services/graphql";
+
+export const ACCOUNT_USERS_QUERY = gql`
+  query accountUsers($accountKey: String!, $pageSize: Int!, $endCursor: String) {
+    account(key: $accountKey) {
+      id
+      key
+      users(first: $pageSize, after: $endCursor, interfaces: [UserInfo, ScopedRole]) {
+        count
+        edges {
+          node {
+            id
+            name
+            firstName
+            lastName
+            key
+            email
+            role
+            organizationRoles {
+              organizationKey
+              organizationRole
+              organizationName
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export function useQueryAccountUsers({pageSize, currentCursor, accountKey, newData}) {
+  return useQuery(ACCOUNT_USERS_QUERY, {
+    service: analytics_service,
+    variables: {
+      pageSize: pageSize,
+      endCursor: currentCursor,
+      accountKey: accountKey,
+    },
+    fetchPolicy: newData ? "network-only" : "cache-first",
+    errorPolicy: "all",
+  });
+}
--- src/app/admin/account/users/accountUsersTable.js
+++ src/app/admin/account/users/accountUsersTable.js
@@ -1,13 +1,12 @@
-import {gql} from "@apollo/client";
-import {Query} from "@apollo/client/react/components";
+
 import React from "react";
 
 import {Table} from "antd";
 
 import {withViewerContext} from "../../../framework/viewer/viewerContext";
-import {analytics_service} from "../../../services/graphql";
 import {withAntPagination} from "../../../components/graphql/withAntPagination";
 import {EditUserForm} from "./editUserForm";
+import { useQueryAccountUsers } from "./accountUsersQuery";
 
 const {Column} = Table;
 
@@ -19,44 +18,14 @@ const AccountUsersPaginatedTable = ({
   currentCursor,
   onNewPage,
   newData,
-}) => (
-  <Query
-    client={analytics_service}
-    query={gql`
-      query accountUsers($accountKey: String!, $pageSize: Int!, $endCursor: String) {
-        account(key: $accountKey) {
-          id
-          key
-          users(first: $pageSize, after: $endCursor, interfaces: [UserInfo, ScopedRole]) {
-            count
-            edges {
-              node {
-                id
-                name
-                firstName
-                lastName
-                key
-                email
-                role
-                organizationRoles {
-                  organizationKey
-                  organizationRole
-                  organizationName
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    variables={{
-      pageSize: pageSize,
-      endCursor: currentCursor,
-      accountKey: viewer.accountKey,
-    }}
-    fetchPolicy={newData ? "network-only" : "cache-first"}
-  >
-    {({loading, error, data}) => {
+}) => {
+      const {error, loading, data} = useQueryAccountUsers({
+        pageSize,
+        currentCursor,
+        accountKey: viewer.accountKey,
+        newData,
+      });
+
       if (error) return null;
       let tableData = [];
       let totalItems = 0;
@@ -110,9 +79,7 @@ const AccountUsersPaginatedTable = ({
           )}
         </Table>
       );
-    }}
-  </Query>
-);
+}
 
 
 export const AccountUsersTableWidget = withViewerContext(withAntPagination(AccountUsersPaginatedTable), ['account-owner'])
