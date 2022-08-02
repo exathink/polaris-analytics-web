db7ea1ce58c429b43444c611ef181365489aeebb
Krishna Kumar <kkumar@exathink.com>
2022-08-02 09:04:49 -0500

    [PP-214] Update the project flow topic route array so that it works correctly with the ui.new-card-design feature flag turned off as well as on.

--- src/app/dashboards/projects/flow/topic.js
+++ src/app/dashboards/projects/flow/topic.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {Topics} from "../../../meta/topics";
 import Summary from "./summary/topic";
 import Second from "./second/topic";
@@ -6,18 +7,27 @@ const topic = {
   ...Topics.flow,
   routes: [
     {
-      match: "first",
+      requiredFeatures: ['ui.new-card-design'],
+      match: "summary",
       subnav: true,
       topic: Summary,
     },
     {
+      requiredFeatures: ['ui.new-card-design'],
       match: "second",
       subnav: true,
       topic: Second,
     },
     {
+      requiredFeatures: ['ui.new-card-design'],
       match: '',
-      redirect: 'second'
+      redirect: 'summary'
+    },
+    // This technique works because when the feature flag is present the redirect route to topic overides the route to
+    // the main dashboard. Its a bit of a hack, but works for now without having to have a switch in the routes array.
+    {
+      match: '',
+      component: React.lazy(() => import('./dashboard'))
     }
   ]
 };
