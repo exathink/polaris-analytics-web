c81a8f2989b2ed3622f13bb5c45728248069f2db
Aman Mavai <aman.mavai@gslab.com>
2022-08-16 16:48:53 +0530

    fix auth test

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,8 +1,17 @@
-describe('User Login', () => {
-  it('User should be able to login with correct credentials', () => {
+import {ORGANIZATION, viewer_info} from "../support/queries-constants";
+
+describe("User Login", () => {
+  it("User should be able to login with correct credentials", () => {
+    // mock response for queries on the home page
+    cy.interceptQuery({operationName: viewer_info, fixturePath: `${viewer_info}.json`});
+    cy.interceptQuery({
+      operationName: ORGANIZATION.with_organization_instance,
+      fixturePath: `${ORGANIZATION.with_organization_instance}.json`,
+    });
+
     const [username, password] = [Cypress.env("username"), Cypress.env("password")];
 
     cy.loginByApi(username, password);
-    cy.visit('/');
-  })
-})
+    cy.visit("/");
+  });
+});
