13a12ef9f6af1f8fa923ba805008059f1bc0d9a1
Aman Mavai <aman.mavai@gslab.com>
2022-08-16 12:37:38 +0530

    remove unused command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,10 +31,6 @@ Cypress.Commands.add("getBySel", (selector, ...args) => {
   return cy.get(`[data-testid=${selector}]`, ...args);
 });
 
-Cypress.Commands.add("getBySelLike", (selector, ...args) => {
-  return cy.get(`[data-test*=${selector}]`, ...args);
-});
-
 Cypress.Commands.add("loginByCSRF", (csrfToken, username, password) => {
   cy.request({
     method: "POST",
