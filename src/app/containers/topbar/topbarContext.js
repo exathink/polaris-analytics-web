3224253bbc38faa4c13cf832afa115aa6fc7570f
Aman Mavai <aman.mavai@gslab.com>
2022-08-24 18:21:05 +0530

    blur context display at the top

--- src/app/containers/topbar/topbarContext.js
+++ src/app/containers/topbar/topbarContext.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {withNavigationContext} from "../../framework/navigation/components/withNavigationContext";
+import {useBlurClass} from "../../helpers/utility";
 
 export default withNavigationContext(({context}) => {
+  const blurClass = useBlurClass();
   const display = context.display();
   const contextStyle = {color: context.color()};
   return (
     <div className="topBarContext">
       <i className={context.icon()} style={contextStyle}/>
       {
-        display ? <span style={contextStyle}>{display}</span> : null
+        display ? <span style={contextStyle} className={blurClass}>{display}</span> : null
       }
     </div>
   );
