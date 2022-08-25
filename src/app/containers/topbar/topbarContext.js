92afefc29f04e62f0f10d07a392ba40b1a9608b2
Aman Mavai <aman.mavai@gslab.com>
2022-08-25 19:44:27 +0530

    fix issue with topBarContext

--- src/app/containers/topbar/topbarContext.js
+++ src/app/containers/topbar/topbarContext.js
@@ -6,11 +6,31 @@ export default withNavigationContext(({context}) => {
   const blurClass = useBlurClass();
   const display = context.display();
   const contextStyle = {color: context.color()};
+
+  let element;
+  if (blurClass) {
+    const [_title] = display?.props?.defaultMessage?.split?.(":") ?? [];
+    const _val = display?.props?.values?.instance;
+    element = (
+      <React.Fragment>
+        <span style={contextStyle}>{_title}</span>
+        {_val ? ":": ""}
+        <span className={blurClass}>{_val}</span>
+      </React.Fragment>
+    );
+  } else {
+    element = display ? (
+      <span style={contextStyle}>
+        {display}
+      </span>
+    ) : null;
+  }
+
   return (
     <div className="topBarContext">
       <i className={context.icon()} style={contextStyle}/>
       {
-        display ? <span style={contextStyle} className={blurClass}>{display}</span> : null
+        element
       }
     </div>
   );
