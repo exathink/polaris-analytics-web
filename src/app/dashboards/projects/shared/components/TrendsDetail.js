bbf5945664ebfdf64db6498c09dfedc87e77065a
Aman Mavai <aman.mavai@gslab.com>
2022-08-30 10:18:57 +0530

    improve styling of the card overall

--- src/app/dashboards/projects/shared/components/TrendsDetail.js
+++ src/app/dashboards/projects/shared/components/TrendsDetail.js
@@ -9,27 +9,27 @@ export function TrendsDetail({
   suffix,
 }) {
   return (
-    <div className="tw-p-2 tw-grid tw-grid-cols-2 tw-gap-8 tw-rounded-md">
+    <div className="tw-p-2 tw-grid tw-grid-cols-2 tw-gap-8 tw-rounded-md tw-text-gray-300">
       <div className="titleWrapper">
-        <div className="title tw-text-base tw-tracking-wide tw-text-gray-300">{title}</div>
+        <div className="title tw-text-xl tw-tracking-wide">{title}</div>
         <div className="subTitle tw-text-xs tw-tracking-tight">{comparedToText}</div>
       </div>
       <div className="value tw-text-2xl">{trendIndicator}</div>
       <div className="prevPeriod">
-        <div className="prevPeriodTitle tw-text-sm tw-tracking-wide tw-text-gray-300">Prior Period</div>
-        <div className="prevPeriodValue tw-text-xs">{prevPeriod}</div>
+        <div className="prevPeriodTitle tw-text-base tw-tracking-wide">Prior Period</div>
+        <div className="prevPeriodValue tw-text-2xs tw-italic">{prevPeriod}</div>
       </div>
 
       <div className="currentPeriod">
-        <div className="prevPeriodTitle tw-text-sm tw-tracking-wide tw-text-gray-300">Current Period</div>
-        <div className="prevPeriodValue tw-text-xs">{currentPeriod}</div>
+        <div className="prevPeriodTitle tw-text-base tw-tracking-wide">Current Period</div>
+        <div className="prevPeriodValue tw-text-2xs tw-italic">{currentPeriod}</div>
       </div>
       <div className="prevVal tw-flex tw-items-baseline tw-space-x-2">
-        <div className="value tw-text-3xl tw-font-medium tw-leading-3">{prevValue}</div>
+        <div className="value tw-text-3xl tw-font-medium tw-leading-3 tw-text-black">{prevValue}</div>
         <div className="unit tw-text-sm tw-font-normal">{suffix}</div>
       </div>
       <div className="currentVal tw-flex tw-items-baseline tw-space-x-2">
-        <div className="value tw-text-3xl tw-font-medium tw-leading-3">{currentValue}</div>
+        <div className="value tw-text-3xl tw-font-medium tw-leading-3 tw-text-black">{currentValue}</div>
         <div className="unit tw-text-sm tw-font-normal">{suffix}</div>
       </div>
     </div>
