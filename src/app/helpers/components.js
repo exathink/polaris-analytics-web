d2bdccd8d8ad596bd8845adfbc7ed6b7d9d0800e
Aman Mavai <aman.mavai@gslab.com>
2022-09-22 19:59:37 +0530

    remove unused display classes

--- src/app/helpers/components.js
+++ src/app/helpers/components.js
@@ -3,8 +3,8 @@ import classNames from "classnames";
 export function LabelValue({label, value, className, labelClassName, valueClassName}) {
   return (
     <div className={classNames("tw-flex tw-items-baseline", className)}>
-      <div className={classNames("label tw-text-sm tw-font-medium tw-uppercase", labelClassName)}>{label}</div>
-      <div className={classNames("value tw-ml-2 tw-text-lg tw-font-semibold", valueClassName)}>{value}</div>
+      <div className={classNames("tw-text-sm tw-font-medium tw-uppercase", labelClassName)}>{label}</div>
+      <div className={classNames("tw-ml-2 tw-text-lg tw-font-semibold", valueClassName)}>{value}</div>
     </div>
   );
 }
