c175f87cb0617fcd25af5a53dd53d37fc6d7d22c
Aman Mavai <aman.mavai@gslab.com>
2022-09-23 21:48:29 +0530

    add uom to LabelValue component

--- src/app/helpers/components.js
+++ src/app/helpers/components.js
@@ -1,10 +1,11 @@
 import classNames from "classnames";
 
-export function LabelValue({label, value, className, labelClassName, valueClassName}) {
+export function LabelValue({label, value, uom, className, labelClassName, valueClassName}) {
   return (
     <div className={classNames("tw-flex tw-items-baseline", className)}>
       <div className={classNames("tw-text-sm tw-font-medium tw-uppercase", labelClassName)}>{label}</div>
       <div className={classNames("tw-ml-2 tw-text-lg tw-font-semibold", valueClassName)}>{value}</div>
+      {uom && <div className="tw-ml-1 tw-text-xs">{uom}</div>}
     </div>
   );
 }
