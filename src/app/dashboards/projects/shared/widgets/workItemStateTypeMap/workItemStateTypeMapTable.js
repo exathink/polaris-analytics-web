f2d4580ddf295edbd67e6a8eda7281367565e49b
Aman Mavai <aman.mavai@gslab.com>
2022-11-09 16:12:18 +0530

    add table component

---
+++ src/app/dashboards/projects/shared/widgets/workItemStateTypeMap/workItemStateTypeMapTable.js
@@ -0,0 +1,43 @@
+import {StripeTable} from "../../../../../components/tables/tableUtils";
+import {WorkItemStateTypeColorClass, WorkItemStateTypeDisplayName} from "../../../../shared/config";
+
+export function useWorkItemStateTypeMapColumns() {
+  const columns = [
+    {
+      title: "Phase",
+      dataIndex: "stateType",
+      key: "stateType",
+      width: "20%",
+      render: (text, record) => <span className="tw-font-semibold">{WorkItemStateTypeDisplayName[text]}</span>,
+    },
+    {
+      title: "State",
+      dataIndex: "state",
+      key: "state",
+      width: "40%",
+      render: (text, record) => text,
+    },
+    {
+      title: "State Type",
+      dataIndex: "test",
+      key: "test",
+      width: "40%",
+      render: (text, record) => text,
+    },
+  ];
+  return columns;
+}
+
+export function WorkItemStateTypeMapTable({tableData, columns, loading, testId, rowSelection}) {
+  return (
+    <StripeTable
+      dataSource={tableData}
+      columns={columns}
+      loading={loading}
+      testId={testId}
+      rowSelection={rowSelection}
+      rowKey={(record) => record.key}
+      rowClassName={(record) => WorkItemStateTypeColorClass[record.stateType]}
+    />
+  );
+}
