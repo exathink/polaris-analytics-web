afc7125bf4ad3b83d7c8fc15b8bd24de8028e90c
Aman Mavai <aman.mavai@gslab.com>
2022-11-18 09:39:06 +0530

    revert title back to old title

--- src/app/dashboards/work_items/activity/durationDetails/workItemDurationDetailsByStateView.js
+++ src/app/dashboards/work_items/activity/durationDetails/workItemDurationDetailsByStateView.js
@@ -1,16 +1,14 @@
 import React from "react";
 import {VizItem, VizRow} from "../../../shared/containers/layout";
 import {WorkItemsAggregateDurationsByStateChart} from "../../../shared/charts/workItemCharts/workItemsAggregateDurationsByStateChart";
-import {useFlowEfficiency} from "../../../projects/shared/helper/hooks";
 
 export const WorkItemDurationDetailsByStateView = ({workItem, view}) => {
-  const flowEfficiencyPercentage = useFlowEfficiency([workItem]);
 
   return <VizRow h={160}>
     <VizItem w={1}>
       <WorkItemsAggregateDurationsByStateChart
         workItems={[workItem]}
-        title={`Flow Efficiency ${flowEfficiencyPercentage}`}
+        title={workItem.stateType !== "closed" ? "Time spent by state" : "Lead time by state"}
       />
     </VizItem>
   </VizRow>
