02c2260e723f6d4de1661b63b8285d7ddb6423b6
Aman Mavai <aman.mavai@gslab.com>
2022-12-15 17:42:38 +0530

    add another test case by passing phases

--- src/app/dashboards/shared/widgets/work_items/clientSideFlowMetrics.spec.js
+++ src/app/dashboards/shared/widgets/work_items/clientSideFlowMetrics.spec.js
@@ -1,4 +1,5 @@
 import {getNDaysAgo} from "../../../../../../cypress/support/utils";
+import { WorkItemStateTypes } from "../../config";
 import {getFlowEfficiencyUtils} from "./clientSideFlowMetrics";
 
 describe("Flow Efficiency Measurement", () => {
@@ -369,8 +370,263 @@ describe("Flow Efficiency Measurement", () => {
     expect(deliveryCycleDurationsByState).not.toHaveProperty("Backlog");
   });
 
-  test("when there are multiple workItems, second scenario", () => {
-    const workItems = [];
-    const result = getFlowEfficiencyUtils(workItems);
+  test("when there are multiple workItems, with phases open and wip", () => {
+    const workItems = [
+      {
+        name: "Cycle Time Card on Flow Summary not updating when specs/all is toggled. ",
+        key: "335acba7-2b8c-433b-abc3-df3cea1e15e4",
+        displayId: "PP-299",
+        workItemType: "bug",
+        epicName: null,
+        state: "CODE REVIEW",
+        stateType: "wip",
+        workItemsSourceKey: "4ff556a6-b566-4775-8cc3-e77d406e0c16",
+        workItemsSourceName: "Polaris Platform",
+        workItemStateDetails: {
+          currentStateTransition: {
+            eventDate: getNDaysAgo(13)
+          },
+          currentDeliveryCycleDurations: [
+            {
+              state: "created",
+              stateType: "backlog",
+              flowType: null,
+              daysInState: 2.828414351851852,
+            },
+            {
+              state: "CODE REVIEW",
+              stateType: "wip",
+              flowType: "active",
+              daysInState: null,
+            },
+          ],
+        },
+      },
+      {
+        name: "Infra for table pagination",
+        key: "e95ceeec-be69-4313-b29c-6446a23ba665",
+        displayId: "PP-292",
+        workItemType: "story",
+        epicName: "Misc UX: H2 2022",
+        state: "In Progress",
+        stateType: "wip",
+        workItemsSourceKey: "4ff556a6-b566-4775-8cc3-e77d406e0c16",
+        workItemsSourceName: "Polaris Platform",
+        workItemStateDetails: {
+          currentStateTransition: {
+            eventDate: getNDaysAgo(20)
+          },
+          currentDeliveryCycleDurations: [
+            {
+              state: "In Progress",
+              stateType: "wip",
+              flowType: "active",
+              daysInState: null,
+            },
+            {
+              state: "created",
+              stateType: "backlog",
+              flowType: null,
+              daysInState: 5.839421296296297,
+            },
+          ],
+        },
+      },
+      {
+        name: "Update the layout of the Pull Requests detail view when bring it up from the Variability analysis dashboard",
+        key: "44453d90-f76d-47d1-83d4-9bd934595662",
+        displayId: "PP-294",
+        workItemType: "story",
+        epicName: "Misc UX: H2 2022",
+        state: "ACCEPTED",
+        stateType: "complete",
+        workItemsSourceKey: "4ff556a6-b566-4775-8cc3-e77d406e0c16",
+        workItemsSourceName: "Polaris Platform",
+        workItemStateDetails: {
+          currentStateTransition: {
+            eventDate: getNDaysAgo(10)
+          },
+          currentDeliveryCycleDurations: [
+            {
+              state: "ACCEPTED",
+              stateType: "complete",
+              flowType: "waiting",
+              daysInState: null,
+            },
+            {
+              state: "created",
+              stateType: "backlog",
+              flowType: null,
+              daysInState: 1.9154166666666668,
+            },
+          ],
+        },
+      },
+      {
+        name: "Layout issue on Flow Mix details dashboard",
+        key: "7019b22f-e515-491c-846f-9a7c491fb68f",
+        displayId: "PP-293",
+        workItemType: "bug",
+        epicName: "Misc UX: H2 2022",
+        state: "ACCEPTED",
+        stateType: "complete",
+        workItemsSourceKey: "4ff556a6-b566-4775-8cc3-e77d406e0c16",
+        workItemsSourceName: "Polaris Platform",
+        teamNodeRefs: [
+          {
+            teamName: "Data Team",
+            teamKey: "304aff69-ea3b-4a71-9433-0bb0911fc817",
+          },
+        ],
+        workItemStateDetails: {
+          currentStateTransition: {
+            eventDate: getNDaysAgo(12)
+          },
+          currentDeliveryCycleDurations: [
+            {
+              state: "created",
+              stateType: "backlog",
+              flowType: null,
+              daysInState: 1.9172222222222222,
+            },
+            {
+              state: "ACCEPTED",
+              stateType: "complete",
+              flowType: "waiting",
+              daysInState: null,
+            },
+          ],
+        },
+      },
+      {
+        name: "Tool tip enhancements for time in state chart",
+        key: "c27503db-4aa0-4d79-a581-22f4abb8e958",
+        displayId: "PP-290",
+        workItemType: "story",
+        epicName: "Flow Efficiency Dashboards",
+        state: "ACCEPTED",
+        stateType: "complete",
+        workItemsSourceKey: "4ff556a6-b566-4775-8cc3-e77d406e0c16",
+        workItemsSourceName: "Polaris Platform",
+        workItemStateDetails: {
+          currentStateTransition: {
+            eventDate: getNDaysAgo(15)
+          },
+          currentDeliveryCycleDurations: [
+            {
+              state: "ACCEPTED",
+              stateType: "complete",
+              flowType: "waiting",
+              daysInState: null,
+            },
+            {
+              state: "created",
+              stateType: "backlog",
+              flowType: null,
+              daysInState: 1.1385648148148149,
+            },
+          ],
+        },
+      },
+      {
+        name: "Arrows on the previous and next buttons are switched on the commits timeline table",
+        key: "267e99f9-3391-44ad-8af1-7ffe8b20eaad",
+        displayId: "PP-251",
+        workItemType: "bug",
+        epicName: null,
+        state: "ACCEPTED",
+        stateType: "complete",
+        workItemsSourceKey: "4ff556a6-b566-4775-8cc3-e77d406e0c16",
+        workItemsSourceName: "Polaris Platform",
+        workItemStateDetails: {
+          currentStateTransition: {
+            eventDate: getNDaysAgo(10)
+          },
+          currentDeliveryCycleDurations: [
+            {
+              state: "created",
+              stateType: "backlog",
+              flowType: null,
+              daysInState: 1.8797916666666667,
+            },
+            {
+              state: "ACCEPTED",
+              stateType: "complete",
+              flowType: "waiting",
+              daysInState: null,
+            },
+          ],
+        },
+      },
+      {
+        name: "UI for excluding repos from value streams and teams. ",
+        key: "b3d0149f-573c-4b98-8f7a-1530844fb99b",
+        displayId: "PP-154",
+        workItemType: "story",
+        epicName: "Repository Exclusions",
+        state: "CODE REVIEW",
+        stateType: "wip",
+        workItemsSourceKey: "4ff556a6-b566-4775-8cc3-e77d406e0c16",
+        workItemsSourceName: "Polaris Platform",
+        workItemStateDetails: {
+          currentStateTransition: {
+            eventDate: getNDaysAgo(20)
+          },
+          currentDeliveryCycleDurations: [
+            {
+              state: "created",
+              stateType: "backlog",
+              flowType: null,
+              daysInState: 2.1222800925925926,
+            },
+            {
+              state: "CODE REVIEW",
+              stateType: "wip",
+              flowType: "active",
+              daysInState: null,
+            },
+          ],
+        },
+      },
+      {
+        name: "PR Cycle Time Detail Widget: Table View",
+        key: "40d0d1fc-1348-4cfa-beb3-7b4761b7b46e",
+        displayId: "PP-148",
+        workItemType: "story",
+        epicName: "Response Time Pull Request Details",
+        state: "CODE REVIEW",
+        stateType: "wip",
+        workItemsSourceKey: "4ff556a6-b566-4775-8cc3-e77d406e0c16",
+        workItemsSourceName: "Polaris Platform",
+        workItemStateDetails: {
+          currentStateTransition: {
+            eventDate: getNDaysAgo(30)
+          },
+          currentDeliveryCycleDurations: [
+            {
+              state: "CODE REVIEW",
+              stateType: "wip",
+              flowType: "active",
+              daysInState: null,
+            },
+            {
+              state: "Backlog",
+              stateType: "backlog",
+              flowType: "waiting",
+              daysInState: 8.92548611111111,
+            },
+            {
+              state: "created",
+              stateType: "backlog",
+              flowType: null,
+              daysInState: 0.04150462962962963,
+            },
+          ],
+        },
+      },
+    ];
+
+    const {deliveryCycleDurationsByState} = getFlowEfficiencyUtils(workItems, [WorkItemStateTypes.open, WorkItemStateTypes.make]);
+    expect(deliveryCycleDurationsByState).not.toHaveProperty("ACCEPTED");
   });
 });
