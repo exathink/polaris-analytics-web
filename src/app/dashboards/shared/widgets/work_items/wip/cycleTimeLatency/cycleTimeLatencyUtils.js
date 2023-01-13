9e7372a4ebabad35bfe42c23f5f86bcd9832e1b2
Aman Mavai <aman.mavai@gslab.com>
2023-01-13 21:54:58 +0530

    move reusable hook to this file

--- src/app/dashboards/shared/widgets/work_items/wip/cycleTimeLatency/cycleTimeLatencyUtils.js
+++ src/app/dashboards/shared/widgets/work_items/wip/cycleTimeLatency/cycleTimeLatencyUtils.js
@@ -1,3 +1,10 @@
+import React from "react";
+import {useIntl} from "react-intl";
+import { localNow } from "../../../../../../helpers/utility";
+import {getHistogramSeries} from "../../../../../projects/shared/helper/utils";
+import { AppTerms } from "../../../../config";
+import {projectDeliveryCycleFlowMetricsMeta} from "../../../../helpers/metricsMeta";
+
 export const Quadrants = {
   ok: 'ok',
   latency: 'latency',
@@ -47,3 +54,52 @@ export function getQuadrantName(cycleTime, latency, cycleTimeTarget, latencyTarg
   return QuadrantNames[getQuadrant(cycleTime, latency, cycleTimeTarget, latencyTarget)]
 }
 
+
+
+const COL_WIDTH_BOUNDARIES = [1, 3, 7, 14, 30, 60, 90];
+export function useCycleTimeLatencyHook(workItems) {
+  const intl = useIntl();
+  const seriesData = React.useMemo(() => {
+    const pointsByState = workItems
+      .filter((cycle) => cycle.workItemType !== "epic")
+      .reduce((acc, item, index) => {
+        const ageVal = projectDeliveryCycleFlowMetricsMeta["age"].value(item);
+        if (acc[item.state] == null) {
+          acc[item.state] = [ageVal];
+        } else {
+          acc[item.state] = [...acc[item.state], ageVal];
+        }
+        return acc;
+      }, {});
+
+    const seriesArr = Object.entries(pointsByState).map(([state, points]) => {
+      return getHistogramSeries({
+        id: state,
+        intl,
+        colWidthBoundaries: COL_WIDTH_BOUNDARIES,
+        name: state,
+        points,
+      });
+    });
+
+    return seriesArr;
+  }, [workItems, intl]);
+
+  return seriesData;
+}
+
+export function getSubTitleForHistogram({workItems, specsOnly, intl}) {
+  const count = workItems.length;
+
+  const countDisplay = `${count} ${
+    count === 1
+      ? specsOnly
+        ? AppTerms.spec.display
+        : AppTerms.card.display
+      : specsOnly
+      ? AppTerms.specs.display
+      : AppTerms.cards.display
+  }`;
+
+  return `${countDisplay} as of ${localNow(intl)}`;
+}
