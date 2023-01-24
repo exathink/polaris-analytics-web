cdf1cfe457751c8ecb844ba7f9c121b2a7e1245b
Aman Mavai <aman.mavai@gslab.com>
2023-01-24 11:27:32 +0530

    add widget abstraction using flexible compound components to solve prop drilling ,ViewComponents implementation is leaking to Widget Components and other issues

---
+++ src/app/framework/viz/dashboard/widgetCore.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {Loading} from "../../../components/graphql/loading";
+import {logGraphQlError} from "../../../components/graphql/utils";
+
+// not including a default value and that's intentional.
+// As we do not want to render without provider and fail in that case if its there.
+const WidgetContext = React.createContext();
+
+export function useWidget() {
+  const context = React.useContext(WidgetContext);
+  if (context === undefined) {
+    throw new Error("useWidget hook must be used within a Provider");
+  }
+  return context;
+}
+/**
+ * this is the widget component which we write for every new query
+ * import {useQueryFn} from "file-path";
+ * export function SpecialWidget({children, queryVars}) {
+ *   const result = useQueryHookFn(queryVars);
+ * 
+ *    return (
+ *      <WidgetCore result={result} showLoadingError={false} errorContext="SpecialWidget.useQueryHookFn">
+ *        {children}
+ *      </WidgetCore>
+ *    );
+ *  }
+ * 
+ *   
+ *   // ViewComponent template
+ *   export function ViewComponent(props) {
+ *     const {data, loading, error} = useWidget();
+ * 
+ *     // use data and view_props to render views
+ *     return <div> {data} </div>;
+ *   }
+ * 
+ *  
+ *  // Dashboard Component Template
+ *  function Dashboard({}) {
+ *     const [dimension, instanceKey, specsOnly, otherProp, view] = ["project", "123", true, "fine", "detail"];
+ *  
+ *     // This is the way to instantiate widget at the dashboard level
+ *     return (
+ *     <SpecialWidget queryVars={{dimension, instanceKey, specsOnly, otherProp}}>
+ *            <ViewComponent />
+ *        
+ *           // can also be used in a nested way 
+ *           <div className="">
+ *            <ViewComponent />
+ *           </div>
+ *
+ *          // handle view prop
+ *           {view === "detail" && <ViewComponent />}
+ *           {view === "primary" && <ViewComponent />}
+ *      </SpecialWidget>
+ *     );
+ *   }
+ * 
+}
+ */
+export function WidgetCore({children, result, showLoadingError = true, errorContext = ""}) {
+  if (showLoadingError) {
+    if (result.loading) return <Loading />;
+    if (result.error) {
+      logGraphQlError(`${errorContext}`, result.error);
+      return null;
+    }
+  }
+
+  return <WidgetContext.Provider value={result}>{children}</WidgetContext.Provider>;
+}
