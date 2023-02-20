120a939c918c2993ee1da89343d2847a16b937b2
Aman Mavai <aman.mavai@gslab.com>
2023-02-20 21:48:33 +0530

    improve WipQueueSizeChart

--- src/app/dashboards/shared/charts/workItemCharts/wipQueueSizeChart.js
+++ src/app/dashboards/shared/charts/workItemCharts/wipQueueSizeChart.js
@@ -1,49 +1,48 @@
-/*
-This file is a template for creating new chart components. Not intended to be used directly,
-use it as a guide on how to structure a new chart component file. Copy/Paste modify as needed.
- */
-import {Chart, tooltipHtml} from "../../../../framework/viz/charts";
-import {buildIndex, pick, elide} from "../../../../helpers/utility";
+
+import {Chart} from "../../../../framework/viz/charts";
 import {DefaultSelectionEventHandler} from "../../../../framework/viz/charts/eventHandlers/defaultSelectionHandler";
+import {Colors, itemsDesc, WorkItemStateTypeColor} from "../../config";
+import { tooltipHtml_v2 } from "../../../../framework/viz/charts/tooltip";
 
-// uncomment this line and add the relative path to src/app/dashboards/shared/config.js
-import {Colors} from "../../config";
+function getStateCounts(items) {
+    const obj = items.reduce((acc, item) => {
+        if (acc[item.state]!=null) {
+            acc[item.state].count += 1;
+        }else {
+            acc[item.state] = {count: 1, stateType: item.stateType};
+        }
+        return acc;
+    }, {});
+    return obj;
 
+}
 // Return an array of  HighChart series data structures from the
 // passed in props.
 function getSeries(items) {
-  return []
+  return [{
+    key: "wipQueSize",
+    data: Object.entries(getStateCounts(items)).map(e => {
+        return {name: e[0], y: e[1].count, color: WorkItemStateTypeColor[e[1].stateType]};
+    }),
+  },]
 }
 
 export const WipQueueSizeChart = Chart({
-  // Update this function to choose which props will cause the chart config to be regenerated.
   chartUpdateProps: (props) => props,
-
-  // Leave this as is unless you want to create a different selection handler than the default one.
   eventHandler: DefaultSelectionEventHandler,
-
-  // when the default selection handler calls its application callback, it calls this
-  // mapper to map point objects into domain objects for the application. Attach domain objects to the series data
-  // points and map them back here.
   mapPoints: (points, _) => points.map(point => point),
-
-  // These are the minimal props passed by the Chart component. Add
-  // all the additional domain props you will pass to React component here so that
-  // you can use them in building the config.
-  getConfig: ({title, subtitle, intl, view, items, stageName}) => {
+  getConfig: ({title, subtitle, intl, view, items, stageName, specsOnly}) => {
     const series = getSeries(items);
     return {
       chart: {
-        // some default options we include on all charts, but might want to
-        // specialize in some cases.
         backgroundColor: Colors.Chart.backgroundColor,
         panning: true,
         panKey: 'shift',
         zoomType: 'xy',
-
+        type: "column"
       },
       title: {
-        text: `${items.length} Items in ${stageName}` || 'Title',
+        text: `${items.length} ${itemsDesc(specsOnly)} in ${stageName}` || 'Title',
         align: 'left',
       },
       subtitle: {
@@ -52,30 +51,23 @@ export const WipQueueSizeChart = Chart({
       },
       xAxis: {
         type: 'linear',
-
-        title: {
-          text: 'X'
-        }
+        categories: Object.keys(getStateCounts(items))
       },
       yAxis: {
         type: 'linear',
-
         title: {
-          text: 'x'
-        },
+            text: itemsDesc(specsOnly)
+        }
       },
 
       tooltip: {
         useHTML: true,
         hideDelay: 50,
         formatter: function () {
-          // This is the standard way we display tool tips.
-          // A header string followed by a two column table with name, value pairs.
-          // The strings can be HTML.
-          return tooltipHtml({
-            header: ``,
+          return tooltipHtml_v2({
+            header: `${this.point.category}`,
             body: [
-              [``, ``],
+              [``, `${this.point.y}`],
             ]
           })
         }
