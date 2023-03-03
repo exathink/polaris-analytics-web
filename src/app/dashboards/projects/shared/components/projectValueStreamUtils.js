796c8401564eae9441b453f0dd9118e512dbeda7
Aman Mavai <aman.mavai@gslab.com>
2023-03-03 20:45:04 +0530

    persist vs query string when main sidebar links are clicked

--- src/app/dashboards/projects/shared/components/projectValueStreamUtils.js
+++ src/app/dashboards/projects/shared/components/projectValueStreamUtils.js
@@ -1,6 +1,6 @@
 import {capitalize} from "lodash";
 import React from "react";
-import {useHistory} from "react-router-dom";
+import {useHistory, useLocation} from "react-router-dom";
 import {useWidget, WidgetCore} from "../../../../framework/viz/dashboard/widgetCore";
 import {SelectDropdown, useSelect} from "../../../shared/components/select/selectDropdown";
 import {useQueryProjectValueStreams} from "../hooks/useQueryValueStreams";
@@ -8,13 +8,23 @@ import {useQueryProjectValueStreams} from "../hooks/useQueryValueStreams";
 const defaultItem = {key: "all", name: "All"};
 export function ValueStreamsDropdown() {
   const {data} = useWidget();
+  const location = useLocation();
 
   let history = useHistory();
 
   const nodes = data.project.valueStreams.edges.map((edge) => edge.node);
   const items = nodes.map((node) => ({key: node.key, name: capitalize(node.name)}));
   const uniqueItems = [defaultItem, ...items];
-  const {handleChange} = useSelect({uniqueItems, defaultVal: defaultItem});
+  const {handleChange, valueIndex} = useSelect({uniqueItems, defaultVal: defaultItem});
+
+  React.useEffect(() => {
+    if (valueIndex === 0) {
+      history.push({search: ""});
+    } else {
+      history.push({search: `?vs=${uniqueItems[valueIndex].key}`});
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.pathname]);
 
   function handleChangeWrapper(index) {
     if (index === 0) {
