ecb3e2ba611c412e2f1e2a463ddcb16051cace02
Aman Mavai <aman.mavai@gslab.com>
2023-03-27 23:31:19 +0530

    add router as a provider for test utils

--- src/test/providers.js
+++ src/test/providers.js
@@ -4,6 +4,8 @@ import {MockedProvider} from "@apollo/client/testing";
 import AppLocale from "../app/i18n";
 import config, {getCurrentLanguage} from "../containers/LanguageSwitcher/config";
 import {TestDataContext} from "../app/framework/viz/charts/TestDataContext";
+import {Router} from "react-router";
+import {createMemoryHistory} from "history";
 
 export const currentAppLocale = AppLocale[getCurrentLanguage(config.defaultLanguage || "english").locale];
 
@@ -15,14 +17,27 @@ function AppProviders({children}) {
   );
 }
 
+
+export function AppRouterProviders({children}) {
+  let history = createMemoryHistory();
+  return (
+    <IntlProvider locale={currentAppLocale.locale} messages={currentAppLocale.messages}>
+      <Router history={history}>{children}</Router>
+    </IntlProvider>
+  );
+}
+
 // Higher order utility function to wrap with MockedProvider
 // this will help in mocking api responses for widget components
 function getAppProviders(mocks) {
+  let history = createMemoryHistory();
   return ({children}) => (
     <IntlProvider locale={currentAppLocale.locale} messages={currentAppLocale.messages}>
-      <MockedProvider mocks={mocks} addTypename={false}>
-        {children}
-      </MockedProvider>
+      <Router history={history}>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          {children}
+        </MockedProvider>
+      </Router>
     </IntlProvider>
   );
 }
