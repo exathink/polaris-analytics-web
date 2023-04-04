cdb6feefc335fc470ffb1043f164865aa1fb434e
Aman Mavai <aman.mavai@gslab.com>
2023-04-04 17:05:44 +0530

    add SelectDropdownMultiple component and its corresponding hook

--- src/app/dashboards/shared/components/select/selectUtils.js
+++ src/app/dashboards/shared/components/select/selectUtils.js
@@ -64,13 +64,86 @@ export function SelectDropdown({
         defaultValue={uniqueItems[defaultValueIndex].value}
         style={{width: 200}}
         className={className}
-        onChange={(value, option) => {
+        onChange={(_value, option) => {
           handleChange(option);
         }}
         title={selectedValue?.label ?? uniqueItems[defaultValueIndex].label}
       >
         {uniqueItems.map((item) => (
           <Option key={item.value} value={item.value} title={item.label}>
+            {item.label}
+          </Option>
+        ))}
+      </Select>
+    </div>
+  );
+}
+
+export function useSelectMultiple() {
+  const [selectedValues, setSelectedValues] = React.useState([]);
+  function handleChange(values) {
+    setSelectedValues(values);
+  }
+  return {selectedValues, handleChange};
+}
+
+/**
+ * @typedef {import("antd/lib/select").DefaultOptionType} DefaultOptionType
+ *
+ * @typedef {Object} PropsMultiple
+ * @property {DefaultOptionType[]} uniqueItems - The unique items.
+ * @property {(values?: [DefaultOptionType]) => void} handleChange - The change handler.
+ * @property {number[]} [defaultValueIndexes] - default value indexes
+ * @property {string} testId - testId for select dropdown
+ * @property {'row' | 'col'} [layout] - row or column layout
+ * @property {string} [wrapperClassName] - class for parent wrapper div
+ * @property {string} [title] - tooltip for select
+ * @property {string} [className] - class for select
+ */
+
+/**
+ * A Custom Select component
+ * @param {PropsMultiple} {
+ *   uniqueItems,
+ *   handleChange,
+ *   defaultValueIndexes
+ *   testId,
+ *   layout="col",
+ *   wrapperClassName,
+ *   title,
+ *   className,
+ * }
+ */
+export function SelectDropdownMultiple({
+  uniqueItems,
+  handleChange,
+  defaultValueIndexes,
+  testId,
+  layout = "col",
+  wrapperClassName,
+  title,
+  className
+}) {
+  const selectClassName = classNames(
+    "tw-flex",
+    layout === "col" ? "tw-flex-col" : "tw-flex-row tw-items-center tw-space-x-4",
+    wrapperClassName
+  );
+
+  return (
+    <div data-testid={testId} className={selectClassName}>
+      {title && <div>{title}</div>}
+      <Select
+        defaultValue={defaultValueIndexes.map(i => uniqueItems[i].value)}
+        style={{width: 200}}
+        className={className}
+        onChange={(_values, options) => {
+          handleChange(options);
+        }}
+        mode="multiple"
+      >
+        {uniqueItems.map((item) => (
+          <Option key={item.value} value={item.value}>
             {item.label}
           </Option>
         ))}
