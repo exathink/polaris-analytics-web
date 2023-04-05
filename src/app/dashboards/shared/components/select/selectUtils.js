77a80a9a9c4ac0f23f3b189f2f6482f25ed062cd
Aman Mavai <aman.mavai@gslab.com>
2023-04-05 18:18:40 +0530

    add showArrow prop

--- src/app/dashboards/shared/components/select/selectUtils.js
+++ src/app/dashboards/shared/components/select/selectUtils.js
@@ -143,6 +143,7 @@ export function SelectDropdownMultiple({
         }}
         mode="multiple"
         getPopupContainer={(node) => node.parentNode}
+        showArrow
       >
         {uniqueItems.map((item) => (
           <Option key={item.value} {...item}>
