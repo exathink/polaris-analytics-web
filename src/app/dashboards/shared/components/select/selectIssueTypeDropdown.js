e1fb3c9e9a235f6af3b4eddd1e61ad8000b129c9
Aman Mavai <aman.mavai@gslab.com>
2023-04-17 13:35:23 +0530

    revert to prev api, fix regression

--- src/app/dashboards/shared/components/select/selectIssueTypeDropdown.js
+++ src/app/dashboards/shared/components/select/selectIssueTypeDropdown.js
@@ -1,14 +1,14 @@
 import {workItemTypeImageMap} from "../../../projects/shared/helper/renderers";
 import {SelectDropdown} from "./selectDropdown";
 
-export const defaultIssueType = {value: "all", label: "All"};
+export const defaultIssueType = {key: "all", name: "All"};
 
 export const uniqueIssueTypes = [
   defaultIssueType,
-  {value: "story", label: "Story", icon: workItemTypeImageMap.story},
-  {value: "task", label: "Task", icon: workItemTypeImageMap.task},
-  {value: "bug", label: "Bug", icon: workItemTypeImageMap.bug},
-  {value: "subtask", label: "Sub Task", icon: workItemTypeImageMap.subtask},
+  {key: "story", name: "Story", icon: workItemTypeImageMap.story},
+  {key: "task", name: "Task", icon: workItemTypeImageMap.task},
+  {key: "bug", name: "Bug", icon: workItemTypeImageMap.bug},
+  {key: "subtask", name: "Sub Task", icon: workItemTypeImageMap.subtask},
 ];
 
 export function SelectIssueTypeDropdown({valueIndex, handleIssueTypeChange, className, wrapperClassName}) {
