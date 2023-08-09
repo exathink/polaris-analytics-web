64bebfa1665da75f9ee04a1519ba63ce5245be45
priya <pmukundan0@gmail.com>
2023-08-08 22:54:13 -0500

    [PP-543] Expose Story Points, Releases and Sprints as table columns in the UI

--- src/app/dashboards/projects/shared/hooks/useQueryProjectClosedDeliveryCycleDetail.js
+++ src/app/dashboards/projects/shared/hooks/useQueryProjectClosedDeliveryCycleDetail.js
@@ -42,6 +42,10 @@ export const queryDimensionClosedDeliveryCycleDetail = (dimension) =>  gql`
             workItemKey
             workItemType
             isBug
+            storyPoints
+            releases
+            priority
+
 
             state
             stateType
