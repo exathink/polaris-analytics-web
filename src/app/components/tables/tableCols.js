c2712a85c502440c451f8fe2acfe447d2d9cf463
priya <pmukundan0@gmail.com>
2023-08-09 06:38:35 -0500

    [PP-543] Expose Story Points, Releases and Sprints as table columns in the UI

--- src/app/components/tables/tableCols.js
+++ src/app/components/tables/tableCols.js
@@ -193,9 +193,54 @@ export function useOptionalColumnsForWorkItems({filters, workTrackingIntegration
     [hasCustomTags, filters]
   );
 
+  const hasStoryPoints = hidden_cols.includes("storyPoints");
+  const col9 = React.useMemo(
+    () => ({
+      field: "storyPoints",
+      headerName: "Story Points",
+      cellRenderer: React.memo(TextWithStyle),
+      menuTabs: MenuTabs,
+      hide: !hasStoryPoints,
+      comparator: SORTER.number_compare,
+    }),
+    [hasStoryPoints]
+  );
+
+  const hasReleases = hidden_cols.includes("releases");
+  const col10 = React.useMemo(
+    () => ({
+      field: "releases",
+      headerName: "Releases",
+      cellRenderer: React.memo(TextWithStyle),
+      menuTabs: MenuTabs,
+      hide: !hasReleases,
+    }),
+    [hasReleases]
+  );
+
+  const hasPriority = hidden_cols.includes("priority");
+  const col11 = React.useMemo(
+    () => ({
+      field: "priority",
+      headerName: "Priority",
+      cellRenderer: React.memo(TextWithStyle),
+      filter: "agSetColumnFilter",
+      filterParams: {
+        filterOptions: ["contains", "startsWith"],
+        buttons: ["reset"],
+        maxNumConditions: 1,
+      },
+      menuTabs: MenuTabs,
+      hide: !hasPriority,
+
+    }),
+    [hasPriority]
+  );
+
+
   const optionalCustomCols = workTrackingIntegrationType === "jira" ? [col6, col7, col8] : [];
 
-  return [col1, col2, col3, col4, col5, ...optionalCustomCols];
+  return [col1, col2, col3, col4, col5, col9, col10, col11, ...optionalCustomCols];
 }
 
 export function getWorkItemNameCol() {
