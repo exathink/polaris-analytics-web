e779690935435aee8fc303d6724dc83071f83787
Aman Mavai <aman.mavai@gslab.com>
2023-09-07 15:00:31 +0530

    update usage of common wip metrics hook

--- src/app/dashboards/shared/widgets/work_items/wip/cycleTimeLatency/dimensionWipMetricsView.js
+++ src/app/dashboards/shared/widgets/work_items/wip/cycleTimeLatency/dimensionWipMetricsView.js
@@ -1,59 +1,27 @@
 import React from "react";
 import { useIntl } from "react-intl";
-import { average, i18nNumber } from "../../../../../../helpers/utility";
 import {AvgAge, Wip} from "../../../../components/flowStatistics/flowStatistics";
-import {getWipLimit, getWorkItemDurations} from "../../clientSideFlowMetrics";
-import { Quadrants, getQuadrant } from "./cycleTimeLatencyUtils";
+import {DevItemRatio, useWipMetricsCommon} from "../../clientSideFlowMetrics";
 import { getPercentage } from "../../../../../projects/shared/helper/utils";
 
-function DevItemRatio({devItemsCount, devItemsPercentage}) {
-  return (
-    <div className="tw-textXl tw-flex tw-flex-col">
-      <div className="tw-flex tw-items-center tw-gap-2">
-        <div>{devItemsCount}</div>
-        <div className="tw-textSm">{devItemsCount===1 ? "Dev Item": "Dev Items"}</div>
-      </div>
-      <div className="tw-flex tw-items-center tw-gap-2">
-        <div className="tw-textSm">Dev Item Ratio</div>
-        <div>{devItemsPercentage}</div>
-      </div>
-    </div>
-  );
-}
 
 export function DimensionWipMetricsView({data, dataForSpecs, flowMetricsData, dimension, displayBag, excludeAbandoned, cycleTimeTarget, latencyTarget, specsOnly, days}) {
   const intl = useIntl();
 
-  const workItems = React.useMemo(() => {
-    const edges = data?.[dimension]?.["workItems"]?.["edges"] ?? [];
-    return edges.map((edge) => edge.node);
-  }, [data, dimension]);
-  const workItemAggregateDurations = excludeAbandoned
-    ? getWorkItemDurations(workItems).filter((w) => getQuadrant(w.cycleTime, w.latency, cycleTimeTarget, latencyTarget) !== Quadrants.abandoned)
-    : getWorkItemDurations(workItems);
-
-    const workItemsForSpecs = React.useMemo(() => {
-      const edges = dataForSpecs?.[dimension]?.["workItems"]?.["edges"] ?? [];
-      return edges.map((edge) => edge.node);
-    }, [dataForSpecs, dimension]);
-
-    const workItemAggregateDurationsForSpecs = excludeAbandoned
-      ? getWorkItemDurations(workItemsForSpecs).filter(
-          (w) => getQuadrant(w.cycleTime, w.latency, cycleTimeTarget, latencyTarget) !== Quadrants.abandoned
-        )
-      : getWorkItemDurations(workItemsForSpecs);
-
-  const avgCycleTime = average(workItemAggregateDurations, (item) => item.cycleTime);
-
-  const pipelineCycleMetrics = {
-    [specsOnly ? "workItemsWithCommits" : "workItemsInScope"]: workItemAggregateDurations.length,
-    avgCycleTime: i18nNumber(intl, avgCycleTime, 2),
-  };
+  const {wipLimit, pipelineCycleMetrics, workItemAggregateDurationsForSpecs, workItemAggregateDurations} =
+    useWipMetricsCommon({
+      data,
+      dataForSpecs,
+      flowMetricsData,
+      dimension,
+      specsOnly,
+      days,
+      excludeAbandoned,
+      cycleTimeTarget,
+      latencyTarget,
+    });
 
   const {displayType, metric, displayProps} = displayBag;
-  
-  const wipLimit = getWipLimit({flowMetricsData, dimension, specsOnly, intl, cycleTimeTarget, days});
-
   const metricMap = {
     volume: (
       <Wip
