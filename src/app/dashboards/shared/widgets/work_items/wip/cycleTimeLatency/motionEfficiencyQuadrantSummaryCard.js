539d6717fa22040fc77e49c4feeb9b2e0116e110
Aman Mavai <aman.mavai@gslab.com>
2023-09-20 16:43:16 +0000

    Resolve PP-587

--- src/app/dashboards/shared/widgets/work_items/wip/cycleTimeLatency/motionEfficiencyQuadrantSummaryCard.js
+++ src/app/dashboards/shared/widgets/work_items/wip/cycleTimeLatency/motionEfficiencyQuadrantSummaryCard.js
@@ -5,6 +5,8 @@ import {useMotionEfficiency} from "../../clientSideFlowMetrics";
 import {FlowEfficiencyDetailsView} from "./flowEfficiencyDetailsView";
 import {filterByStateTypes} from "./cycleTimeLatencyUtils";
 import {WorkItemsCycleTimeVsLatencyChart} from "../../../../charts/workItemCharts/workItemsCycleTimeVsLatencyChart";
+import { CardInspectorWithDrawer, useCardInspector } from "../../../../../work_items/cardInspector/cardInspectorUtils";
+import { EVENT_TYPES } from "../../../../../../helpers/utility";
 
 export function MotionEfficiencyQuadrantSummaryCard({
   workItems,
@@ -14,12 +16,21 @@ export function MotionEfficiencyQuadrantSummaryCard({
   latencyTarget,
   onQuadrantClick,
   selectedQuadrant,
+  context,
   className,
   displayBag,
 }) {
   const filteredWorkItems = filterByStateTypes(workItems, stateTypes);
   const [workInMotion, percentage] = useMotionEfficiency(filteredWorkItems, latencyTarget);
   const workItemsDisplay = itemsDesc(specsOnly);
+  const {workItemKey, setWorkItemKey, showPanel, setShowPanel} = useCardInspector();
+
+  function handleSelectionChange(items, eventType) {
+    if (eventType === EVENT_TYPES.POINT_CLICK) {
+      setWorkItemKey(items[0].key);
+      setShowPanel(true);
+    }
+  }
 
   return (
     <PlainCard
@@ -39,28 +50,42 @@ export function MotionEfficiencyQuadrantSummaryCard({
         placement: "bottom",
         content: <FlowEfficiencyDetailsView workItems={workItems} phases={stateTypes} />,
       }}
-      latencyView={displayBag?.showLatencyPopup ? {
-        title: (
-          <div className="tw-text-lg tw-text-gray-300">
-            Motion Analysis, All {specsOnly? "Dev Items" : "Work Items"}
-          </div>
-        ),
-        placement: "bottom",
-        content: (
-          <div className="tw-w-[500px]">
-            <WorkItemsCycleTimeVsLatencyChart
-              stageName={"Process"}
-              workItems={filteredWorkItems}
-              groupByState={true}
-              tooltipType={"small"}
-              specsOnly={specsOnly}
-              stateTypes={stateTypes}
-              cycleTimeTarget={cycleTimeTarget}
-              latencyTarget={latencyTarget}
-            />
-          </div>
-        ),
-      }: undefined}
+      latencyView={
+        displayBag?.showLatencyPopup
+          ? {
+              title: (
+                <div className="tw-text-lg tw-text-gray-300">
+                  Motion Analysis, All {specsOnly ? "Dev Items" : "Work Items"}
+                </div>
+              ),
+              placement: "bottom",
+              content: (
+                <div className="tw-w-[500px]">
+                  <WorkItemsCycleTimeVsLatencyChart
+                    stageName={"Process"}
+                    workItems={filteredWorkItems}
+                    groupByState={true}
+                    tooltipType={"small"}
+                    specsOnly={specsOnly}
+                    stateTypes={stateTypes}
+                    cycleTimeTarget={cycleTimeTarget}
+                    latencyTarget={latencyTarget}
+                    onSelectionChange={handleSelectionChange}
+                  />
+
+                    <CardInspectorWithDrawer
+                      workItemKey={workItemKey}
+                      showPanel={showPanel}
+                      setShowPanel={setShowPanel}
+                      context={context}
+                      drawerOptions={{placement: "bottom"}}
+                    />
+                  
+                </div>
+              ),
+            }
+          : undefined
+      }
     >
       <QuadrantSummaryPanel
         workItems={filteredWorkItems}
@@ -68,7 +93,14 @@ export function MotionEfficiencyQuadrantSummaryCard({
         latencyTarget={latencyTarget}
         onQuadrantClick={onQuadrantClick}
         selectedQuadrant={selectedQuadrant}
-        popupProps={{specsOnly, cycleTimeTarget, latencyTarget, stateTypes, showQuadrantPopup: displayBag?.showQuadrantPopup}}
+        popupProps={{
+          specsOnly,
+          cycleTimeTarget,
+          latencyTarget,
+          stateTypes,
+          showQuadrantPopup: displayBag?.showQuadrantPopup,
+          context,
+        }}
         {...displayBag}
       />
     </PlainCard>
