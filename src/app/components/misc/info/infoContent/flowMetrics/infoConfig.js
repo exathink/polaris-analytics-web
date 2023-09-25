73fcb8ac47ac9d5ad3c41f1915725f5ea4619a59
Krishna Kumar <kkumar@exathink.com>
2023-09-25 14:28:39 -0500

    PP-598: Add summary for Stability

--- src/app/components/misc/info/infoContent/flowMetrics/infoConfig.js
+++ src/app/components/misc/info/infoContent/flowMetrics/infoConfig.js
@@ -15,13 +15,31 @@ export function StabilityInfoCard({ inline = true }) {
   return (
     <InfoCard
       title={"Stability"}
-      content={"bar"}
-      showDrawer={true}
+      content={<StabilityInfoSummary/>}
+      showDrawer={false}
       drawerContent={"baz"}
       inline={inline}
     />
   );
 };
+
+function StabilityInfoSummary() {
+  return (
+    <>
+      <Section>
+        <p>
+          It is relatively easy to release <em>something</em> every <em>N</em> days.
+        </p>
+        <p>But it is much harder to release everything you <em>started</em> working on, <em>within N days</em>.</p>
+        <p>If your delivery process can do this for some fixed <em>N</em>, it is a <em>stable</em> process.</p>
+
+        <p>
+          The data and analytics from this dashboard can be used to tell if your development process is unstable, and guide you on how to stabilize it.
+        </p>
+      </Section>
+    </>
+  )
+}
 
 export function TimeboxInfoCard({ inline = true }) {
   return (
