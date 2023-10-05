f02ea10d49d36258688aa1aba66e68d225004b0c
Aman Mavai <aman.mavai@gslab.com>
2023-10-05 18:17:11 +0000

    PP-606: Clearing filters on the WIP dashboard needs more fine tuning

--- src/app/dashboards/shared/widgets/work_items/wip/cycleTimeLatency/dimensionCycleTimeLatencyView.js
+++ src/app/dashboards/shared/widgets/work_items/wip/cycleTimeLatency/dimensionCycleTimeLatencyView.js
@@ -93,6 +93,24 @@ export const DimensionCycleTimeLatencyView = ({
     displayBag?.setWipChartType("queue");
   }
 
+  function handleAgeFilterReset() {
+    appliedFilters.delete(FILTERS.PRIMARY_CATEGORY);
+    appliedFilters.delete(FILTERS.CURRENT_INTERACTION);
+    appliedFilters.delete(FILTERS.HISTOGRAM_BUCKET);
+
+    setAppliedFilters(new Map(appliedFilters));
+    displayBag?.setWipChartType("age");
+  }
+
+  function handleQueueSizeFilterReset() {
+    appliedFilters.delete(FILTERS.PRIMARY_CATEGORY);
+    appliedFilters.delete(FILTERS.CURRENT_INTERACTION);
+    appliedFilters.delete(FILTERS.STATE);
+
+    setAppliedFilters(new Map(appliedFilters));
+    displayBag?.setWipChartType("queue");
+  }
+
   function handleQuadrantClear() {
     appliedFilters.delete(FILTERS.QUADRANT_PANEL);
     appliedFilters.delete(FILTERS.CURRENT_INTERACTION);
@@ -234,7 +252,7 @@ export const DimensionCycleTimeLatencyView = ({
       selectedFilter = getFilterValue(appliedFilters, FILTERS.STATE)[0].value;
     }
 
-    const ageFilterElement = <AgeFilterWrapper selectedFilter={selectedFilter} handleClearClick={handleResetAll} />;
+    const ageFilterElement = <AgeFilterWrapper selectedFilter={selectedFilter} handleClearClick={handleAgeFilterReset} />;
     const quadrantFilterElement = selectedQuadrant && chartCategory === chart_category && (
       <QuadrantFilterWrapper
         selectedQuadrant={QuadrantNames[selectedQuadrant]}
@@ -243,7 +261,7 @@ export const DimensionCycleTimeLatencyView = ({
       />
     );
     const queueSizeFilterElement = (
-      <QueueSizeFilterWrapper selectedFilter={selectedFilter} handleClearClick={handleResetAll} />
+      <QueueSizeFilterWrapper selectedFilter={selectedFilter} handleClearClick={handleQueueSizeFilterReset} />
     );
 
     let filterElement;
