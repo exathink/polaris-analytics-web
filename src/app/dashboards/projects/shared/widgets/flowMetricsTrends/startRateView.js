72486d9f2f9f5d92c3b87ff5c1fc84168d5b0a82
Aman Mavai <aman.mavai@gslab.com>
2023-10-31 17:55:51 +0000

    PP-644: Start Rate remaining items

--- src/app/dashboards/projects/shared/widgets/flowMetricsTrends/startRateView.js
+++ src/app/dashboards/projects/shared/widgets/flowMetricsTrends/startRateView.js
@@ -6,14 +6,29 @@
 
 import {StartRate} from "../../../../shared/components/flowStatistics/flowStatistics";
 
+function getGoodnessIndicator(currentThroughputTrend, currentArrivalTrend, specsOnly) {
+    const START_RATE_THRESHOLD = 5;
+    const departures = specsOnly ? currentThroughputTrend["workItemsWithCommits"] : currentThroughputTrend["workItemsInScope"];
+    const arrivals = currentArrivalTrend['arrivals'];
+    if (arrivals > 0) {
+      // good means that the departure rate is within threshold % of the arrival rate.
+      return (Math.abs(arrivals - departures) / arrivals)*100 < START_RATE_THRESHOLD;
+    } else {
+      return true
+    }
+}
+
 export function StartRateView({
   arrivalDepartureTrends,
+  cycleMetricsTrends,
   flowAnalysisPeriod,
   specsOnly,
   displayType,
   displayProps,
 }) {
   let [currentArrivalTrend, previousArrivalTrend] = arrivalDepartureTrends;
+  let [currentThroughputTrend] = cycleMetricsTrends;
+  const dummy_target = 2;
   const startRate = currentArrivalTrend["arrivals"] ? currentArrivalTrend["arrivals"] / flowAnalysisPeriod : null;
   const supportingMetric = currentArrivalTrend["arrivals"] ? <span>Start Rate: {startRate.toFixed(1)} / day</span> : <span></span>;
   return (
@@ -23,6 +38,11 @@ export function StartRateView({
         previousMeasurement={previousArrivalTrend}
         displayType={displayType}
         specsOnly={specsOnly}
+        target={dummy_target}
+        // explicitly setting this to false since we need to figure out a better mechanism for goodness indicator here.
+        good={false}
+        // explicity passing a function to evaluate the goodness of value since we are not passing goodness of trend.
+        valueGood={()=> getGoodnessIndicator(currentThroughputTrend, currentArrivalTrend, specsOnly)}
         displayProps={{
           info: {title: "title"},
           subTitle: <span>Last {flowAnalysisPeriod} Days</span>,
