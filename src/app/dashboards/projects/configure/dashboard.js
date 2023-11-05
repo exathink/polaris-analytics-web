9268ad148073167169b071b9cd717a45be875292
Krishna Kumar <kkumar@exathink.com>
2023-11-05 09:55:27 -0600

    PP-649:Change the title of the value streams model topic to "Nested Value Streams"

--- src/app/dashboards/projects/configure/dashboard.js
+++ src/app/dashboards/projects/configure/dashboard.js
@@ -193,7 +193,7 @@ export function ValueStreamWorkStreamEditorView({projectKey}) {
         />
       )}
       <div className="tw-flex tw-items-center tw-justify-between">
-        <LabelValue label={"Value Streams"} className="tw-ml-2" />
+        <LabelValue label={"Nested Value Streams"} className="tw-ml-2" />
         <Button type="primary" className="tw-mr-2" onClick={() => setVisible(true)}>
           {" "}
           <PlusOutlined /> New Value Stream
