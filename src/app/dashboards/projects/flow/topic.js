377055ebb30d9224c75dff228a56bb47acefb85a
Krishna Kumar <kkumar@exathink.com>
2023-11-05 16:08:13 -0600

    PP-649:Cleanup: remove ununsed feature flag check on stability routes.

--- src/app/dashboards/projects/flow/topic.js
+++ src/app/dashboards/projects/flow/topic.js
@@ -12,31 +12,30 @@ const topic = {
   ...Topics.flow,
   routes: [
     {
-      requiredFeatures: ['ui.new-card-design'],
       match: "stability",
       subnav: true,
       topic: Stability,
     },
     {
-      requiredFeatures: ['ui.new-card-design'],
+
       match: "wip",
       subnav: true,
       topic: Wip,
     },
     {
-      requiredFeatures: ['ui.new-card-design'],
+
       match: "responseTime",
       subnav: true,
       topic: ResponseTime,
     },
    {
-      requiredFeatures: ['ui.new-card-design'],
+
       match: "throughput",
       subnav: true,
       topic: Throughput,
     },
     {
-      requiredFeatures: ['ui.new-card-design'],
+
       match: "valueMix",
       subnav: true,
       topic: ValueMix,
@@ -47,16 +46,9 @@ const topic = {
       topic: TimeboxSettings
     },
     {
-      requiredFeatures: ['ui.new-card-design'],
       match: '',
       redirect: 'stability'
     },
-    // This technique works because when the feature flag is present the redirect route to topic overides the route to
-    // the main dashboard. Its a bit of a hack, but works for now without having to have a switch in the routes array.
-    {
-      match: '',
-      component: React.lazy(() => import('./dashboard'))
-    }
   ]
 };
 
