06745a3af69fae8e0dce35bf5a4f9b2d089472dd
Krishna Kumar <kkumar@exathink.com>
2023-11-05 10:55:08 -0600

    PP-649: Refactor: Move value stream editor dashboard components out of configure/dashboard.js into valueStreams/dashboard.js

--- src/app/dashboards/projects/configure/dashboard.js
+++ src/app/dashboards/projects/configure/dashboard.js
@@ -7,7 +7,6 @@ import { WorkItemStateTypeMapWidget } from "../shared/widgets/workItemStateTypeM
 import styles from "./dashboard.module.css";
 import fontStyles from "../../../framework/styles/fonts.module.css";
 import classNames from "classnames";
-import Button from "../../../../components/uielements/button";
 
 import {
   MeasurementSettingsDashboard,
@@ -22,16 +21,8 @@ import {
 import {
   DeliveryProcessMappingInitialInfoConfig
 } from "../../../components/misc/info/infoContent/deliveryProcessMapping/infoConfig";
-import { useWidget, WidgetCore } from "../../../framework/viz/dashboard/widgetCore";
-import { useQueryProjectValueStreams } from "../shared/hooks/useQueryValueStreams";
-import { LabelValue } from "../../../helpers/components";
-import { PlusOutlined } from "@ant-design/icons";
-import { ValueStreamForm } from "../shared/components/projectValueStreamUtils";
-import { ValueStreamEditorTable } from "../shared/components/valueStreamEditorTable";
-import { useCreateValueStream } from "../shared/hooks/useCreateValueStream";
-import { logGraphQlError } from "../../../components/graphql/utils";
-import { Alert } from "antd";
 import { ValueStreamMappingDashboard } from "./valueStreamMapping/dashboard";
+import { ValueStreamWorkStreamEditorDashboard } from "./valueStreams/dashboard";
 
 const dashboard_id = "dashboards.project.configure";
 ValueStreamMappingDashboard.videoConfig = {
@@ -105,142 +96,6 @@ export function ValueStreamMappingInitialDashboard() {
                 showMeLinkVisible={true}
               />
             );
-          }}
-        />
-      </DashboardRow>
-    </Dashboard>
-  );
-}
-
-export function ValueStreamWorkStreamEditorView({projectKey}) {
-  const {data} = useWidget();
-  const edges = data.project.valueStreams?.edges ?? [];
-  const items = edges.map((edge) => edge.node);
-
-  const tags = data.project.tags ?? [];
-  const uniqWorkItemSelectors = [...new Set(tags)];
-
-  const [status, updateStatus] = React.useReducer(
-    (data, partialData) => ({
-      ...data,
-      ...partialData,
-    }),
-    {mode: "", message: ""}
-  );
-
-  const [visible, setVisible] = React.useState(false);
-  function onClose() {
-    setVisible(false);
-  }
-
-  // mutation to create value stream
-  const [mutate, {loading: mutationLoading, client}] = useCreateValueStream({
-    onCompleted: ({createValueStream}) => {
-      if (createValueStream.success) {
-        updateStatus({mode: "success", message: "Created Value Stream Successfully."});
-        client.resetStore();
-      } else {
-        logGraphQlError("ValueStreamEditorTable.useUpdateValueStream", createValueStream.errorMessage);
-        updateStatus({mode: "error", message: createValueStream.errorMessage});
-      }
-    },
-    onError: (error) => {
-      logGraphQlError("ValueStreamEditorTable.useUpdateValueStream", error);
-      updateStatus({mode: "error", message: error.message});
-    },
-  });
-
-  function handleSubmit(values) {
-    const payload = {
-      name: values.name,
-      description: values.description ?? "test",
-      workItemSelectors: values.workItemSelectors,
-    };
-    // call mutation on save button click
-    mutate({
-      variables: {
-        projectKey,
-        ...payload,
-      },
-    });
-
-    onClose();
-  }
-
-  return (
-    <div className="">
-      {mutationLoading && (
-        <Button className="tw-ml-auto tw-mr-[90px]" type="primary" loading>
-          Processing...
-        </Button>
-      )}
-      {status.mode === "success" && (
-        <Alert
-          message={status.message}
-          type="success"
-          showIcon
-          closable
-          className="tw-ml-auto tw-mr-[90px] tw-w-[300px]"
-        />
-      )}
-      {status.mode === "error" && (
-        <Alert
-          message={status.message}
-          type="error"
-          showIcon
-          closable
-          className="tw-ml-auto tw-mr-[90px] tw-w-[300px]"
-        />
-      )}
-      <div className="tw-flex tw-items-center tw-justify-between">
-        <LabelValue label={"Value Streams"} className="tw-ml-2" />
-        <Button type="primary" className="tw-mr-2" onClick={() => setVisible(true)}>
-          {" "}
-          <PlusOutlined /> Nested Value Stream
-        </Button>
-        <ValueStreamForm
-          formType="NEW_FORM"
-          uniqWorkItemSelectors={uniqWorkItemSelectors}
-          onSubmit={handleSubmit}
-          initialValues={{
-            name: "",
-            description: "",
-            workItemSelectors: [],
-          }}
-          visible={visible}
-          onClose={onClose}
-        />
-      </div>
-      <ValueStreamEditorTable tableData={items} projectKey={projectKey} uniqWorkItemSelectors={uniqWorkItemSelectors} />
-    </div>
-  );
-}
-
-export function ValueStreamWorkStreamEditorWidget({instanceKey, context, view}) {
-  const result = useQueryProjectValueStreams({instanceKey});
-
-  return (
-    <WidgetCore result={result} errorContext="ValueStreamWorkStreamEditorWidget.useQueryProjectValueStreams">
-      {view === "primary" && <ValueStreamWorkStreamEditorView projectKey={instanceKey} />}
-    </WidgetCore>
-  );
-}
-
-export function ValueStreamWorkStreamEditorDashboard({}) {
-  const {
-    project: {key},
-    context,
-  } = useProjectContext();
-
-  return (
-    <Dashboard gridLayout={true} className="tw-grid tw-grid-cols-5 tw-gap-2">
-      <DashboardRow title={""}>
-        <DashboardWidget
-          className="tw-col-span-3 tw-col-start-2 tw-row-span-6 tw-row-start-1"
-          title={" "}
-          name="valuestream-workstream-editor"
-          render={({view}) => {
-            return <ValueStreamWorkStreamEditorWidget instanceKey={key} context={context} view={view} />;
           }}
         />
       </DashboardRow>
--- src/app/dashboards/projects/configure/valueStreams/dashboard.js
+++ src/app/dashboards/projects/configure/valueStreams/dashboard.js
@@ -4,9 +4,156 @@
  *
  */
 
-import { ProjectDashboard } from "../../projectDashboard";
-import {ValueStreamWorkStreamEditorDashboard} from "../dashboard";
+import { ProjectDashboard, useProjectContext } from "../../projectDashboard";
 import React from "react";
+import { useWidget, WidgetCore } from "../../../../framework/viz/dashboard/widgetCore";
+import { useCreateValueStream } from "../../shared/hooks/useCreateValueStream";
+import { logGraphQlError } from "../../../../components/graphql/utils";
+import Button from "../../../../../components/uielements/button";
+import { Alert } from "antd";
+import { LabelValue } from "../../../../helpers/components";
+import { PlusOutlined } from "@ant-design/icons";
+import { ValueStreamForm } from "../../shared/components/projectValueStreamUtils";
+import { ValueStreamEditorTable } from "../../shared/components/valueStreamEditorTable";
+import { useQueryProjectValueStreams } from "../../shared/hooks/useQueryValueStreams";
+import { Dashboard, DashboardRow, DashboardWidget } from "../../../../framework/viz/dashboard";
+
+export function ValueStreamWorkStreamEditorView({ projectKey }) {
+  const { data } = useWidget();
+  const edges = data.project.valueStreams?.edges ?? [];
+  const items = edges.map((edge) => edge.node);
+
+  const tags = data.project.tags ?? [];
+  const uniqWorkItemSelectors = [...new Set(tags)];
+
+  const [status, updateStatus] = React.useReducer(
+    (data, partialData) => ({
+      ...data,
+      ...partialData
+    }),
+    { mode: "", message: "" }
+  );
+
+  const [visible, setVisible] = React.useState(false);
+
+  function onClose() {
+    setVisible(false);
+  }
+
+  // mutation to create value stream
+  const [mutate, { loading: mutationLoading, client }] = useCreateValueStream({
+    onCompleted: ({ createValueStream }) => {
+      if (createValueStream.success) {
+        updateStatus({ mode: "success", message: "Created Value Stream Successfully." });
+        client.resetStore();
+      } else {
+        logGraphQlError("ValueStreamEditorTable.useUpdateValueStream", createValueStream.errorMessage);
+        updateStatus({ mode: "error", message: createValueStream.errorMessage });
+      }
+    },
+    onError: (error) => {
+      logGraphQlError("ValueStreamEditorTable.useUpdateValueStream", error);
+      updateStatus({ mode: "error", message: error.message });
+    }
+  });
+
+  function handleSubmit(values) {
+    const payload = {
+      name: values.name,
+      description: values.description ?? "test",
+      workItemSelectors: values.workItemSelectors
+    };
+    // call mutation on save button click
+    mutate({
+      variables: {
+        projectKey,
+        ...payload
+      }
+    });
+
+    onClose();
+  }
+
+  return (
+    <div className="">
+      {mutationLoading && (
+        <Button className="tw-ml-auto tw-mr-[90px]" type="primary" loading>
+          Processing...
+        </Button>
+      )}
+      {status.mode === "success" && (
+        <Alert
+          message={status.message}
+          type="success"
+          showIcon
+          closable
+          className="tw-ml-auto tw-mr-[90px] tw-w-[300px]"
+        />
+      )}
+      {status.mode === "error" && (
+        <Alert
+          message={status.message}
+          type="error"
+          showIcon
+          closable
+          className="tw-ml-auto tw-mr-[90px] tw-w-[300px]"
+        />
+      )}
+      <div className="tw-flex tw-items-center tw-justify-between">
+        <LabelValue label={"Value Streams"} className="tw-ml-2" />
+        <Button type="primary" className="tw-mr-2" onClick={() => setVisible(true)}>
+          {" "}
+          <PlusOutlined /> Nested Value Stream
+        </Button>
+        <ValueStreamForm
+          formType="NEW_FORM"
+          uniqWorkItemSelectors={uniqWorkItemSelectors}
+          onSubmit={handleSubmit}
+          initialValues={{
+            name: "",
+            description: "",
+            workItemSelectors: []
+          }}
+          visible={visible}
+          onClose={onClose}
+        />
+      </div>
+      <ValueStreamEditorTable tableData={items} projectKey={projectKey} uniqWorkItemSelectors={uniqWorkItemSelectors} />
+    </div>
+  );
+}
+
+export function ValueStreamWorkStreamEditorWidget({ instanceKey, context, view }) {
+  const result = useQueryProjectValueStreams({ instanceKey });
+
+  return (
+    <WidgetCore result={result} errorContext="ValueStreamWorkStreamEditorWidget.useQueryProjectValueStreams">
+      {view === "primary" && <ValueStreamWorkStreamEditorView projectKey={instanceKey} />}
+    </WidgetCore>
+  );
+}
+
+export function ValueStreamWorkStreamEditorDashboard({}) {
+  const {
+    project: { key },
+    context
+  } = useProjectContext();
+
+  return (
+    <Dashboard gridLayout={true} className="tw-grid tw-grid-cols-5 tw-gap-2">
+      <DashboardRow title={""}>
+        <DashboardWidget
+          className="tw-col-span-3 tw-col-start-2 tw-row-span-6 tw-row-start-1"
+          title={" "}
+          name="valuestream-workstream-editor"
+          render={({ view }) => {
+            return <ValueStreamWorkStreamEditorWidget instanceKey={key} context={context} view={view} />;
+          }}
+        />
+      </DashboardRow>
+    </Dashboard>
+  );
+}
 
 const dashboard = (({viewerContext}) => (
   <ProjectDashboard>
